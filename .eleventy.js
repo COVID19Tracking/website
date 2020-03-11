0a9e0be12e2da3bd219ebb553948de347e59055f
Kai Curry <kai@cape.io>
2020-03-11 19:26:13 -0400

    netlify redirects file for API proxy

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("_src/_assets");
 	eleventyConfig.addPassthroughCopy("_src/sw.js");
 	eleventyConfig.addPassthroughCopy("_src/admin/");
+	eleventyConfig.addPassthroughCopy("_redirects");
 
 	eleventyConfig.addFilter(
 		'cssmin',
