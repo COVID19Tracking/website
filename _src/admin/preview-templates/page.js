22ec8eb53b24605933668f4bab3b18578cd97ed9
Mat Marquis <mat@matmarquis.com>
2020-03-11 10:09:07 -0400

    Fine-tuning previews.

--- _src/admin/preview-templates/page.js
+++ _src/admin/preview-templates/page.js
@@ -4,16 +4,19 @@ const html = htm.bind(h);
 
 // Preview component for a Page
 const Page = createClass({
-  render() {
-    const entry = this.props.entry;
+	render() {
+	const entry = this.props.entry;
 
-    return html`
-      <main>
-        <h1>${entry.getIn(["data", "title"], null)}</h1>
-        ${this.props.widgetFor("body")}
-      </main>
-    `;
-  }
+	return html`
+	<main id="main">
+		<div class="body container">
+			<h1>${entry.getIn(["data", "title"], null)}</h1>
+			<p class="lede">${entry.getIn(["data", "summary"], null)}</p>
+			${this.props.widgetFor("body")}
+		</div>
+	</main>
+		`;
+		}
 });
 
 export default Page;
