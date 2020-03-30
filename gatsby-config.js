292ef9c5bd17e3d33be1d9f26649289e91c8f5b8
Kevin Miller <keveemiller@gmail.com>
2020-03-30 07:41:34 -0400

    feat(design): New icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -139,7 +139,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`,
+        icon: `src/images/icon.svg`,
       },
     },
   ],
