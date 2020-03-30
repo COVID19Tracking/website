0ff19dc53641f65f4cb2a8391abfa6a02732a794
Andrew Schwartz <andrew@aschwartz.me>
2020-03-30 10:54:44 -0600

    feat(design): add div.tablesaw-overflow wrapper to summary table, re-indent

--- src/components/common/summary-table.js
+++ src/components/common/summary-table.js
@@ -4,62 +4,64 @@ import '../../scss/components/common/summary-table.scss'
 import thousands from '../../utilities/format-thousands'
 
 export default ({ data }) => (
-  <Table>
-    <caption className="sr-only">{/* todo include captions */}</caption>
-    <thead>
-      <colgroup span="3" />
-      <colgroup span="2" />
-      <colgroup span="2" />
-      <colgroup span="2" />
-      <col />
-      <col />
-      <col />
-      <col />
-      <tr>
-        <th scope="colgroup" colSpan="3">
-          Tests
-        </th>
-        <th scope="colgroup" colSpan="2">
-          Hospitalized
-        </th>
-        <th scope="colgroup" colSpan="2">
-          In ICU
-        </th>
-        <th scope="colgroup" colSpan="2">
-          On Ventilator
-        </th>
-        <td colSpan="2" />
-      </tr>
-      <tr>
-        <th scope="col">Positive</th>
-        <th scope="col">Negative</th>
-        <th scope="col">Pending</th>
-        <th scope="col">Currently</th>
-        <th scope="col">Cumulative</th>
-        <th scope="col">Currently</th>
-        <th scope="col">Cumulative</th>
-        <th scope="col">Recovered</th>
-        <th scope="col">Discharged</th>
-        <th scope="col">Deaths</th>
-        <th scope="col">
-          Total test results <span>(Positive + Negative)</span>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{data.positive ? thousands(data.positive) : 'N/A'}</td>
-        <td>{data.negative ? thousands(data.negative) : 'N/A'}</td>
-        <td>{data.pending ? thousands(data.pending) : 'N/A'}</td>
-        <td>#,###{/* todo configure API */}</td>
-        <td>{data.hospitalized ? thousands(data.hospitalized) : 'N/A'}</td>
-        <td>#,###{/* todo configure API */}</td>
-        <td>#,###{/* todo configure API */}</td>
-        <td>#,###{/* todo configure API */}</td>
-        <td>#,###{/* todo configure API */}</td>
-        <td>{data.death ? thousands(data.death) : 'N/A'}</td>
-        <td>{data.total ? thousands(data.total) : 'N/A'}</td>
-      </tr>
-    </tbody>
-  </Table>
+  <div className="tablesaw-overflow">
+    <Table>
+      <caption className="sr-only">{/* todo include captions */}</caption>
+      <thead>
+        <colgroup span="3" />
+        <colgroup span="2" />
+        <colgroup span="2" />
+        <colgroup span="2" />
+        <col />
+        <col />
+        <col />
+        <col />
+        <tr>
+          <th scope="colgroup" colSpan="3">
+            Tests
+          </th>
+          <th scope="colgroup" colSpan="2">
+            Hospitalized
+          </th>
+          <th scope="colgroup" colSpan="2">
+            In ICU
+          </th>
+          <th scope="colgroup" colSpan="2">
+            On Ventilator
+          </th>
+          <td colSpan="2" />
+        </tr>
+        <tr>
+          <th scope="col">Positive</th>
+          <th scope="col">Negative</th>
+          <th scope="col">Pending</th>
+          <th scope="col">Currently</th>
+          <th scope="col">Cumulative</th>
+          <th scope="col">Currently</th>
+          <th scope="col">Cumulative</th>
+          <th scope="col">Recovered</th>
+          <th scope="col">Discharged</th>
+          <th scope="col">Deaths</th>
+          <th scope="col">
+            Total test results <span>(Positive + Negative)</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{data.positive ? thousands(data.positive) : 'N/A'}</td>
+          <td>{data.negative ? thousands(data.negative) : 'N/A'}</td>
+          <td>{data.pending ? thousands(data.pending) : 'N/A'}</td>
+          <td>#,###{/* todo configure API */}</td>
+          <td>{data.hospitalized ? thousands(data.hospitalized) : 'N/A'}</td>
+          <td>#,###{/* todo configure API */}</td>
+          <td>#,###{/* todo configure API */}</td>
+          <td>#,###{/* todo configure API */}</td>
+          <td>#,###{/* todo configure API */}</td>
+          <td>{data.death ? thousands(data.death) : 'N/A'}</td>
+          <td>{data.total ? thousands(data.total) : 'N/A'}</td>
+        </tr>
+      </tbody>
+    </Table>
+  </div>
 )
