6be27ff9acff7be59320d6b55b76228ea0816978
Kevin Miller <keveemiller@gmail.com>
2020-03-30 18:14:31 -0400

    fix(design): Less press items

--- src/components/pages/homepage/press-list.js
+++ src/components/pages/homepage/press-list.js
@@ -11,7 +11,7 @@ export default () => {
           title: { ne: "null" }
         }
         sort: { fields: publishDate, order: DESC }
-        limit: 6
+        limit: 4
       ) {
         edges {
           node {
