c2eeb30f52df0bbeafd430a8f8dd71fc38518ecc
Kevin Miller <keveemiller@gmail.com>
2020-03-31 18:10:07 -0400

    feat(docs): Include contributing guide

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,14 @@
 import { addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import typography from '../src/utilities/typography'
+import { addons } from '@storybook/addons'
 import '../src/scss/global.scss'
 import './style.css'
+import theme from './theme'
+
+addons.setConfig({
+  theme: theme,
+})
 
 typography.injectStyles()
 
---
+++ .storybook/theme.js
@@ -0,0 +1,12 @@
+import { create } from '@storybook/theming/create'
+import logo from '../src/images/project-logo-black.svg'
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'The COVID Tracking Project',
+  brandUrl: 'http://covidtracking.com/',
+  brandImage: logo,
+})
+
+//0C0D3B,#1D1F61,#575AAD,#FFFFFF,#1D1F61
