260ef18d671e5b8893669c7bbfce1116396301d4
Andrew Schwartz <andrew@aschwartz.me>
2020-04-02 12:41:05 -0600

    fix: use svgs for state grades instead of css

--- src/components/common/state-grade.js
+++ src/components/common/state-grade.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import '../../scss/components/common/state-grade.scss'
+import gradeA from '../../images/state-grades/grade-a.svg'
+import gradeB from '../../images/state-grades/grade-b.svg'
+import gradeC from '../../images/state-grades/grade-c.svg'
+import gradeD from '../../images/state-grades/grade-d.svg'
+import gradeF from '../../images/state-grades/grade-f.svg'
+import gradeNA from '../../images/state-grades/grade-na.svg'
 
-const StateGrade = ({ letterGrade }) => (
-  <p className="state-grade">
-    <span>
-      Current <Link to="about-data#data-quality-grade">data grade</Link>:
-    </span>
-    <div data-grade={letterGrade}>
-      <span>{letterGrade || 'N/A'}</span>
-    </div>
-  </p>
-)
+const StateGrade = ({ letterGrade }) => {
+  const svgElement = function(grade) {
+    switch (grade) {
+      case 'A':
+        return <img src={gradeA} alt="Grade A" width="28px" />
+      case 'B':
+        return <img src={gradeB} alt="Grade B" width="28px" />
+      case 'C':
+        return <img src={gradeC} alt="Grade C" width="28px" />
+      case 'D':
+        return <img src={gradeD} alt="Grade D" width="28px" />
+      case 'F':
+        return <img src={gradeF} alt="Grade F" width="28px" />
+      default:
+        return <img src={gradeNA} alt="No grade" width="28px" />
+    }
+  }
+
+  return (
+    <p className="state-grade">
+      <span>
+        Current <Link to="about-data#data-quality-grade">data grade</Link>:
+      </span>
+      <div title={`Grade ${letterGrade || 'not available'}`}>
+        {svgElement(letterGrade)}
+      </div>
+    </p>
+  )
+}
 
 export default StateGrade
