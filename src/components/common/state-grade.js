de4b27591381fcb58212aaffa810859419c789b3
Andrew Schwartz <andrew@aschwartz.me>
2020-04-01 22:53:30 -0600

    fix: data quality grade link: 404 --> anchor link

--- src/components/common/state-grade.js
+++ src/components/common/state-grade.js
@@ -5,7 +5,7 @@ import '../../scss/components/common/state-grade.scss'
 const StateGrade = ({ letterGrade }) => (
   <p className="state-grade">
     <span>
-      Current <Link to="about-data/data-quality-grade">data grade</Link>:
+      Current <Link to="about-data#data-quality-grade">data grade</Link>:
     </span>
     <div data-grade={letterGrade}>
       <span>{letterGrade || 'N/A'}</span>
