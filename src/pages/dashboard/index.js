0020ab9b987c61b7ef2c9010178666bc7a613c1d
Gabe O'Leary <oleary.gabe@gmail.com>
2020-04-03 10:39:49 -0700

    initial conversion commit for dashboard

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/layout'
+import Map from './_Map'
+
+const DashboardPage = () => (
+  <Layout title="Visual Dashboard">
+    <p>Test</p>
+    <Map />
+  </Layout>
+)
+
+export default DashboardPage
