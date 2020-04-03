40deb20260d62dee9cf260fce239a7d775fa3ec5
Gabe O'Leary <oleary.gabe@gmail.com>
2020-04-03 16:06:22 -0700

    feat(dashboard-gatsby): added bubble & bubble legend. using memoized values/fns

--- src/pages/dashboard/_Map.js
+++ src/pages/dashboard/_Map.js
@@ -3,8 +3,9 @@ import React, { useState, useEffect, useMemo } from 'react'
 import { format } from 'd3-format'
 import { geoPath, geoAlbersUsa } from 'd3-geo'
 import { json } from 'd3-fetch'
+import { max } from 'd3-array'
 import { nest } from 'd3-collection'
-import { scaleThreshold } from 'd3-scale'
+import { scaleSqrt, scaleThreshold } from 'd3-scale'
 import { schemeOranges, schemeGreys, schemePurples } from 'd3-scale-chromatic'
 import { timeFormat, timeParse } from 'd3-time-format'
 
@@ -55,39 +56,35 @@ const getColor = {
   ),
 }
 
-export default function Map() {
-  const formatDate = timeFormat('%b. %d')
-  const formatNumber = format(',.0f')
-  const parseDate = timeParse('%Y%m%d')
+//should be imported from constants file
+const colors = {
+  totalTestResults: '#696DC2',
+  positive: '#E5A968',
+  death: '#404856',
+}
 
+const formatNumber = format(',.0f')
+const formatDate = timeFormat('%b. %d')
+const parseDate = timeParse('%Y%m%d')
+
+export default function Map() {
   const StatesJson = StatesWithPopulation
 
-  //should be imported from constants file
-  const colors = {
-    totalTestResults: '#696DC2',
-    positive: '#E5A968',
-    death: '#404856',
-  }
-  const getValue = (d, field = currentField, normalized = false) =>
-    ((d.properties.dailyData[currentDate] &&
-      d.properties.dailyData[currentDate][field]) ||
-      0) / (normalized ? d.properties.population / 1000000 : 1)
-
   // holds the date of the displayed day
-  const [currentDate, setCurrentDate] = useState('')
+  const [currentDate, setCurrentDate] = useState('20200401')
   // holds the field we are currently viewing
   const [currentField, setCurrentField] = useState('positive')
 
   const [useChoropleth, setUseChoropleth] = useState(true)
 
-  const margin = {
-    bottom: 10,
-    left: 10,
-    right: 10,
-    top: 10,
-  }
-  const height = 400
-  const width = 700
+  const getValue = useMemo(
+    () => (d, field = currentField, normalized = false) =>
+      ((d.properties.dailyData[currentDate] &&
+        d.properties.dailyData[currentDate][field]) ||
+        0) / (normalized ? d.properties.population / 1000000 : 1),
+    [currentDate, currentField],
+  )
+
   const [rawStateData, setRawStateData] = useState(null)
 
   const joinedData = useMemo(() => {
@@ -115,7 +112,26 @@ export default function Map() {
     }))
     return { ...StatesJson, features: joinedFeatures }
   }, [rawStateData])
-  // holds all data in geojson objects TODO: convert to useMemo? - won't be necessary once we're using gatsby data
+
+  const maxValue = useMemo(
+    () =>
+      joinedData &&
+      max(joinedData.features, d => getValue(d, 'totalTestResults')),
+    [joinedData, getValue],
+  )
+
+  const r = useMemo(
+    () =>
+      maxValue &&
+      scaleSqrt()
+        .domain([0, maxValue])
+        .range([0, 50]),
+    [maxValue],
+  )
+
+  console.log('maxValue:', maxValue)
+  console.log('r:', r)
+
   useEffect(() => {
     const fetchData = async () => {
       const stateData = await json('https://covidtracking.com/api/states/daily')
@@ -127,31 +143,37 @@ export default function Map() {
         })
         .entries(stateData)
         .reverse()
-      /*
-      slider
-        .attr('min', 0)
-        .attr('max', groupedByDate.length - 1)
-        .attr('step', 1)
-        */
     }
     fetchData()
   }, [])
+
   return (
     <div className="test">
       Test
-      {joinedData && (
-        <States
-          geoJson={joinedData}
-          useChoropleth={useChoropleth}
-          currentDate={currentDate}
-          currentField={currentField}
-        />
+      {joinedData && !useChoropleth && (
+        <BubbleLegend joinedData={joinedData} r={r} maxValue={maxValue} />
       )}
+      <svg width={width} height={height}>
+        {joinedData && (
+          <React.Fragment>
+            <States
+              geoJson={joinedData}
+              useChoropleth={useChoropleth}
+              currentDate={currentDate}
+              currentField={currentField}
+            />
+            {!useChoropleth && (
+              <Bubbles geoJson={joinedData} getValue={getValue} r={r} />
+            )}
+          </React.Fragment>
+        )}
+      </svg>
     </div>
   )
 }
 
 const States = ({ geoJson, useChoropleth, currentDate, currentField }) => {
+  //below function should use getValue
   const getColorFromFeature = d => {
     if (!useChoropleth) return 'white'
     const normalizationPopulation = 1000000 // 1 million;
@@ -171,9 +193,84 @@ const States = ({ geoJson, useChoropleth, currentDate, currentField }) => {
       stroke="#ababab"
     />
   ))
+  return <g>{states}</g>
+}
+
+const Bubbles = ({ geoJson, r, getValue }) => {
+  if (!r) return null
+
+  // filter out "states" outside of render area (should be hoisted)
+  const features = geoJson.features.filter(
+    d => d.properties.centroidCoordinates[0],
+  )
+
+  const createBubble = (d, i, property) => {
+    const props = {
+      cx: d.properties.centroidCoordinates[0],
+      cy: d.properties.centroidCoordinates[1],
+      r: r(getValue(d, property)),
+    }
+    return (
+      <circle
+        key={property + i}
+        {...props}
+        fill={colors[property]}
+        stroke={colors[property]}
+        fillOpacity={property === 'positive' ? 0.8 : 0.2}
+      />
+    )
+  }
+  const testBubbles = features.map((d, i) =>
+    createBubble(d, i, 'totalTestResults'),
+  )
+  const positiveBubbles = features.map((d, i) => createBubble(d, i, 'positive'))
   return (
-    <svg width={700} height={500}>
-      {states}
+    <React.Fragment>
+      <g id="testBubbles">{testBubbles}</g>
+      <g id="positiveBubble">{positiveBubbles}</g>
+    </React.Fragment>
+  )
+}
+
+const BubbleLegend = ({ r, maxValue }) => {
+  console.log('BubbleLegend maxValue: ', maxValue)
+  const formatLegendEntry = d => parseInt(format('.1r')(d))
+  const legendData = [
+    formatLegendEntry(maxValue * 0.1),
+    formatLegendEntry(maxValue * 0.5),
+    formatLegendEntry(maxValue),
+  ]
+  const legendBubbles = legendData.map((d, i) => (
+    <circle
+      key={'legendBubbles' + i}
+      cx={52}
+      cy={145 - r(d)}
+      r={r(d)}
+      stroke="#ababab"
+      fill="none"
+    />
+  ))
+  const legendLines = legendData.map((d, i) => (
+    <line
+      key={'legendLines' + i}
+      x1={52}
+      y1={145 - 2 * r(d)}
+      x2={130}
+      y2={145 - 2 * r(d)}
+      stroke="#ababab"
+      strokeDasharray="5 5"
+    />
+  ))
+  const legendText = legendData.map((d, i) => (
+    <text key={'legendText' + i} x={110} y={140 - 2 * r(d)}>
+      {formatNumber(d)}
+    </text>
+  ))
+  return (
+    <svg width={150} height={150} style={{ overflow: 'visible' }}>
+      {legendBubbles}
+      {legendLines}
+      {legendText}
     </svg>
   )
 }
