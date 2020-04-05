c65147eec21b340aa5a408c6fbdd57a9b2d0202b
melbee <melba@transferhero.com>
2020-04-05 16:35:49 -0700

    fix(dashboard): resolved rebase conflicts

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,74 +1,15 @@
-import { json } from 'd3-fetch'
-import { timeParse } from 'd3-time-format'
+/* eslint-disable */
 
 import React, { useState, useEffect } from 'react'
 import Layout from '../../components/layout'
-import AreaChart from './_AreaChart'
+import AreaChart from './AreaChart'
 // import Map from './_Map'
 
-const parseDate = timeParse('%Y%m%d')
-
-function calculateTotal(d) {
-  return d.positive + (d.negative || 0)
-}
-
-function transformUsDaily(data) {
-  return data
-    .map(d => {
-      const date = parseDate(d.date)
-      return [
-        {
-          date,
-          label: 'Total',
-          value: calculateTotal(d),
-        },
-        {
-          date,
-          label: 'Positive',
-          value: d.positive,
-        },
-      ]
-    })
-    .flat()
-}
-
+// this is the entry point component for the dashboard page and should only render imported components
 const DashboardPage = () => {
-  const [data, updateData] = useState([])
-
-  useEffect(() => {
-    async function fetch() {
-      const usDaily = await json('https://covidtracking.com/api/us/daily')
-      const sortedUsDaily = usDaily.sort(function(a, b) {
-        const aDate = parseDate(a.date)
-        const bDate = parseDate(b.date)
-
-        return aDate - bDate
-      })
-      const transformed = transformUsDaily(sortedUsDaily)
-      updateData(transformed)
-    }
-    fetch()
-  }, [])
-
   return (
     <Layout title="Visual Dashboard">
-      <p>Test</p>
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      <AreaChart
-        data={data}
-        fill={d => {
-          if (d === 'Total') return '#585BC1'
-          return '#FFA270'
-        }}
-        height={400}
-        labelOrder={['Total', 'Positive']}
-        marginBottom={40}
-        marginLeft={80}
-        marginRight={10}
-        marginTop={10}
-        xTicks={2}
-        width={400}
-      />
+      <AreaChart />
       {/* <Map /> */}
     </Layout>
   )
