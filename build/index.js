8b56b1be22ae09710b25cbac622682f4fcefa910
Phips Peter <pspeter333@gmail.com>
2020-04-06 13:42:58 -0700

    Make API errors red
    Hopefully this makes them easier to read in Netlify output.

--- build/index.js
+++ build/index.js
@@ -2,12 +2,13 @@
 const { forEachP } = require('understory')
 const { fetchSave } = require('./handlers')
 const resources = require('./datasources')
+const util = require('util')
 
 // Send each resouce to fetchSave(). Waits for promise to resolve before sending next one.
 
 function exitError(err) {
-  console.error('API ERROR!')
-  console.error(err)
+  console.error(`\u001b[${util.inspect.colors.red[0]}mAPI ERROR\n${err}\u001b[${util.inspect.colors.red[1]}m`)
   process.exit(1)
 }
+
 forEachP(fetchSave, resources).catch(exitError)
