8ea8301602e38305a46917cd92e6a6c9f9e09571
Kai Curry <kai@cape.io>
2020-04-06 17:02:40 -0400

    API: Apply prettier

--- build/index.js
+++ build/index.js
@@ -1,13 +1,15 @@
+const util = require('util')
 // const _ = require('lodash/fp')
 const { forEachP } = require('understory')
 const { fetchSave } = require('./handlers')
 const resources = require('./datasources')
-const util = require('util')
 
 // Send each resouce to fetchSave(). Waits for promise to resolve before sending next one.
 
 function exitError(err) {
-  console.error(`\u001b[${util.inspect.colors.red[0]}mAPI ERROR\n${err}\u001b[${util.inspect.colors.red[1]}m`)
+  console.error(
+    `\u001b[${util.inspect.colors.red[0]}mAPI ERROR\n${err}\u001b[${util.inspect.colors.red[1]}m`,
+  )
   process.exit(1)
 }
 
