9d7b039024c77c920ae991e318729145fedac309
Gabe O'Leary <oleary.gabe@gmail.com>
2020-04-07 18:04:06 -0700

    converted US Area chart to use Gatsby data

---
+++ src/pages/dashboard/_UsAreaChartContainer.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import AreaChart from './charts/_AreaChart'
+import { parseDate } from './_util'
+
+export default function UsAreaChartContainer() {
+  const transformData = data => {
+    const transformedData = data.allCovidUsDaily.edges
+      .map(({ node }) => [
+        {
+          date: parseDate(node.date),
+          label: 'Total',
+          value: node.totalTestResults,
+        },
+        { date: parseDate(node.date), label: 'Positive', value: node.positive },
+      ])
+      .flat()
+    return transformedData
+  }
+  const query = graphql`
+    {
+      allCovidUsDaily {
+        edges {
+          node {
+            totalTestResults
+            positive
+            date
+          }
+        }
+      }
+    }
+  `
+  return (
+    <StaticQuery
+      query={query}
+      render={data => (
+        <AreaChart
+          data={transformData(data)}
+          fill={d => {
+            if (d === 'Total') return '#585BC1'
+            return '#FFA270'
+          }}
+          height={400}
+          labelOrder={['Total', 'Positive']}
+          marginBottom={40}
+          marginLeft={80}
+          marginRight={10}
+          marginTop={10}
+          xTicks={2}
+          width={400}
+        />
+      )}
+    />
+  )
+}
