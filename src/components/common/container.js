49b9c2e30b9d601e5b8e0a74bfaf417c5d229374
jcrane <jcrane@partnercomm.net>
2020-04-13 11:54:58 -0500

    Conditionally render narrow container div

--- src/components/common/container.js
+++ src/components/common/container.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import containerStyles from './container.module.scss'
 
-export default ({ children, narrow }) => (
-  <div className={`container ${containerStyles.container}`}>
-    <div className={`${narrow && containerStyles.containerNarrow}`}>
-      {children}
-    </div>
-  </div>
-)
+export default ({ children, narrow }) => {
+  if (narrow) {
+    return (
+      <div className={`container ${containerStyles.container}`}>
+        <div className={`${narrow && containerStyles.containerNarrow}`}>
+          {children}
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div className={`container ${containerStyles.container}`}>{children}</div>
+  )
+}
