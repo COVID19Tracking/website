c0ad057b77bea325513e150b3e045ccdc20d7219
goldfarb <a_f_goldfarb@yahoo.com>
2020-04-13 15:46:28 -0400

    added alignment for the total cumulative deaths chart

--- src/pages/dashboard/_UsCumulativeDeathsContainer.js
+++ src/pages/dashboard/_UsCumulativeDeathsContainer.js
@@ -48,6 +48,7 @@ export default function UsAreaChartContainer() {
             marginTop={10}
             xTicks={3}
             width={400}
+            align="center"
           />
         </div>
       </div>
