1179edc1294a2d9c94e87c3cdcd5bf71a82917ed
jcrane <jcrane@partnercomm.net>
2020-04-13 10:54:04 -0500

    Align narrow containers to left and reduce max width

--- src/components/common/container.js
+++ src/components/common/container.js
@@ -2,10 +2,9 @@ import React from 'react'
 import containerStyles from './container.module.scss'
 
 export default ({ children, narrow }) => (
-  <div
-    className={`container ${containerStyles.container} ${narrow &&
-      containerStyles.containerNarrow}`}
-  >
-    {children}
+  <div className={`container ${containerStyles.container}`}>
+    <div className={`${narrow && containerStyles.containerNarrow}`}>
+      {children}
+    </div>
   </div>
 )
