3a04c6e851854269a95b56d995286769f9569786
friej715 <friej715@newschool.edu>
2020-04-12 21:56:12 -0400

    moving locally, tweaking style

--- src/components/common/swagger-sandbox.js
+++ src/components/common/swagger-sandbox.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import SwaggerUI from 'swagger-ui-react'
 import 'swagger-ui-react/swagger-ui.css'
+import './swagger-sandbox.scss'
 
 export default () => (
   <SwaggerUI
     defaultModelExpandDepth={10}
     docExpansion="list"
-    url="https://api.swaggerhub.com/apis/janelastname/COVID-endpoints/1.0/swagger.json"
+    url="/api-docs/COVID-tracking-endpoints-1.0-docs.json"
   />
 )
