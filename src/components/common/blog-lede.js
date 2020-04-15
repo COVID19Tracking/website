df96d72e575ebc26b8f6e553fb62ab68bae20d24
Andrew Schwartz <andrew@aschwartz.me>
2020-04-15 09:17:01 -0600

    feat(blog-lede): add css class

--- src/components/common/blog-lede.js
+++ src/components/common/blog-lede.js
@@ -5,7 +5,7 @@ import blogLedeStyles from './blog-lede.module.scss'
 export default ({ headline, author, date, lede }) => (
   <div className={blogLedeStyles.lede}>
     <h1>{headline}</h1>
-    <p>{lede}</p>
+    <p className="blog-lede">{lede}</p>
     <Byline author={author} date={date} />
   </div>
 )
