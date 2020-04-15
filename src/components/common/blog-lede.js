872d1ccf5f6b573203d1f4196d7c776a18c76986
Andrew Schwartz <andrew@aschwartz.me>
2020-04-15 09:15:07 -0600

    fix(blog-lede): make blog headlines h1

--- src/components/common/blog-lede.js
+++ src/components/common/blog-lede.js
@@ -4,7 +4,7 @@ import blogLedeStyles from './blog-lede.module.scss'
 
 export default ({ headline, author, date, lede }) => (
   <div className={blogLedeStyles.lede}>
-    <h2>{headline}</h2>
+    <h1>{headline}</h1>
     <p>{lede}</p>
     <Byline author={author} date={date} />
   </div>
