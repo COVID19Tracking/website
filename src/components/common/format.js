493ff0785996c7f675cebf78cd91549cb2e11473
Andrew Schwartz <andrew@aschwartz.me>
2020-04-16 09:54:15 -0600

    feat: increase date format flexibility
    - add the lowercaseMeridiem function to make AM/PM --> am/pm, when given a datestring
    - add the formatDateToString function to return a date _string_,
    not a React component, to allow for inline date rendering

--- src/components/common/format.js
+++ src/components/common/format.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { DateTime } from 'luxon'
 
-const FormatDate = ({ date, format = "ccc LLL d yyyy h:mm a 'ET'" }) => {
+function lowercaseMeridiem(dateString) {
+  return dateString.replace('AM', 'am').replace('PM', 'pm')
+}
+
+function formatDateToString(date, format = "ccc LLL d yyyy h:mm a 'ET'") {
   if (typeof date === 'undefined') {
     return null
   }
-  return <>{DateTime.fromISO(date).toFormat(format)}</>
+  return lowercaseMeridiem(DateTime.fromISO(date).toFormat(format))
+}
+
+const FormatDate = ({ date, format = "ccc LLL d yyyy h:mm a 'ET'" }) => {
+  return <>{formatDateToString(date, format)}</>
 }
 
 const FormatNumber = ({ number }) => (
   <>{number || number === 0 ? number.toLocaleString() : 'N/A'}</>
 )
 
-export { FormatDate, FormatNumber }
+export { FormatDate, FormatNumber, lowercaseMeridiem, formatDateToString }
