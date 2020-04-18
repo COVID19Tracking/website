5e01e31b82e1d1f7e0ae79fe62f1fb4e27a96503
Vijay Sharwar <vijaysharwar@gmail.com>
2020-04-18 19:56:40 -0400

    feat(charts): prevent AreaChart tooltips from crowding right edge of viewport

--- src/components/charts/tooltip.js
+++ src/components/charts/tooltip.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 import styles from './tooltip.module.scss'
 
+const rightBuffer = 250
+
 const Tooltip = ({ x, y, children }) => {
+  let left = x
+
+  if (window.innerWidth < x + rightBuffer) {
+    left -= x + rightBuffer - window.innerWidth
+  }
+
   return (
-    <div style={{ top: y, left: x }} className={styles.root}>
+    <div style={{ top: y, left }} className={styles.root}>
       {children}
     </div>
   )
