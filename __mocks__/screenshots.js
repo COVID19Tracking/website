a6a3fe78cd9ff5e056bdf475de0e67ff1cc209a2
Kevin Miller <keveemiller@gmail.com>
2020-04-19 19:44:41 -0400

    chore: Added tests for state page components

---
+++ __mocks__/screenshots.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    node: {
+      dateChecked: '2020-04-17T06:15:06Z',
+      url: 'https://covidtracking.com/screenshot/1.png',
+    },
+  },
+
+  {
+    node: {
+      dateChecked: '2020-04-17T10:15:06Z',
+      url: 'https://covidtracking.com/screenshot/2.png',
+    },
+  },
+
+  {
+    node: {
+      dateChecked: '2020-03-05T10:15:06Z',
+      url: 'https://covidtracking.com/screenshot/3.png',
+    },
+  },
+]
---
+++ src/__tests__/components/pages/state/screenshots.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Screenshots from '../../../../components/pages/state/screenshots'
+import sampleScreenshots from 'screenshots'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('Components : Pages : State : Screenshots', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Screenshots date={20200417} screenshots={sampleScreenshots} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+
+    expect(
+      shallow(
+        <Screenshots date={20200417} screenshots={sampleScreenshots} />,
+      ).find('li').length,
+    ).toBe(2)
+  })
+})
