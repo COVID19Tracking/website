54064004b490598c4bf1b8588323db2efe1bcb6a
Kevin Miller <keveemiller@gmail.com>
2020-04-20 06:40:06 -0400

    chore: Added code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
   },
+  reporters: ['default', 'jest-junit'],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
