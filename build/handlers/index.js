90d1a127387988a929646151de67992330875635
Kai Curry <kai@cape.io>
2020-04-20 16:50:28 -0400

    fix(api): move error handler to handlers/index

--- build/handlers/index.js
+++ build/handlers/index.js
@@ -1,3 +1,4 @@
+const util = require('util')
 const _ = require('lodash/fp')
 const { getSheet } = require('./sheets')
 const getXml = require('./xml')
@@ -30,19 +31,22 @@ function fetchParseFix(resource) {
   const handler = fetch || handlers[app]
   if (!_.isFunction(handler))
     throw new Error(`Handler not found: ${resource.app}`)
-  return handler(resource)
-    .then(processResult(resource.fixItems))
-    .catch(errMsg => {
-      setTimeout(() => {
-        throw new Error(errMsg)
-      })
-    })
+  return handler(resource).then(processResult(resource.fixItems))
 }
+
+function exitError(err) {
+  console.error(
+    `\u001b[${util.inspect.colors.red[0]}mAPI ERROR\n${err}\u001b[${util.inspect.colors.red[1]}m`,
+  )
+  process.exit(1)
+}
+
 function fetchSave(resource) {
   const save = resource.createPages || defaultPage(resource.path)
   return fetchParseFix(resource)
     .then(save)
     .then(saveAll)
+    .catch(exitError)
 }
 
 module.exports = {
