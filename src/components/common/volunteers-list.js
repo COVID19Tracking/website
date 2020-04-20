e479eeb6322593e176a719a6803f57753b0a8396
Andrew Schwartz <andrew@aschwartz.me>
2020-04-20 09:57:12 -0600

    fix(voluntters-list): missing http/https prefix
    remove unnecessary return statement from module

--- src/components/common/volunteers-list.js
+++ src/components/common/volunteers-list.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import volunteersListStyles from './volunteers-list.module.scss'
 
-export default ({ items }) => {
-  return (
-    <ul className={volunteersListStyles.list}>
-      {items.map(({ node }) => (
-        <li>
-          {node.website ? (
-            <a href={node.website}>{node.name}</a>
-          ) : (
-            <span>{node.name}</span>
-          )}
-        </li>
-      ))}
-    </ul>
-  )
+function fixWebsitePrefix(website) {
+  if (!website.startsWith('http')) {
+    // includes both http and https
+    return `http://${website}`
+  }
+  return website
 }
+
+export default ({ items }) => (
+  <ul className={volunteersListStyles.list}>
+    {items.map(({ node }) => (
+      <li>
+        {node.website ? (
+          <a href={fixWebsitePrefix(node.website)}>{node.name}</a>
+        ) : (
+          <span>{node.name}</span>
+        )}
+      </li>
+    ))}
+  </ul>
+)
