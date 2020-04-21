c842a9d48f9a29e05fe514a81bb185d6f70d71f9
Kevin Miller <keveemiller@gmail.com>
2020-04-21 11:32:53 -0400

    fix: Unused components, fixed path to layout

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/layout'
-import GetInvolved from '../components/common/get-involved'
+import { graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const ContactPage = ({ data }) => (
   <Layout title="Contact" narrow textHeavy>
