aeb1ba6fba2e7d02c391c181c200c6155cc7915e
beep <me@ethanmarcotte.com>
2020-04-21 10:32:37 -0400

    fix(avoid passing visualization style as a prop)
    per https://github.com/COVID19Tracking/website/pull/638#discussion_r412059957

--- src/components/pages/homepage/visualization.js
+++ src/components/pages/homepage/visualization.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import DetailText from '../../common/detail-text'
 import { PublicationTitle, PublicationSource } from '../../common/publication'
+import visualizationStyles from './visualizations.module.scss'
 
-const Visualization = ({
-  image,
-  sourceLink,
-  itemClass,
-  sourceTitle,
-  publicationName,
-}) => (
-  <li className={itemClass}>
+const Visualization = ({ image, sourceLink, sourceTitle, publicationName }) => (
+  <li className={visualizationStyles.visualization}>
     <h3>
       <a href={sourceLink}>
         <img src={image} alt="" />
--- src/components/pages/homepage/visualizations.js
+++ src/components/pages/homepage/visualizations.js
@@ -16,21 +16,18 @@ const Visualizations = () => (
           sourceLink="https://www.nytimes.com/interactive/2020/03/17/us/coronavirus-testing-data.html"
           sourceTitle="U.S. Lags in Coronavirus Testing After Slow Response to Outbreak"
           publicationName="The New York Times"
-          itemClass={visualizationsStyles.visualization}
         />
         <Visualization
           image={politicoMap}
           sourceLink="https://www.politico.com/interactives/2020/coronavirus-testing-by-state-chart-of-new-cases/"
           sourceTitle="Coronavirus by State Map"
           publicationName="Politico"
-          itemClass={visualizationsStyles.visualization}
         />
         <Visualization
           image={wsjGraph}
           sourceLink="https://www.wsj.com/articles/how-the-cdcs-restrictive-testing-guidelines-hid-the-coronavirus-epidemic-11584882001"
           sourceTitle="Number of tests performed versus positive cases"
           publicationName="The Wall Street Journal"
-          itemClass={visualizationsStyles.visualization}
         />
       </ul>
     </Container>
