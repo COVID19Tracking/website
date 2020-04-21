346448e2fcabf21d66fa6251d709d10b3482e2e0
beep <me@ethanmarcotte.com>
2020-04-21 16:41:24 -0400

    fix(updating get-involved tests to `list-arrow`)

--- src/__tests__/components/common/list-arrow.js
+++ src/__tests__/components/common/list-arrow.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import GetInvolved from '../../../components/common/get-involved'
+import ListArrow from '../../../components/common/list-arrow'
 
 describe('Components : Common: Get Involved', () => {
   it('renders correctly', () => {
     const tree = renderer
-      .create(
-        <GetInvolved items={([<p>Sample text</p>], [<p>More text</p>])} />,
-      )
+      .create(<ListArrow items={([<p>Sample text</p>], [<p>More text</p>])} />)
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
