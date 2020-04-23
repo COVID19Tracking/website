b3546725a8c4c88276ebfd6e2486a027912fb6a4
Kai Curry <kai@cape.io>
2020-04-23 18:05:14 -0400

    Update states.js

--- build/datasources/states.js
+++ build/datasources/states.js
@@ -41,8 +41,8 @@ const grade = {
   ...sheets,
   worksheetId: '1_6zwoekv0Mzpp6KEp4OziZizaWxGOxMoDT2C-iBvyEg',
 //   worksheetId: '1MrjtmYpfKxzn0-oNEdxXzTtfifZ0ZMkMR7k9_WXtTPs',
-//   path: 'states/grades',
-  sheetName: 'Publishing',
+  path: 'states/grades',
+//   sheetName: 'Publishing',
   fixItems: _.flow(
     _.filter(x => x.state && x.grade),
     _.map(
