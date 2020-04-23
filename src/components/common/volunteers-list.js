db19d1d05dde6f1a19cdbfac39c69b16515601db
Kevin Miller <keveemiller@gmail.com>
2020-04-23 11:23:45 -0400

    chore: Added note about adding yourself

--- src/components/common/volunteers-list.js
+++ src/components/common/volunteers-list.js
@@ -10,15 +10,21 @@ function fixWebsitePrefix(website) {
 }
 
 export default ({ items }) => (
-  <ul className={volunteersListStyles.list}>
-    {items.map(({ node }) => (
-      <li key={`volunteer-${node.name}`}>
-        {node.website ? (
-          <a href={fixWebsitePrefix(node.website)}>{node.name}</a>
-        ) : (
-          <span>{node.name}</span>
-        )}
-      </li>
-    ))}
-  </ul>
+  <>
+    <ul className={volunteersListStyles.list}>
+      {items.map(({ node }) => (
+        <li key={`volunteer-${node.name}`}>
+          {node.website ? (
+            <a href={fixWebsitePrefix(node.website)}>{node.name}</a>
+          ) : (
+            <span>{node.name}</span>
+          )}
+        </li>
+      ))}
+    </ul>
+    <p className={volunteersListStyles.note}>
+      Are you a volunteer who wants to be added to this list? Edit your profile
+      in Slack and scroll down to &quot;Volunteer web page.&quot;
+    </p>
+  </>
 )
