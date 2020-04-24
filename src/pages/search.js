c7e663ccd07a93df5a1446581c3a0d38f5f63157
Michel Tomas <michel@kissmy.co>
2020-04-24 04:52:31 +0200

    WIP: add withLocation HOC, add search behavior to get results based on query string
    feat(search): add some styles & links for states/blogPosts results

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,24 +1,62 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'gatsby'
 import algoliasearch from 'algoliasearch'
 import { prefixSearchIndex } from '../utilities/algolia'
 import Layout from '../components/layout'
+import withLocation from '../components/utils/with-location'
+import pressListStyle from '../components/common/press-list.module.scss'
+import { PublicationTitle } from '../components/common/publication'
+import DetailText from '../components/common/detail-text'
 
-export default () => {
+export default withLocation(({ search }) => {
   const client = algoliasearch(
     process.env.GATSBY_ALGOLIA_APP_ID,
     process.env.GATSBY_ALGOLIA_SEARCH_KEY,
   )
 
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState(search.q || '')
   const [results, setResults] = useState({
-    states: [],
-    blogPosts: [],
+    states: {},
+    blogPosts: {},
   })
   const stateIndex = client.initIndex(prefixSearchIndex('states'))
   const blogIndex = client.initIndex(prefixSearchIndex('blog_posts'))
+
+  /* eslint-disable */
+  
+  async function queryIndex(index) {
+    try {
+      const hits = await index.search(query)
+      return hits
+    } catch (e) {
+      console.error(e)
+      return []
+    }
+  }
+  
+  useEffect(() => {
+    async function fetchResults() {
+      const [states, blogPosts] = await Promise.all([
+        queryIndex(stateIndex),
+        queryIndex(blogIndex),
+      ])
+
+      setResults({
+        states,
+        blogPosts,
+      })
+      console.log(states, blogPosts)
+    }
+    if (search.q) {
+      fetchResults()
+    }
+  }, [])
+
+  const totalHits = (results.states.nbHits || 0) + (results.blogPosts.nbHits || 0)
+  const hitsInfo = query.length ? ` : ${query} (${totalHits} result${totalHits === 1 ? '' : 's'})` : ''
   return (
     <Layout title="Search">
-      <h2>Search {query}</h2>
+      <h2 className="hed-primary">Search{hitsInfo}</h2>
       <form>
         <label htmlFor="item">
           Search
@@ -33,8 +71,9 @@ export default () => {
         </label>
         <button
           type="submit"
-          onClick={event => {
+          onClick={async event => {
             event.preventDefault()
+            await 
             stateIndex
               .search(query)
               .then(({ hits }) => {
@@ -60,6 +99,30 @@ export default () => {
           Search
         </button>
       </form>
+      <div id="searchResults">
+        {results.states.nbHits > 0 && <h3>States ({results.states.nbHits})</h3>}
+        {results.states.nbHits > 0 && results.states.hits.map(state => (
+          <div key={state.state}>
+            <PublicationTitle>
+              <Link to={state.slug}>{state.name}</Link>
+            </PublicationTitle>
+          </div>
+        ))}
+
+        {results.blogPosts.nbHits > 0 && <h3>Blog Posts ({results.blogPosts.nbHits})</h3>}
+        {results.blogPosts.nbHits > 0 && results.blogPosts.hits.map(post => (
+          <div key={post.objectId}>
+            <PublicationTitle>
+              <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+            </PublicationTitle>
+            <DetailText>
+              {post.author_name}
+              <span className={pressListStyle.dotSeparator}>•</span>
+              {post.publishDate}
+            </DetailText>
+          </div>
+        ))}
+      </div>
     </Layout>
   )
-}
+})
