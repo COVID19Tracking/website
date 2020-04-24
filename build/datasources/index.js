56c50e3014a218a2e3d8c667d1e3785f0198af7e
Kevin Miller <keveemiller@gmail.com>
2020-04-24 16:39:04 -0400

    chore: Revert

--- build/datasources/index.js
+++ build/datasources/index.js
@@ -1,2 +1,28 @@
+const { cdcTests, counties, press } = require('./sheets')
+const statesDaily = require('./statesDaily')
+const statesInfo = require('./statesInfo')
+const usCurrent = require('./usCurrent')
+const usDaily = require('./usDaily')
 const { grade, states } = require('./states')
-module.exports = [grade]
+const urls = require('./urls')
+// const { statesPop } = require('./census')
+const screenshots = require('./screenshots')
+const volunteers = require('./volunteers')
+
+const resources = [
+  cdcTests,
+  counties,
+  press,
+  screenshots,
+  states, // States Current
+  statesDaily,
+  statesInfo,
+  grade,
+  // statesPop, This needs to be static.
+  urls,
+  usCurrent,
+  usDaily,
+  volunteers,
+]
+
+module.exports = resources
