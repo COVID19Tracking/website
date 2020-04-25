099e89bd07900852354ddafd32a7a1e9ed6050d2
Michel Tomas <michel@kissmy.co>
2020-04-25 19:47:07 +0200

    feat(search): add page results with highlighted result

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
 import algoliasearch from 'algoliasearch'
+import marked from 'marked'
+import truncate from 'lodash/truncate'
 import { prefixSearchIndex } from '../utilities/algolia'
 import Layout from '../components/layout'
 import withLocation from '../components/utils/with-location'
@@ -18,9 +20,11 @@ export default withLocation(({ search }) => {
   const [results, setResults] = useState({
     states: {},
     blogPosts: {},
+    pages: {},
   })
   const stateIndex = client.initIndex(prefixSearchIndex('states'))
   const blogIndex = client.initIndex(prefixSearchIndex('blog_posts'))
+  const pageIndex = client.initIndex(prefixSearchIndex('pages'))
 
   /* eslint-disable */
   
@@ -33,27 +37,42 @@ export default withLocation(({ search }) => {
       return []
     }
   }
+
+  async function fetchResults() {
+    const [states, blogPosts, pages] = await Promise.all([
+      queryIndex(stateIndex),
+      queryIndex(blogIndex),
+      queryIndex(pageIndex),
+    ])
+
+    setResults({
+      states,
+      blogPosts,
+      pages,
+    })
+    console.log(states, blogPosts, pages)
+  }
+
+  function getHighlightResultOrExcerpt(hitType, hit) {
+    switch (hitType) {
+      case 'page':
+        return hit._highlightResult.body && hit._highlightResult.body.value 
+          ? marked(hit._highlightResult.body.value)
+          : marked(truncate(hit.body))
+    }
+  }
   
   useEffect(() => {
-    async function fetchResults() {
-      const [states, blogPosts] = await Promise.all([
-        queryIndex(stateIndex),
-        queryIndex(blogIndex),
-      ])
-
-      setResults({
-        states,
-        blogPosts,
-      })
-      console.log(states, blogPosts)
-    }
     if (search.q) {
       fetchResults()
     }
   }, [])
 
-  const totalHits = (results.states.nbHits || 0) + (results.blogPosts.nbHits || 0)
+  const totalHits = (results.states.nbHits || 0) + (results.blogPosts.nbHits || 0) + (results.pages.nbHits || 0)
   const hitsInfo = query.length ? ` : ${query} (${totalHits} result${totalHits === 1 ? '' : 's'})` : ''
+
+
+
   return (
     <Layout title="Search">
       <h2 className="hed-primary">Search{hitsInfo}</h2>
@@ -100,15 +119,17 @@ export default withLocation(({ search }) => {
         </button>
       </form>
       <div id="searchResults">
+        {/* State results */}
         {results.states.nbHits > 0 && <h3>States ({results.states.nbHits})</h3>}
         {results.states.nbHits > 0 && results.states.hits.map(state => (
           <div key={state.state}>
             <PublicationTitle>
-              <Link to={state.slug}>{state.name}</Link>
+              <Link to={`${state.slug}`}>{state.name}</Link>
             </PublicationTitle>
           </div>
         ))}
 
+        {/* Blog post results */}
         {results.blogPosts.nbHits > 0 && <h3>Blog Posts ({results.blogPosts.nbHits})</h3>}
         {results.blogPosts.nbHits > 0 && results.blogPosts.hits.map(post => (
           <div key={post.objectId}>
@@ -119,6 +140,22 @@ export default withLocation(({ search }) => {
               {post.author_name}
               <span className={pressListStyle.dotSeparator}>•</span>
               {post.publishDate}
+            </DetailText>
+          </div>
+        ))}
+
+        {/* Pages results */}
+        {results.pages.nbHits > 0 && <h3>Pages ({results.pages.nbHits})</h3>}
+        {results.pages.nbHits > 0 && results.pages.hits.map(page => (
+          <div key={page.objectID}>
+            <PublicationTitle>
+              {/* FIXME this should be handled during indexing (avoids external Link issues) */}
+              <Link to={`${page.slug[0] === '/' ? page.slug : '/' + page.slug}`}>{page.title}</Link>
+            </PublicationTitle>
+            <DetailText>
+              <div dangerouslySetInnerHTML={{
+                __html: getHighlightResultOrExcerpt('page', page),
+              }} />
             </DetailText>
           </div>
         ))}
