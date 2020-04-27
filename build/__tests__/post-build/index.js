9e9e88b169d17caacc6015ee211a6858afcad654
Kevin Miller <keveemiller@gmail.com>
2020-04-27 12:55:46 -0400

    chore: typo

--- build/__tests__/post-build/index.js
+++ build/__tests__/post-build/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
 
-describe('Build has occured', () => {
+describe('Build was successful', () => {
   it('has a data directory', () => {
     expect(fs.existsSync('_data/v1')).toBeTruthy()
   })
