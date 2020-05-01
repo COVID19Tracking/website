e9efa74f123ccfb834726682e752ab0104f8f221
Kevin Miller <keveemiller@gmail.com>
2020-05-01 13:01:03 -0400

    chore: Removed unused style

--- src/components/pages/blog/blog-teaser-list.js
+++ src/components/pages/blog/blog-teaser-list.js
@@ -1,13 +1,12 @@
 import React, { Fragment } from 'react'
 import { Link } from 'gatsby'
 import { Byline } from './byline'
-import blogTeaserListStyles from './blog-teaser-list.module.scss'
 
 export default ({ items }) => (
   <>
     {items.map(({ node }) => (
       <Fragment key={`blog-${node.slug}`}>
-        <h2 className={`hed-primary ${blogTeaserListStyles.postTitle}`}>
+        <h2 className={`hed-primary `}>
           <Link to={`/blog/${node.slug}`}>{node.title}</Link>
         </h2>
         <Byline authors={node.authors} date={node.publishDate} smallmargin />
