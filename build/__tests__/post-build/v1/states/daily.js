876205b6a7556a95ea4ede719c0300b05877de28
Kevin Miller <keveemiller@gmail.com>
2020-05-05 12:52:55 -0400

    chore: Add two new fields to spreadsheet

--- build/__tests__/post-build/v1/states/daily.js
+++ build/__tests__/post-build/v1/states/daily.js
@@ -34,6 +34,8 @@ describe('File : States : Daily', () => {
       negativeIncrease: expect.anything(),
       positiveIncrease: expect.anything(),
       totalTestResultsIncrease: expect.anything(),
+      lastUpdateEt: expect.anything(),
+      grade: expect.anything(),
     }
 
     fs.createReadStream('_data/v1/states/daily.csv')
