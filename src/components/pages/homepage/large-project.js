1f86affa03e27c24db7105b52f5a0b3a2abcd05e
Kevin Miller <keveemiller@gmail.com>
2020-05-06 17:11:33 -0400

    chore(content): Update name per #806

--- src/components/pages/homepage/large-project.js
+++ src/components/pages/homepage/large-project.js
@@ -37,15 +37,17 @@ export default () => {
         <h3>
           <Link to="/race">
             COVID-19 isn&apos;t affecting all communities equally. We&apos;ve
-            partnered with Anti-Racism Policy Center to collect the most
-            complete racial data anywhere in the COVID Racial Data Tracker.
+            partnered with the Antiracist Research &amp; Policy Center to
+            collect the most complete racial data anywhere in the COVID Racial
+            Data Tracker.
           </Link>
         </h3>
         <p>
           We&apos;re tracking racial and ethnic data from every state that
           reports it—and pushing those that don&apos;t to start. Together with
-          the Anti-Racism Policy Center, we&apos;re analyzing this data to
-          uncover the true impact of the outbreak on vulnerable communities.
+          the the Antiracist Research &amp; Policy Center, we&apos;re analyzing
+          this data to uncover the true impact of the outbreak on vulnerable
+          communities.
         </p>
         <CtaLink to="/race" centered>
           See the racial data tracker
