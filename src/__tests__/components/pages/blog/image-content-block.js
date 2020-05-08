76d5c0dd72567da39d542bb3bd57ae4a49d780c4
Andrew Schwartz <andrew@aschwartz.me>
2020-05-08 14:44:57 -0600

    test(image-content-block): add tests

---
+++ src/__tests__/components/pages/blog/image-content-block.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import ImageContentBlock from '~components/pages/blog/image-content-block'
+
+describe('Components : Pages : Blog : Image Content Block', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+    .create(
+      <ImageContentBlock
+        image={{
+          ['en-US']: {
+            fields: {
+              file: {
+                ['en-US']: {
+                  url: '//images.ctfassets.net/anexampleimage',
+                },
+                title: {
+                  ['en-US']: 'This is a test image title'
+                }
+              },
+              title: 'An Example Image',
+            }
+          }
+        }}
+        caption={{
+          ['en-US']: 'This is an image caption'
+        }}
+      />,
+      )
+    .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
