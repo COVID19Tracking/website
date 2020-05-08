f58e6e691a554a3bde692d875e2f1c6c5ed1cd47
Andrew Schwartz <andrew@aschwartz.me>
2020-05-08 11:05:11 -0600

    test(blog-image): add test, snpashot

---
+++ src/__tests__/components/pages/blog/blog-image.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import BlogImage from '~components/pages/blog/blog-image'
+
+describe('Components : Pages : Blog : Byline', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <BlogImage
+          imageSource={'//imagehosting.com/thisimage'}
+          imageAlt={'Test alt text'}
+          caption={'Author Designer / The COVID Tracking Project'}
+          containerStyle={'style-class'}
+        />,
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
