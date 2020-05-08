95e32d5e6628ada54be52f6f8f720b437aff4dcd
Andrew Schwartz <andrew@aschwartz.me>
2020-05-08 11:21:01 -0600

    test(blog-teaser-list): add test, snapshot

---
+++ src/__tests__/components/pages/blog/blog-teaser-list.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import BlogTeaserList from '~components/pages/blog/blog-teaser-list'
+
+const items = [
+  {
+    node: {
+      authors: [
+        {
+          name: 'test name',
+          twitterLink: 'https://twitter.com/test',
+          headshot: {
+            file: {
+              fileName: 'file.png',
+            },
+            resize: {
+              src: '//images.ctfassets.net/arandomfile',
+            },
+          },
+        },
+        {
+          name: 'test name II',
+          twitterLink: 'https://twitter.com/testii',
+          headshot: {
+            file: {
+              fileName: 'file2.png',
+            },
+            resize: {
+              src: '//images.ctfassets.net/aspecificfile',
+            },
+          },
+        },
+      ],
+      publishDate: "Aug 1 2020",
+      slug: "we-made-a-blog-post",
+      title: "We Made a Blog Post",
+      lede: {
+        lede: "Huh, this blog thing seems to be working! This is the lede."
+      }
+    }
+  },
+  {
+    node: {
+      authors: [
+        {
+          name: 'test name',
+          twitterLink: 'https://twitter.com/test',
+          headshot: {
+            file: {
+              fileName: 'file.png',
+            },
+            resize: {
+              src: '//images.ctfassets.net/arandomfile',
+            },
+          },
+        },
+        {
+          name: 'test name II',
+          twitterLink: 'https://twitter.com/testii',
+          headshot: {
+            file: {
+              fileName: 'file2.png',
+            },
+            resize: {
+              src: '//images.ctfassets.net/aspecificfile',
+            },
+          },
+        },
+      ],
+      publishDate: "Apr 15 2020",
+      slug: "new-blog-post",
+      title: "New Blog Post",
+      lede: {
+        lede: "This is a new blog post, not an old one."
+      }
+    }
+  }
+]
+
+describe('Components : Pages : Blog : Blog Teaser List', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<BlogTeaserList items={items} />,)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
