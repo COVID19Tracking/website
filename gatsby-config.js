bbccc03d11cf7006df4dc8d2dc753d1c078f3a60
Kevin Miller <keveemiller@gmail.com>
2020-05-11 19:19:10 -0400

    chore: Update contentful space

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,6 +128,7 @@ const gatsbyConfig = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE,
         accessToken: process.env.CONTENTFUL_TOKEN,
+        environment: 'sub-navigation',
       },
     },
     {
