4d592e5debcb91c5866d0017b189a5fe2e856b6d
Dana Sulit <dana.sulit@mapbox.com>
2020-05-11 01:43:28 -0400

    fix(tables): Style left border of column groups, add clarifying comments

--- src/components/common/summary-table.js
+++ src/components/common/summary-table.js
@@ -1,42 +1,128 @@
+/* eslint-disable */
 import React from 'react'
 import { Th, Td, Table } from './table'
 import { formatDateToString, FormatNumber } from '../utils/format'
 import Timezone from './timezone'
 
+// The following two functions are useful for rendering column group styling.
+
 const renderColumnHeaders = columnData =>
-  columnData.map(group =>
-    group.columns.map((column, i) => (
-      <Th header={group.header} showHeader={i === 0}>
-        {column}
+  columnData.map(group => {
+    if (group.isHidden) return <Th key={`${group.header}-th-hidden`} />
+    return group.columns.map((column, i) => (
+      <Th
+        key={`${group.header}-${column.header}-th`}
+        header={group.header}
+        isFirst={i === 0}
+        alignLeft={column.alignLeft || i === 0} // Left-align the column header underneath the group header
+      >
+        {column.header}
       </Th>
-    )),
-  )
+    ))
+  })
+
+const renderColumns = columnData =>
+  columnData.map(group => {
+    if (group.isHidden) return <Td key={`${group.header}-td-hidden`} />
+    return group.columns.map((column, i) => (
+      <Td
+        key={`${group.header}-${column.header}-td`}
+        isFirst={i === 0}
+        alignLeft={column.alignLeft}
+      >
+        <FormatNumber number={column.data} />
+      </Td>
+    ))
+  })
 
 export default ({ data, lastUpdated, showOutcomes = true }) => {
   const columns = [
     {
       header: 'Tests',
-      columns: ['Positive', 'Negative', 'Pending'],
+      columns: [
+        {
+          header: 'Positive',
+          alignLeft: !showOutcomes,
+          data: data.positive,
+        },
+        {
+          header: 'Negative',
+          alignLeft: !showOutcomes,
+          data: data.negative,
+        },
+        {
+          header: 'Pending',
+          alignLeft: !showOutcomes,
+          data: data.pending,
+        },
+      ],
     },
     {
       header: 'Hospitalized',
-      columns: ['Currently', 'Cumulative'],
+      isHidden: !showOutcomes,
+      columns: [
+        {
+          header: 'Currently',
+          data: data.hospitalizedCurrently,
+        },
+        {
+          header: 'Cumulative',
+          data: data.hospitalizedCumulative,
+        },
+      ],
     },
     {
       header: 'In ICU',
-      columns: ['Currently', 'Cumulative'],
+      isHidden: !showOutcomes,
+      columns: [
+        {
+          header: 'Currently',
+          data: data.inIcuCurrently,
+        },
+        {
+          header: 'Cumulative',
+          data: data.inIcuCumulative,
+        },
+      ],
     },
     {
       header: 'On Ventilator',
-      columns: ['Currently', 'Cumulative'],
+      isHidden: !showOutcomes,
+      columns: [
+        {
+          header: 'Currently',
+          data: data.onVentilatorCurrently,
+        },
+        {
+          header: 'Cumulative',
+          data: data.onVentilatorCumulative,
+        },
+      ],
     },
     {
       header: 'Outcomes',
-      columns: ['Recovered', 'Deaths'],
+      columns: [
+        {
+          header: 'Recovered',
+          alignLeft: !showOutcomes,
+          data: data.recovered,
+        },
+        {
+          header: 'Deaths',
+          alignLeft: !showOutcomes,
+          data: data.death,
+        },
+      ],
     },
     {
       header: 'Total Test Results',
-      columns: ['Positive + Negative'],
+      columns: [
+        {
+          header: 'Positive + Negative',
+          alignLeft: !showOutcomes,
+          data: data.totalTestResults,
+        },
+      ],
     },
   ]
 
@@ -54,48 +140,7 @@ export default ({ data, lastUpdated, showOutcomes = true }) => {
         <tr>{renderColumnHeaders(columns)}</tr>
       </thead>
       <tbody>
-        <tr>
-          <Td>
-            <FormatNumber number={data.positive} />
-          </Td>
-          <Td>
-            <FormatNumber number={data.negative} />
-          </Td>
-          <Td>
-            <FormatNumber number={data.pending} />
-          </Td>
-          {showOutcomes && (
-            <>
-              <Td>
-                <FormatNumber number={data.hospitalizedCurrently} />
-              </Td>
-              <Td>
-                <FormatNumber number={data.hospitalizedCumulative} />
-              </Td>
-              <Td>
-                <FormatNumber number={data.inIcuCurrently} />
-              </Td>
-              <Td>
-                <FormatNumber number={data.inIcuCumulative} />
-              </Td>
-              <Td>
-                <FormatNumber number={data.onVentilatorCurrently} />
-              </Td>
-              <Td>
-                <FormatNumber number={data.onVentilatorCumulative} />
-              </Td>
-              <Td>
-                <FormatNumber number={data.recovered} />
-              </Td>
-            </>
-          )}
-          <Td>
-            <FormatNumber number={data.death} />
-          </Td>
-          <Td>
-            <FormatNumber number={data.totalTestResults} />
-          </Td>
-        </tr>
+        <tr>{renderColumns(columns)}</tr>
       </tbody>
     </Table>
   )
--- src/components/common/table.js
+++ src/components/common/table.js
@@ -1,33 +1,46 @@
 /* eslint-disable jsx-a11y/aria-role */
+/*
+  This rule is disabled to allow role="text", which
+  prevents problems with text splitting on screen readers.
+  See https://axesslab.com/text-splitting/
+*/
 
 import React from 'react'
 import DetailText from './detail-text'
 import tableStyle from './table.module.scss'
 
-const Th = ({ children, alignLeft, colSpan, header, showHeader }) => (
-  <th
-    scope="col"
-    colSpan={colSpan}
-    className={alignLeft && tableStyle.alignLeft}
-  >
-    <span role="text">
-      {children}
-      {header && (
-        <span
-          className={
-            showHeader ? tableStyle.headerLabel : tableStyle.headerLabelHidden
-          }
-        >
-          {header}
-        </span>
-      )}
-    </span>
-  </th>
-)
+const Th = ({ children, colSpan, header, isFirst, alignLeft }) => {
+  let thClasses
+  if (alignLeft || isFirst)
+    thClasses = `${alignLeft ? tableStyle.alignLeft : ''} ${
+      isFirst ? tableStyle.borderLeft : ''
+    }`
+  return (
+    <th scope="col" colSpan={colSpan} className={thClasses}>
+      <span role="text">
+        {children}
+        {header && (
+          <span
+            className={
+              isFirst ? tableStyle.headerLabel : tableStyle.headerLabelHidden
+            }
+          >
+            {header}
+          </span>
+        )}
+      </span>
+    </th>
+  )
+}
 
-const Td = ({ children, alignLeft }) => (
-  <td className={alignLeft && tableStyle.alignLeft}>{children}</td>
-)
+const Td = ({ children, alignLeft, isFirst }) => {
+  let tdClasses
+  if (alignLeft || isFirst)
+    tdClasses = `${alignLeft ? tableStyle.alignLeft : ''} ${
+      isFirst ? tableStyle.borderLeft : ''
+    }`
+  return <td className={tdClasses}>{children}</td>
+}
 
 const Table = ({ children, tableLabel }) => (
   <div>
