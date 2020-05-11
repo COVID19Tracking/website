26d4708ddc44d6e60790e0ac6fdcbfdcaf497a36
jcrane <jcrane@partnercomm.net>
2020-05-11 11:40:47 -0500

    chore(subnav): Add better touch targets for toggling subnav

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -9,7 +9,6 @@ import projectLogo from '../../images/project-logo.svg'
 import atlanticLogo from '../../images/atlantic-logo.svg'
 import headerStyle from './header.module.scss'
 import searchIcon from '../../images/icons/search.svg'
-import navCaretIcon from '../../images/icons/nav-caret.svg'
 import searchIconInvert from '../../images/icons/search-inverted.svg'
 import Container from '../common/container'
 import colors from '../../scss/colors.module.scss'
@@ -93,7 +92,6 @@ const HeaderNavigation = () => {
 
   const ref = useRef()
   useOnClickOutside(ref, () => setShowSubMenu(menuState))
-  // Call hook passing in the ref and a function to call on outside click
 
   const toggleSubMenu = index => {
     setShowSubMenu(menuState === index ? '' : index)
@@ -105,42 +103,55 @@ const HeaderNavigation = () => {
   })
 
   return (
-    <nav className="js-disabled-block">
-      <ul>
+    <nav className="js-disabled-block" role="navigation">
+      <ul role="menubar">
         {data.allNavigationYaml.edges[0].node.items.map((item, index) => (
-          <li key={item.link} ref={ref}>
+          <li
+            role="menuitem"
+            aria-haspopup={item.subNavigation ? 'true' : false}
+            className={menuState === index ? headerStyle.showSubMenu : ''}
+            key={item.link}
+            ref={ref}
+          >
             <Link to={item.link}>{item.title}</Link>
             {item.subNavigation &&
               typeof subNavigation[item.subNavigation] !== 'undefined' && (
-                <SubNavigation items={subNavigation[item.subNavigation]} />
+                <>
+                  <ul>
+                    {subNavigation[item.subNavigation].map(subItem => (
+                      <li key={subItem.link}>
+                        <Link to={subItem.link}>{subItem.title}</Link>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <button
+                    className={headerStyle.navCaret}
+                    type="button"
+                    aria-expanded={menuState === index}
+                    aria-label={menuState === index ? 'hide' : 'show'}
+                    onClick={() => {
+                      toggleSubMenu(index)
+                    }}
+                  >
+                    <svg
+                      width="12"
+                      height="7"
+                      viewBox="0 0 12 7"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M5.59523 6.56387C5.7446 6.71053 5.91203 6.7381 6.0001 6.7381C6.09838 6.7381 6.25421 6.71136 6.40443 6.56387L10.9015 1.76778C11.1074 1.5483 11.093 1.20669 10.8695 1.00471C10.6459 0.802637 10.298 0.817297 10.0924 1.03625L5.99993 5.40006L1.90754 1.03625C1.70151 0.816664 1.35357 0.802742 1.13034 1.00471C0.906907 1.20669 0.892512 1.5483 1.09823 1.76778L5.59523 6.56387Z" />
+                    </svg>
+                  </button>
+                </>
               )}
-            <button
-              className={headerStyle.navCaret}
-              type="button"
-              aria-expanded={menuState === index}
-              aria-label={menuState === index ? 'hide' : 'show'}
-              onClick={() => {
-                toggleSubMenu(index)
-              }}
-            >
-              <img src={navCaretIcon} alt="" />
-            </button>
           </li>
         ))}
       </ul>
     </nav>
   )
 }
-
-const SubNavigation = ({ items }) => (
-  <ul>
-    {items.map(item => (
-      <li key={item.link}>
-        <Link to={item.link}>{item.title}</Link>
-      </li>
-    ))}
-  </ul>
-)
 
 const HeaderSearch = ({ children }) => {
   const [searchState] = useSearch()
