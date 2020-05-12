e49df9ae8705dd85c50f80ec729a004110dcc8c8
Kevin Miller <keveemiller@gmail.com>
2020-05-12 17:02:45 -0400

    feat(content): Added table support

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,6 +6,7 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import AuthorFooter from '../components/pages/blog/author-footer'
 import Categories from '../components/pages/blog/categories'
 import ImageContentBlock from '../components/pages/blog/image-content-block'
+import TableContentBlock from '../components/pages/blog/table-content-block'
 import Layout from '../components/layout'
 import Lede from '../components/pages/blog/blog-lede'
 
@@ -14,8 +15,25 @@ import blogPostStyles from './blog-post.module.scss'
 const options = {
   renderNode: {
     [BLOCKS.EMBEDDED_ENTRY]: node => {
-      const { image, caption } = node.data.target.fields
-      return <ImageContentBlock image={image} caption={caption} />
+      if (typeof node.data.target.fields === 'undefined') {
+        return null
+      }
+      if (
+        node.data.target.sys.contentType.sys.contentful_id ===
+        'contentBlockTable'
+      ) {
+        return (
+          <TableContentBlock table={node.data.target.fields.table['en-US']} />
+        )
+      }
+      if (
+        node.data.target.sys.contentType.sys.contentful_id ===
+        'contentBlockImage'
+      ) {
+        const { image, caption } = node.data.target.fields
+        return <ImageContentBlock image={image} caption={caption} />
+      }
+      return null
     },
   },
 }
