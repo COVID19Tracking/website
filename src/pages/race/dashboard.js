c9181879cf61b6f3c58fa4c34b82fc579986fd08
Andrew Schwartz <andrew@aschwartz.me>
2020-05-13 19:17:37 -0600

    feat(race): add NoData component

--- src/pages/race/dashboard.js
+++ src/pages/race/dashboard.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
 
+import NoData from '~components/pages/race/no-data'
+
 export default ({ data }) => (
   <Layout
     title="Racial Data dashboard"
     description="The COVID-19 pandemic isn’t affecting all communities the same way. The COVID Racial Data Dashboard helps us track this inequity by publishing topline racial data compared with state demographic data."
   >
+    <NoData stateName="North Dakota" />
     {data.allCovidStateInfo.edges.map(({ node }) => (
       <h3>{node.name}</h3>
     ))}
