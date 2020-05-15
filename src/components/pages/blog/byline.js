0e78029484b0d01e8ecc2736f8fca4f0ecf2730e
Andrew Schwartz <andrew@aschwartz.me>
2020-05-14 18:32:13 -0600

    chore(byline): revert byline text to p element

--- src/components/pages/blog/byline.js
+++ src/components/pages/blog/byline.js
@@ -77,13 +77,13 @@ const Byline = ({ authors, date, smallmargin = false }) => {
           ))}
         </div>
       )}
-      <div className={bylineStyles.bylineText}>
+      <p className={bylineStyles.bylineText}>
         <span className={bylineStyles.spacer}>By </span>
         <span className={bylineStyles.author}>
           <AuthorsText authors={authors} />
         </span>
         <span className={bylineStyles.date}>{date}</span>
-      </div>
+      </p>
     </div>
   )
 }
