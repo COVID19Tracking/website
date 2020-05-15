06426d70c52e406fd843ff01ece4ded13ceef22f
Kevin Miller <keveemiller@gmail.com>
2020-05-15 07:36:55 -0400

    chore: Add promise, error reporting in plugin

--- plugins/gatsby-source-covid-tracking-counties/gatsby-node.js
+++ plugins/gatsby-source-covid-tracking-counties/gatsby-node.js
@@ -29,57 +29,64 @@ const formatCountyDate = county => {
   }
 }
 
-exports.sourceNodes = async (
-  { actions, createNodeId, reporter },
-  configOptions,
-) => {
-  const { createNode } = actions
-  const countyResults = {}
-  reporter.info('Fetching county data')
-  const demographics = require(configOptions.demographics)
-  const results = await fetch(configOptions.nytimesUrl).then(response =>
-    response.text(),
-  )
+exports.sourceNodes = ({ actions, createNodeId, reporter }, configOptions) => {
+  return new Promise((resolve, reject) => {
+    const { createNode } = actions
+    const countyResults = {}
+    const demographics = require(configOptions.demographics)
+    fetch(configOptions.nytimesUrl)
+      .then(response => response.text())
+      .then(results => {
+        const counties = parse(results, {
+          columns: true,
+          skip_empty_lines: true,
+        })
+        counties.forEach(county => {
+          if (typeof countyResults[county.fips] === 'undefined') {
+            countyResults[county.fips] = {
+              name: county.county,
+              state: county.state,
+              fips: county.fips,
+              current: formatCountyDate(county),
+              demographics: cleanKeys(
+                demographics.find(element => element.FIPS === county.fips),
+              ),
+              history: [],
+            }
+          }
+          countyResults[county.fips].history.push(formatCountyDate(county))
+          if (county.date > countyResults[county.fips].current.date) {
+            countyResults[county.fips].current = formatCountyDate(county)
+          }
+        })
+        Object.keys(countyResults).forEach(fips => {
+          const county = countyResults[fips]
+          const digest = crypto
+            .createHash('md5')
+            .update(JSON.stringify(county))
+            .digest('hex')
+          const nodeTemplate = {
+            id: createNodeId(`${configOptions.type}.${fips}`),
+            children: [],
+            parent: null,
+            internal: {
+              type: configOptions.type,
+              contentDigest: digest,
+            },
+          }
 
-  const counties = parse(results, {
-    columns: true,
-    skip_empty_lines: true,
+          createNode({ ...county, ...nodeTemplate })
+        })
+        if (Object.keys(countyResults).length < 1000) {
+          reject('There were not enough counties fetched')
+        }
+        reporter.success(
+          `Imported ${Object.keys(countyResults).length} counties`,
+        )
+        resolve()
+      })
+      .catch(error => {
+        reject(`Could not fetch counties: ${error}`)
+      })
   })
-  counties.forEach(county => {
-    if (typeof countyResults[county.fips] === 'undefined') {
-      countyResults[county.fips] = {
-        name: county.county,
-        state: county.state,
-        fips: county.fips,
-        current: formatCountyDate(county),
-        demographics: cleanKeys(
-          demographics.find(element => element.FIPS === county.fips),
-        ),
-        history: [],
-      }
-    }
-    countyResults[county.fips].history.push(formatCountyDate(county))
-    if (county.date > countyResults[county.fips].current.date) {
-      countyResults[county.fips].current = formatCountyDate(county)
-    }
-  })
-  Object.keys(countyResults).forEach(fips => {
-    const county = countyResults[fips]
-    const digest = crypto
-      .createHash('md5')
-      .update(JSON.stringify(county))
-      .digest('hex')
-    const nodeTemplate = {
-      id: createNodeId(`${configOptions.type}.${fips}`),
-      children: [],
-      parent: null,
-      internal: {
-        type: configOptions.type,
-        contentDigest: digest,
-      },
-    }
-
-    createNode({ ...county, ...nodeTemplate })
-  })
-  reporter.info(`Imported ${countyResults.length} counties`)
 }
