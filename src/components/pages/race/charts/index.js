be28f9e378f7cc5221179c5a80b9cad0c71f27ee
Kevin Miller <keveemiller@gmail.com>
2020-05-19 16:25:00 -0400

    chore: Add disclaimer support to both charts

--- src/components/pages/race/charts/index.js
+++ src/components/pages/race/charts/index.js
@@ -30,6 +30,22 @@ const numberToWord = [
 const numberWords = number =>
   typeof numberToWord[number] === 'undefined' ? number : numberToWord[number]
 
+const DisclaimerNY = ({ data }) => (
+  <>
+    {data.find(county => county.name === 'New York City') && (
+      <span className={chartsStyle.nycDisclaimer}>
+        Note that <em>The New York Times</em> county dataset combines the five
+        counties that make up New York City&amp;s boroughs (New York County,
+        Kings County, Bronx County, Richmond County, and Queens County) into one
+        listing for New York City. When combined, these counties show up as
+        predominantly White, non-Hispanic. However, Bronx County, which is
+        predominantly Hispanic/Latino and/or Black, has been a hotspot for COVID
+        cases and deaths.
+      </span>
+    )}
+  </>
+)
+
 export default () => {
   const [isCasesOpen, setIsCasesOpen] = useState(false)
   const [isDeathsOpen, setIsDeathsOpen] = useState(false)
@@ -110,6 +126,7 @@ export default () => {
             group in most of these counties. This is in line with Census
             statistics, which show that more than 60 percent of Americans are
             White, non-Hispanic or Latino.
+            <DisclaimerNY data={countiesByCases} />
             <DisclosureButton className={chartsStyle.showChartData}>
               {isCasesOpen ? (
                 <>
@@ -159,22 +176,7 @@ export default () => {
             counties, Black people represent the largest racial group.{' '}
             {numberWords(topRepresented)} of the top five counties with the
             highest death rates in the nation are all predominantly Black.
-            {countiesByDeaths.find(
-              county => county.name === 'New York City',
-            ) && (
-              <>
-                <br />
-                <br />
-                Note that <em>The New York Times</em> county dataset combines
-                the five counties that make up New York City&apos;s boroughs
-                (New York County, Kings County, Bronx County, Richmond County,
-                and Queens County) into one listing for New York City. When
-                combined, these counties show up as predominantly White,
-                non-Hispanic. However, Bronx County, which is predominantly
-                Hispanic/Latino and/or Black, has been a hotspot for COVID
-                deaths.
-              </>
-            )}
+            <DisclaimerNY data={countiesByDeaths} />
             <DisclosureButton className={chartsStyle.showChartData}>
               {isDeathsOpen ? (
                 <>
