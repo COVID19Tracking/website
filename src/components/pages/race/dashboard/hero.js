6d498d6e43dde9ee1acc90eb91d19ce3cc5c07d3
Kevin Miller <keveemiller@gmail.com>
2020-05-20 17:29:06 -0400

    chore: Missing apos

--- src/components/pages/race/dashboard/hero.js
+++ src/components/pages/race/dashboard/hero.js
@@ -4,8 +4,8 @@ import HeroStyles from './hero.module.scss'
 export default ({ ledeContent }) => (
   <div className={HeroStyles.container}>
     <h1>
-      Here's the latest race and ethnicity data from every state and territory
-      that reports it.
+      Here&apos;s the latest race and ethnicity data from every state and
+      territory that reports it.
     </h1>
     <div
       dangerouslySetInnerHTML={{
