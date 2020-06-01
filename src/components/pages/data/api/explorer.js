8821de0d4e82e32c55ed1d6928f31f3b2506b5f0
Kevin Miller <keveemiller@gmail.com>
2020-06-01 14:25:17 -0400

    feat: Use built-in API docs

--- src/components/pages/data/api/explorer.js
+++ src/components/pages/data/api/explorer.js
@@ -1,10 +1,73 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@reach/disclosure'
+import explorerStyles from './explorer.module.scss'
 import definition from '../../../../../_api/v1/openapi.json'
 
-const Paths = ({ tag }) => {
-  return <p>{tag}</p>
+const Fields = ({ schema }) => {
+  const fields =
+    definition.components.schemas[schema.split('/').pop()].properties
+
+  return (
+    <ul>
+      {Object.keys(fields).map(property => (
+        <li>
+          <strong>
+            <code>{property}</code>
+          </strong>{' '}
+          - {fields[property].description}
+        </li>
+      ))}
+    </ul>
+  )
 }
+
+const Tag = ({ tag }) => (
+  <>
+    <h2>{tag}</h2>
+    {Object.keys(definition.paths).map(path => (
+      <>
+        {definition.paths[path].get.tags.indexOf(tag) > -1 && (
+          <Disclosure>
+            <DisclosureButton className={explorerStyles.button}>
+              <h3>{definition.paths[path].get.description}</h3>
+            </DisclosureButton>
+            <DisclosurePanel className={explorerStyles.panel}>
+              <p>
+                <strong>JSON URL:</strong>{' '}
+                <code>
+                  <a href={path.replace('{format}', 'json')}>
+                    {path.replace('{format}', 'json')}
+                  </a>
+                </code>
+              </p>
+              <p>
+                <strong>CSV URL:</strong>{' '}
+                <code>
+                  <a href={path.replace('{format}', 'json')}>
+                    {path.replace('{format}', 'json')}
+                  </a>
+                </code>
+              </p>
+              <h4>Fields</h4>
+              <Fields
+                schema={
+                  definition.paths[path].get.responses[200].content[
+                    'application/json'
+                  ].schema.items.$ref
+                }
+              />
+            </DisclosurePanel>
+          </Disclosure>
+        )}
+      </>
+    ))}
+  </>
+)
 
 export default () => {
   const data = useStaticQuery(graphql`
@@ -17,7 +80,6 @@ export default () => {
     }
   `)
   const { hiddenApiTags } = data.site.siteMetadata
-  console.log(definition)
   const tags = []
   Object.keys(definition.paths).forEach(path => {
     if (definition.paths[path].get.tags) {
@@ -31,10 +93,7 @@ export default () => {
   return (
     <>
       {tags.map(tag => (
-        <>
-          <h2>{tag}</h2>
-          <Paths tag={tag} />
-        </>
+        <Tag key={tag} tag={tag} />
       ))}
     </>
   )
