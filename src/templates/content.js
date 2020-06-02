3d6aa0c1d17038e9fb6de10c58e643d7d61dd9ff
Andrew Schwartz <andrew@aschwartz.me>
2020-06-02 13:00:31 -0600

    fix: description query on content pages

--- src/templates/content.js
+++ src/templates/content.js
@@ -56,7 +56,7 @@ export const query = graphql`
           }
           socialCard {
             description {
-              id
+              description
             }
             image {
               resize(width: 1200) {
