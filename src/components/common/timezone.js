f5e37f97d2dfcc80b9882f648f762c089a66f914
Kevin Miller <keveemiller@gmail.com>
2020-06-02 09:25:23 -0400

    feat: Use API build time for data update

--- src/components/common/timezone.js
+++ src/components/common/timezone.js
@@ -1,23 +1,9 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
 
 export default () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          inDST
-        }
-      }
-    }
-  `)
-  const title = data.site.siteMetadata.inDST
-    ? 'Eastern Daylight Time'
-    : 'Eastern Standard Time'
-  const abbreviation = data.site.siteMetadata.inDST ? 'EDT' : 'EST'
   return (
-    <abbr title={title} aria-label={title}>
-      {abbreviation}
+    <abbr title="Eastern Time" aria-label="Eastern Time">
+      ET
     </abbr>
   )
 }
