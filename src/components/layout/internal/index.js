1e4c4136249ebc33ba6dcb386d74602e9ee7a07a
Kevin Miller <keveemiller@gmail.com>
2020-06-05 11:10:24 -0400

    chore: Remove auth wrapper

--- src/components/layout/internal/index.js
+++ src/components/layout/internal/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Container from '~components/common/container'
+import DevelopmentWarning from '~components/layout/header/development-warning'
 import projectLogo from '~images/project-logo.svg'
 import layoutStyles from './internal-layout.module.scss'
 
 export default ({ title, children }) => (
   <>
     <Helmet title="Internal" />
+    <DevelopmentWarning />
     <header className={layoutStyles.header}>
       <Container>
         <div className={layoutStyles.container}>
