802fb3ab0b6042178c0803b173447e79ce740af0
Kevin Miller <keveemiller@gmail.com>
2020-06-05 10:43:10 -0400

    chore: Add links to API download

--- src/components/pages/internal/state/api-preview.js
+++ src/components/pages/internal/state/api-preview.js
@@ -14,6 +14,23 @@ export default ({ state, date }) => {
   return (
     <section>
       <h2>API</h2>
+      <p>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={`/api/v1/states/${state.toLowerCase()}/${date}.json`}
+        >
+          Raw JSON file
+        </a>{' '}
+        |{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={`/api/v1/states/${state.toLowerCase()}/${date}.csv`}
+        >
+          Download CSV
+        </a>
+      </p>
       <code>
         <pre>{apiResult || 'Loading...'}</pre>
       </code>
