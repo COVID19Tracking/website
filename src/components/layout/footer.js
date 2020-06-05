e5bdace98ac514e60f60611ae141755658be1772
Kevin Miller <keveemiller@gmail.com>
2020-06-05 13:13:02 -0400

    chore: Add link to T&C

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -19,6 +19,9 @@ const Footer = () => (
             <Link to="/accessibility">Accessibility</Link>
           </li>
           <li>
+            <Link to="/terms-and-conditions">Terms and Conditions</Link>
+          </li>
+          <li>
             <Link to="/license">License</Link>
           </li>
           <li>
