d353b49a9a0587b2d474f9b90a951c9585f4ac91
Backup <tiagofsanchez>
2020-06-09 14:58:21 +0800

    form design(suggestion): suggestion for the from design for review

--- src/components/common/form.js
+++ src/components/common/form.js
@@ -7,7 +7,6 @@ const Form = ({ children }) => <div className={formStyles.form}>{children}</div>
 const FormGroup = ({ children, className }) => (
   <div className={classnames([formStyles.group, className])}>{children}</div>
 )
-
 const FormLabel = ({ children, htmlFor, required }) => (
   <label className={formStyles.label} htmlFor={htmlFor}>
     {children}
@@ -15,4 +14,33 @@ const FormLabel = ({ children, htmlFor, required }) => (
   </label>
 )
 
-export { Form, FormGroup, FormLabel }
+const FormInput = props => {
+  const { inputType, htmlFor, required, label } = props
+
+  let inputElement = null
+  switch (inputType) {
+    case 'text':
+      inputElement = <textarea {...props} />
+      break
+    case 'input':
+      inputElement = (
+        <>
+          <label className={formStyles.label} htmlFor={htmlFor}>
+            {label}
+            {required ? (
+              <span className={formStyles.required}>required</span>
+            ) : (
+              <span className={formStyles.required}>optional</span>
+            )}
+          </label>
+          <input {...props} />
+        </>
+      )
+      break
+    default:
+      inputElement = <input {...props} />
+  }
+  return inputElement
+}
+
+export { Form, FormGroup, FormInput, FormLabel }
