692f35c5cc8d1551700758bb51a8349c54cc3e17
Backup <tiagofsanchez>
2020-06-13 22:58:02 +0800

    form design(Vol getInv styling): changes to Volunteer, get Involved and styling

--- src/components/pages/contact/volunteer-form.js
+++ src/components/pages/contact/volunteer-form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, FormGroup, FormInput } from '~components/common/form'
+import { Form, FormInput } from '~components/common/form'
 import { Button } from '~components/common/button'
 import VolunteerFormStyles from './volunteer-form.module.scss'
 import {
@@ -13,142 +13,121 @@ export default () => {
   return (
     <Form>
       <form
-        className={VolunteerFormStyles.contactForm}
         method="POST"
         name="volunteer"
         action="/contact/volunteer-success"
         netlify-honeypot="covid-bot-field"
         data-netlify="true"
       >
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            htmlFor="name"
-            label="Name"
-            inputtype="input"
-            type="text"
-            name="name"
-            id="name"
-            aria-required="true"
-            isRequired
-          />
-        </FormGroup>
+        <FormInput
+          htmlFor="name"
+          label="Name"
+          inputtype="input"
+          type="text"
+          name="name"
+          id="name"
+          aria-required="true"
+          isRequired
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            htmlFor="email"
-            label="Email"
-            inputtype="inputWithDetail"
-            type="email"
-            name="email"
-            id="email"
-            aria-required="true"
-            isRequired
-            detailText="If possible, this should be a Gmail or Google-linked address,
+        <FormInput
+          htmlFor="email"
+          label="Email"
+          inputtype="inputWithDetail"
+          type="email"
+          name="email"
+          id="email"
+          aria-required="true"
+          isRequired
+          detailText="If possible, this should be a Gmail or Google-linked address,
               since we rely heavily on Google Docs and Sheets. We will show your
               email internally to other volunteers but will not share it
               elsewhere."
-          />
-        </FormGroup>
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            htmlFor="url"
-            label="URL"
-            inputtype="inputWithDetail"
-            type="text"
-            name="url"
-            id="url"
-            detailText="Personal website, LinkedIn, or other website that will tell us
+        <FormInput
+          htmlFor="url"
+          label="URL"
+          inputtype="inputWithDetail"
+          type="text"
+          name="url"
+          id="url"
+          detailText="Personal website, LinkedIn, or other website that will tell us
               about you."
-          />
-        </FormGroup>
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            htmlFor="hours"
-            label="About how many hours are you available to volunteer each week?"
-            inputtype="select"
-            name="hours"
-            id="hours"
-            optionsArray={HoursPerWeek}
-            aria-required="true"
-            isRequired
-            required
-          />
-        </FormGroup>
+        <FormInput
+          htmlFor="hours"
+          label="About how many hours are you available to volunteer each week?"
+          inputtype="select"
+          name="hours"
+          id="hours"
+          optionsArray={HoursPerWeek}
+          aria-required="true"
+          isRequired
+          required
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            htmlFor="htimezone"
-            label="What time zone are you in?"
-            inputtype="select"
-            name="timezone"
-            id="timezone"
-            aria-required="true"
-            optionsArray={Timezone}
-            isRequired
-            required
-          />
-        </FormGroup>
+        <FormInput
+          htmlFor="htimezone"
+          label="What time zone are you in?"
+          inputtype="select"
+          name="timezone"
+          id="timezone"
+          aria-required="true"
+          optionsArray={Timezone}
+          isRequired
+          required
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            label="When are you most available to volunteer?"
-            isRequired
-            inputtype="fieldset"
-            optionsArray={Availability}
-            required
-            type="checkbox"
-            name="availability"
-          />
-        </FormGroup>
+        <FormInput
+          label="When are you most available to volunteer?"
+          isRequired
+          inputtype="fieldset"
+          optionsArray={Availability}
+          required
+          type="checkbox"
+          name="availability"
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            htmlFor="skills"
-            label="Specializations / Skills"
-            inputtype="inputWithDetail"
-            type="text"
-            name="skills"
-            id="skills"
-            aria-required="true"
-            isRequired
-            detailText="Examples: Python, SQL, Tableau, data viz, editing, social media,
+        <FormInput
+          htmlFor="skills"
+          label="Specializations / Skills"
+          inputtype="inputWithDetail"
+          type="text"
+          name="skills"
+          id="skills"
+          aria-required="true"
+          isRequired
+          detailText="Examples: Python, SQL, Tableau, data viz, editing, social media,
               public health, research, journalism, etc."
-          />
-        </FormGroup>
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            label="What would you like to volunteer to do?"
-            isRequired
-            inputtype="fieldset"
-            optionsArray={RadioOptions}
-            required
-            type="radio"
-            name="workstream"
-          />
-        </FormGroup>
+        <FormInput
+          label="What would you like to volunteer to do?"
+          isRequired
+          inputtype="fieldset"
+          optionsArray={RadioOptions}
+          required
+          type="radio"
+          name="workstream"
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            label="What kinds of projects or tasks would you most like to help with?"
-            inputtype="text"
-            name="why"
-            id="why"
-            rows="5"
-          />
-        </FormGroup>
+        <FormInput
+          label="What kinds of projects or tasks would you most like to help with?"
+          inputtype="text"
+          name="why"
+          id="why"
+          rows="5"
+        />
 
-        <FormGroup className={VolunteerFormStyles.group}>
-          <FormInput
-            label="I was referred by ..."
-            inputtype="input"
-            type="text"
-            id="referred"
-            name="referred"
-          />
-        </FormGroup>
+        <FormInput
+          label="I was referred by ..."
+          inputtype="input"
+          type="text"
+          id="referred"
+          name="referred"
+        />
 
         <div className={VolunteerFormStyles.botField}>
           <label htmlFor="covid-bot-field">
