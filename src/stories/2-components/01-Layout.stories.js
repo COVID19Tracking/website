72597c034db4617e347128e95126aae4685a5505
Kevin Miller <keveemiller@gmail.com>
2020-06-18 10:34:24 -0600

    chore: Add SCSS mixin

--- src/stories/2-components/01-Layout.stories.js
+++ src/stories/2-components/01-Layout.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import classnames from 'classnames'
 import Container from '~components/common/container'
 import spacers from './spacers.module.scss'
+import grid from './grid.module.scss'
 
 export default {
   title: 'Layout',
@@ -80,3 +81,44 @@ containerNarrow.story = {
     },
   },
 }
+
+export const gridWithSass = () => (
+  <div>
+    <p>
+      Use the <code>row()</code> and <code>col()</code> mixins to add grids to
+      your components. The <code>col()</code> accepts three widths that need to
+      all be set: <code>small medium large</code>.
+    </p>
+
+    <p>
+      To control the padding of each column, the second argument is a list of
+      spacers for the left padding, and the third argument is a list of spacers
+      for the right padding.
+    </p>
+    <div className={grid.row}>
+      <div className={grid.half}>
+        <div className={grid.col}>col(2 3 6)</div>
+      </div>
+      <div className={grid.quarter}>
+        <div className={grid.col}>col(1 2 3)</div>
+      </div>
+      <div className={grid.quarter}>
+        <div className={grid.col}>col(1 2 3)</div>
+      </div>
+    </div>
+
+    <div className={grid.row}>
+      <div className={grid.twoThirds}>
+        <div className={grid.col}>col(4 3 9)</div>
+      </div>
+      <div className={grid.oneThird}>
+        <div className={grid.col}>col(4 3 4, 8 16 32)</div>
+      </div>
+    </div>
+    <div className={grid.row}>
+      <div className={grid.full}>
+        <div className={grid.col}>col(4 6 12)</div>
+      </div>
+    </div>
+  </div>
+)
