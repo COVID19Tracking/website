1fec7260f833ab0a8ffc586cfb0a0aaf55cf9f97
Kevin Miller <keveemiller@gmail.com>
2020-06-22 20:05:57 -0600

    fix(design): Make hero full width

--- src/components/pages/homepage/header-hero.js
+++ src/components/pages/homepage/header-hero.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import Container from '~components/common/landing-page/container'
-import HeroHeader from '~components/common/landing-page/hero/header'
 import heroStyle from './header-hero.module.scss'
 
 export default () => {
   return (
     <div className={`hero ${heroStyle.hero}`}>
       <Container>
-        <HeroHeader>
-          The public deserves the most complete data available about COVID-19 in
-          the{' '}
+        <h2>
+          The public deserves the most complete data available about{' '}
+          <span className="nowrap">COVID-19</span> in the{' '}
           <abbr title="United States" aria-label="United States">
             US
           </abbr>
           . No official source is providing it,{' '}
           <span className="nowrap">so we are</span>.
-        </HeroHeader>
+        </h2>
       </Container>
     </div>
   )
