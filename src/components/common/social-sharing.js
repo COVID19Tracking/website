ad16f87976d81f3313cfa4c9cca31f825149d129
Andrew Schwartz <andrew@aschwartz.me>
2020-06-26 16:43:28 -0600

    chore: add onClick behavior for link sharing

--- src/components/common/social-sharing.js
+++ src/components/common/social-sharing.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import copy from 'copy-to-clipboard'
+
 import facebookIcon from '~images/social-shares/facebook.svg'
 import twitterIcon from '~images/social-shares/twitter.svg'
 import linkIcon from '~images/social-shares/link.svg'
+
 import socialSharingStyles from './social-sharing.module.scss'
 
 export default ({ shares, url, text }) => {
+  const copyUrl = () => copy(url)
   const types = {
     facebook: {
       icon: facebookIcon,
@@ -16,12 +20,17 @@ export default ({ shares, url, text }) => {
       alt: 'Share on Twitter',
       url: `https://twitter.com/intent/tweet?url=${url}&text=${text}`,
     },
-    link: { icon: linkIcon, alt: 'Copy link' }, // todo configure onClick
+    link: { icon: linkIcon, alt: 'Copy link', url: '#', onClick: copyUrl },
+    // todo include copy success message/icon change
   }
   return (
     <div className={socialSharingStyles.wrapper}>
       {shares.map(share => (
-        <a href={types[share].url} className={socialSharingStyles.socialButton}>
+        <a
+          href={types[share].url}
+          onClick={types[share].onClick}
+          className={socialSharingStyles.socialButton}
+        >
           <img src={types[share].icon} alt={types[share].alt} />
         </a>
       ))}
