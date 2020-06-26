1ac28acc2337025d5a7815eabfb9482806ba4118
Andrew Schwartz <andrew@aschwartz.me>
2020-06-26 16:22:28 -0600

    chore(container): accept className prop

--- src/components/common/container.js
+++ src/components/common/container.js
@@ -2,9 +2,11 @@ import React from 'react'
 import classnames from 'classnames'
 import containerStyles from './container.module.scss'
 
-export default ({ children, narrow, centered }) => {
+export default ({ children, narrow, centered, className }) => {
   return (
-    <div className={classnames('container', containerStyles.container)}>
+    <div
+      className={classnames('container', containerStyles.container, className)}
+    >
       <div
         className={classnames(
           centered && containerStyles.centered,
