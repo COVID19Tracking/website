26b3a9a57311ad033bc1d5054e1af5dfa3b258bb
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-06-30 16:38:37 +0800

    feat(navigation): implemented navigation functionality for the blog, need to work on styling

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -1,14 +1,49 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from '~components/layout'
 import BlogTeaserList from '~components/pages/blog/blog-teaser-list'
 
-export default ({ data }) => {
+export default ({ data, pageContext }) => {
   /* eslint-disable  no-console */
-  console.log(data)
+
+  const { currentPage, numPages } = pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage =
+    currentPage - 1 === 1 ? '/blog' : `/blog/${(currentPage - 1).toString()}`
+  const nextPage = `/blog/${(currentPage + 1).toString()}`
   return (
     <Layout title="Blog" path="/blog" narrow>
       <BlogTeaserList items={data.allContentfulBlogPost.nodes} />
+      {!isFirst && (
+        <Link to={prevPage} rel="prev">
+          ← Previous Page
+        </Link>
+      )}
+      {Array.from({ length: numPages }, (_, i) => (
+        <li
+          key={`pagination-number${i + 1}`}
+          style={{
+            margin: 0,
+          }}
+        >
+          <Link
+            to={`/blog/${i === 0 ? '' : i + 1}`}
+            style={{
+              textDecoration: 'none',
+              color: i + 1 === currentPage ? '#ffffff' : '',
+              background: i + 1 === currentPage ? '#007acc' : '',
+            }}
+          >
+            {i + 1}
+          </Link>
+        </li>
+      ))}
+      {!isLast && (
+        <Link to={nextPage} rel="next">
+          Next Page →
+        </Link>
+      )}
     </Layout>
   )
 }
