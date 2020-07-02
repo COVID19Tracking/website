8444d29c401f3c71764b25cb38c73edba49a99f6
Andrew Schwartz <andrew@aschwartz.me>
2020-07-02 09:48:52 -0600

    chore(blog-categories-list): expand toggle to whole header

--- src/components/pages/blog/blog-categories-list.js
+++ src/components/pages/blog/blog-categories-list.js
@@ -28,13 +28,16 @@ export default () => {
   return (
     <div>
       <div className={blogCategoriesListStyles.container}>
-        <div className={blogCategoriesListStyles.header}>
+        <button
+          className={blogCategoriesListStyles.header}
+          onClick={() => setisOpen(!isOpen)} // toggle
+          type="button"
+          aria-expanded={isOpen}
+        >
           <h3>Posts by Category</h3>
-          <button
+          <div
             className={blogCategoriesListStyles.caret}
             aria-expanded={isOpen}
-            type="button"
-            onClick={() => setisOpen(!isOpen)} // toggle
           >
             <svg
               width="8"
@@ -47,8 +50,8 @@ export default () => {
               <path d="M0.146447 4.81307C-0.0488155 5.00833 -0.0488155 5.32492 0.146447 5.52018C0.341709 5.71544 0.658291 5.71544 0.853553 5.52018L0.146447 4.81307ZM4 1.66663L4.35355 1.31307L4 0.959519L3.64645 1.31307L4 1.66663ZM7.14645 5.52018C7.34171 5.71544 7.65829 5.71544 7.85355 5.52018C8.04882 5.32492 8.04882 5.00833 7.85355 4.81307L7.14645 5.52018ZM0.853553 5.52018L4.35355 2.02018L3.64645 1.31307L0.146447 4.81307L0.853553 5.52018ZM3.64645 2.02018L7.14645 5.52018L7.85355 4.81307L4.35355 1.31307L3.64645 2.02018Z" />
             </svg>
             <span className="a11y-only">todo hide/show categories</span>
-          </button>
-        </div>
+          </div>
+        </button>
         <ul
           role="navigation"
           aria-label="Categories"
