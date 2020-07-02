c6d33642ff476c0927de5deee1ff3422449eadbe
Andrew Schwartz <andrew@aschwartz.me>
2020-07-02 10:08:58 -0600

    chore(blog-teaser-list): add spacing between blog teasers

--- src/components/pages/blog/blog-teaser-list.js
+++ src/components/pages/blog/blog-teaser-list.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 import { Byline } from './byline'
 import Container from '~components/common/container'
@@ -8,7 +8,7 @@ import blogTeaserListStyle from './blog-teaser-list.module.scss'
 export default ({ items }) => (
   <Container narrow className={blogTeaserListStyle.container}>
     {items.map(node => (
-      <Fragment key={`blog-${node.slug}`}>
+      <div key={`blog-${node.slug}`} className={blogTeaserListStyle.teaser}>
         <h2 className={blogTeaserListStyle.title}>
           <Link to={`/blog/${node.slug}`}>{node.title}</Link>
         </h2>
@@ -16,7 +16,7 @@ export default ({ items }) => (
         <p className="lede">
           <CleanSpacing>{node.lede.lede}</CleanSpacing>
         </p>
-      </Fragment>
+      </div>
     ))}
   </Container>
 )
