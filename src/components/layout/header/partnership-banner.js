df938e4e4426e32ee6fc51ace592dabeb486b2c1
Kevin Miller <keveemiller@gmail.com>
2020-07-06 17:18:03 -0600

    chore: Track clicks on CRDT banner

--- src/components/layout/header/partnership-banner.js
+++ src/components/layout/header/partnership-banner.js
@@ -5,6 +5,18 @@ import partnershipBanner from './partnership-banner.module.scss'
 export default () => (
   <div className={partnershipBanner.banner}>
     <span className={partnershipBanner.new}>New</span>{' '}
-    <Link to="/race">We just launched the COVID Racial Data Tracker</Link>
+    <Link
+      to="/race"
+      onClick={() => {
+        if (
+          typeof window !== 'undefined' &&
+          typeof window.fathom !== 'undefined'
+        ) {
+          window.fathom.trackGoal('3GO7TUHI', 0)
+        }
+      }}
+    >
+      We just launched the COVID Racial Data Tracker
+    </Link>
   </div>
 )
