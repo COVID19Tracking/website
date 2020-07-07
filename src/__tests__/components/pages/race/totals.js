28b06a7c545e8ed1c9b17cd398eda86c84fa7445
Andrew Schwartz <andrew@aschwartz.me>
2020-07-07 10:51:20 -0600

    chore(tests): race totals

---
+++ src/__tests__/components/pages/race/totals.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { useStaticQuery } from 'gatsby'
+import Totals from '~components/pages/race/totals'
+
+beforeEach(() => {
+  useStaticQuery.mockImplementation(() => ({
+    covidRaceDataHomepage: {
+      statesReportingCases: "42",
+      statesReportingDeaths: "41"
+    }
+  }))
+})
+
+describe('Components : Race : Totals', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Totals />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
