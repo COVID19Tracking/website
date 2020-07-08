debd8e015d9269cb6e965dc42e4a1602a7671a56
Andrew Schwartz <andrew@aschwartz.me>
2020-07-08 11:42:33 -0600

    chore(tests): add header sorter

---
+++ src/__tests__/components/pages/race/dashboard/header-sorter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import HeaderSorter from '~components/pages/race/dashboard/header-sorter'
+
+describe('Components : Race : Dashboard : Header Sorter', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<HeaderSorter stateName={'Arizona'} stateReports={'race'} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
