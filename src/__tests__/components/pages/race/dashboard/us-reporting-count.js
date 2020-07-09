eba04b82c56def7e4c4c05c900bb946688532e06
Andrew Schwartz <andrew@aschwartz.me>
2020-07-09 16:04:19 -0600

    chore(tests): add table symbol key and us reporting count

---
+++ src/__tests__/components/pages/race/dashboard/us-reporting-count.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { useStaticQuery } from 'gatsby'
+import UsReportingCount from '~components/pages/race/dashboard/us-reporting-count'
+
+describe('Components : Pages : Race : Dashboard : US Overview', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <UsReportingCount
+          statesCasesCount={49}
+          statesDeathsCount={48}
+          className={'css-class-name'}
+        />,
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
