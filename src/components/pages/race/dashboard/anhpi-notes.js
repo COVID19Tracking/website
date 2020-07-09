bbf03fbf4e17d5e574c540087b8a8df8f449a0f1
Andrew Schwartz <andrew@aschwartz.me>
2020-07-09 14:13:49 -0600

    fix(anhpi-notes): typo in nhpiDeathCaution

--- src/components/pages/race/dashboard/anhpi-notes.js
+++ src/components/pages/race/dashboard/anhpi-notes.js
@@ -24,7 +24,7 @@ export default (data, stateNotes) => {
       ? data.nhpiANHPIPosNotes
       : notes.nhpiPos
   }
-  if (data.nhpiIDeathCaution) {
+  if (data.nhpiDeathCaution) {
     notes.nhpiDeath = data.nhpiANHPIDeathNotes
       ? data.nhpiANHPIDeathNotes
       : notes.nhpiDeath
