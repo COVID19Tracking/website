8f63bfe3e28bc7e22d137dbe5d4cc65d7fa3a045
Andrew Schwartz <andrew@aschwartz.me>
2020-07-15 10:01:51 -0600

    chore(summary): use css grid instead of component grid

--- src/components/common/state/summary.js
+++ src/components/common/state/summary.js
@@ -6,189 +6,152 @@ import {
   Statistic,
   SourcingLink,
 } from '~components/common/statistic'
-import { Row, Col } from '~components/common/grid'
 import summaryStyles from './summary.module.scss'
 
-const SummaryCol = ({ width = [4, 3, 4], children }) => (
-  <Col width={width} paddingLeft={[0, 0, 16]} paddingRight={[0, 0, 0]}>
-    {children}
-  </Col>
-)
-
-const SummaryRow = ({ children }) => (
-  <Row className={summaryStyles.row}>{children}</Row>
-)
+// const SummaryCol = ({ width = [4, 3, 4], children }) => (
 
 export default ({ stateSlug, data, raceData }) => {
   const deathsLabel =
     data.deathProbable || data.deathConfirmed ? 'Total deaths' : 'Deaths'
   return (
-    <>
-      <SummaryRow>
-        <SummaryCol>
-          <Card
-            title="Cases"
-            link={
-              <Link to={`/data/state/${stateSlug}/cases`}>Historical data</Link>
-            }
-          >
-            <CardBody>
-              <Statistic title="Total cases" value={data.positive}>
-                <SourcingLink to="#" />
-                <DrillDown label="New Cases" value={data.positiveIncrease} />
-                <DrillDown
-                  label="Increase in 7 days"
-                  value={data.positiveIncrease}
-                />
-                {/* todo set the 7 day change value */}
-              </Statistic>
-            </CardBody>
-          </Card>
-        </SummaryCol>
-        <SummaryCol>
-          <Card
-            title="Tests"
-            link={
-              <Link to={`/data/state/${stateSlug}/tests`}>Historical data</Link>
-            }
-          >
-            <CardBody>
-              <Statistic title="Negative" value={data.negative} />
-              <Statistic title="Pending" value={data.pending} />
-              <Statistic title="Total" value={data.posNeg} />
-            </CardBody>
-          </Card>
-        </SummaryCol>
-        <SummaryCol>
-          <Card
-            title="Cumulative Hospitalization"
-            link={
-              <Link to={`/data/state/${stateSlug}/hospitalization`}>
-                Historical data
-              </Link>
-            }
-          >
-            <CardBody>
+    <div className={summaryStyles.container}>
+      <Card
+        title="Cases"
+        link={
+          <Link to={`/data/state/${stateSlug}/cases`}>Historical data</Link>
+        }
+      >
+        <CardBody>
+          <Statistic title="Total cases" value={data.positive}>
+            <SourcingLink to="#" />
+            <DrillDown label="New Cases" value={data.positiveIncrease} />
+            <DrillDown
+              label="Increase in 7 days"
+              value={data.positiveIncrease}
+            />
+            {/* todo set the 7 day change value */}
+            {/* Hm, so this is a little harder than it sounds since you can't do transforms in Gatsby after all sources are loaded, so this would be difficult. I have a proposal out for putting this into the API eventually. Maybe check out what @Gabe O'Leary did in /src/components/common/summary-charts - that dailyAverage function could be moved to be an include. */}
+          </Statistic>
+        </CardBody>
+      </Card>
+      <Card
+        title="Tests"
+        link={
+          <Link to={`/data/state/${stateSlug}/tests`}>Historical data</Link>
+        }
+      >
+        <CardBody>
+          <Statistic title="Negative" value={data.negative} />
+          <Statistic title="Pending" value={data.pending} />
+          <Statistic title="Total" value={data.posNeg} />
+        </CardBody>
+      </Card>
+      <Card
+        title="Cumulative Hospitalization"
+        link={
+          <Link to={`/data/state/${stateSlug}/hospitalization`}>
+            Historical data
+          </Link>
+        }
+      >
+        <CardBody>
+          <Statistic title="Cumulative" value={data.hospitalizedCumulative} />
+          <Statistic title="In ICU cumulative" value={data.inIcuCumulative} />
+          <Statistic
+            title="On ventilator cumulative"
+            value={data.onVentilatorCumulative}
+          />
+        </CardBody>
+      </Card>
+      <Card
+        title="Outcomes"
+        link={
+          <Link to={`/data/state/${stateSlug}/outcomes`}>Historical data</Link>
+        }
+      >
+        <CardBody>
+          <Statistic title="Recovered" value={data.recovered} />
+          <Statistic title={deathsLabel} value={data.death} />
+          {data.deathProbable && (
+            <Statistic title="Probable deaths" value={data.deathProbable} />
+          )}
+          {data.deathConfirmed && (
+            <Statistic title="Confirmed deaths" value={data.deathConfirmed} />
+          )}
+        </CardBody>
+      </Card>
+      <Card
+        title="Race &amp; Ethnicity"
+        link={
+          <Link to={`/data/state/${stateSlug}/race-ethnicity`}>
+            Historical data
+          </Link>
+        }
+      >
+        <CardBody>
+          {raceData.combined && (
+            <>
+              <p>Reported race &amp; ethnicity data for:</p>
               <Statistic
-                title="Cumulative"
-                value={data.hospitalizedCumulative}
+                title="Cases"
+                value={Math.round(raceData.combined.knownRaceEthPos * 100)}
+                suffix="%"
               />
               <Statistic
-                title="In ICU cumulative"
-                value={data.inIcuCumulative}
+                title="Death"
+                value={Math.round(raceData.combined.knownRaceEthDeath * 100)}
+                suffix="%"
+              />
+            </>
+          )}
+          {raceData.separate && (
+            <>
+              <p>Reported race data for:</p>
+              <Statistic
+                title="Cases"
+                value={Math.round(raceData.separate.knownRacePos * 100)}
+                suffix="%"
               />
               <Statistic
-                title="On ventilator cumulative"
-                value={data.onVentilatorCumulative}
+                title="Death"
+                value={Math.round(raceData.separate.knownRaceDeath * 100)}
+                suffix="%"
               />
-            </CardBody>
-          </Card>
-        </SummaryCol>
-      </SummaryRow>
-      <SummaryRow>
-        <SummaryCol>
-          <Card
-            title="Outcomes"
-            link={
-              <Link to={`/data/state/${stateSlug}/outcomes`}>
-                Historical data
-              </Link>
-            }
-          >
-            <CardBody>
-              <Statistic title="Recovered" value={data.recovered} />
-              <Statistic title={deathsLabel} value={data.death} />
-              {data.deathProbable && (
-                <Statistic title="Probable deaths" value={data.deathProbable} />
-              )}
-              {data.deathConfirmed && (
-                <Statistic
-                  title="Confirmed deaths"
-                  value={data.deathConfirmed}
-                />
-              )}
-            </CardBody>
-          </Card>
-        </SummaryCol>
-        <SummaryCol>
-          <Card
-            title="Race &amp; Ethnicity"
-            link={
-              <Link to={`/data/state/${stateSlug}/race-ethnicity`}>
-                Historical data
-              </Link>
-            }
-          >
-            <CardBody>
-              {raceData.combined && (
-                <>
-                  <p>Reported race &amp; ethnicity data for:</p>
-                  <Statistic
-                    title="Cases"
-                    value={Math.round(raceData.combined.knownRaceEthPos * 100)}
-                    suffix="%"
-                  />
-                  <Statistic
-                    title="Death"
-                    value={Math.round(
-                      raceData.combined.knownRaceEthDeath * 100,
-                    )}
-                    suffix="%"
-                  />
-                </>
-              )}
-              {raceData.separate && (
-                <>
-                  <p>Reported race data for:</p>
-                  <Statistic
-                    title="Cases"
-                    value={Math.round(raceData.separate.knownRacePos * 100)}
-                    suffix="%"
-                  />
-                  <Statistic
-                    title="Death"
-                    value={Math.round(raceData.separate.knownRaceDeath * 100)}
-                    suffix="%"
-                  />
-                  <p>Reported ethnicity &amp; data for:</p>
-                  <Statistic
-                    title="Cases"
-                    value={Math.round(raceData.separate.knownEthPos * 100)}
-                    suffix="%"
-                  />
-                  <Statistic
-                    title="Death"
-                    value={Math.round(raceData.separate.knownEthDeath * 100)}
-                    suffix="%"
-                  />
-                </>
-              )}
-            </CardBody>
-          </Card>
-        </SummaryCol>
-        <SummaryCol>
-          <Card
-            title="Current Hospitalization"
-            link={
-              <Link to={`/data/state/${stateSlug}/hospitalization`}>
-                Historical data
-              </Link>
-            }
-          >
-            <CardBody>
+              <p>Reported ethnicity &amp; data for:</p>
               <Statistic
-                title="Currently hospitalized"
-                value={data.hospitalizedCurrently}
+                title="Cases"
+                value={Math.round(raceData.separate.knownEthPos * 100)}
+                suffix="%"
               />
-              <Statistic title="Currently in ICU" value={data.inIcuCurrently} />
               <Statistic
-                title="Currently on ventilator"
-                value={data.onVentilatorCurrently}
+                title="Death"
+                value={Math.round(raceData.separate.knownEthDeath * 100)}
+                suffix="%"
               />
-            </CardBody>
-          </Card>
-        </SummaryCol>
-      </SummaryRow>
-    </>
+            </>
+          )}
+        </CardBody>
+      </Card>
+      <Card
+        title="Current Hospitalization"
+        link={
+          <Link to={`/data/state/${stateSlug}/hospitalization`}>
+            Historical data
+          </Link>
+        }
+      >
+        <CardBody>
+          <Statistic
+            title="Currently hospitalized"
+            value={data.hospitalizedCurrently}
+          />
+          <Statistic title="Currently in ICU" value={data.inIcuCurrently} />
+          <Statistic
+            title="Currently on ventilator"
+            value={data.onVentilatorCurrently}
+          />
+        </CardBody>
+      </Card>
+    </div>
   )
 }
