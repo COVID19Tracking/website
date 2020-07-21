6f543f44cb70a912b3b1d3847d3e517e1a92140a
Andrew Schwartz <andrew@aschwartz.me>
2020-07-21 13:38:25 -0600

    chore(cards): move cases card to separate file

---
+++ src/components/common/state/state-cards/cases-card.js
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react'
+import { Link } from 'gatsby'
+import { Card, CardBody } from '~components/common/card'
+import { DefinitionPanelContext } from '~components/common/state/definitions-panel'
+import {
+  DrillDown,
+  Statistic,
+  DefinitionLink,
+} from '~components/common/statistic'
+
+export default ({
+  stateSlug,
+  positive,
+  positiveIncrease,
+  sevenDayIncrease,
+}) => {
+  const sevenDayIncreasePercent = Math.round(sevenDayIncrease * 100 * 10) / 10
+  const drillDownValue = Number.isNaN(sevenDayIncreasePercent)
+    ? 'N/A'
+    : sevenDayIncreasePercent
+  const drillDownSuffix = Number.isNaN(sevenDayIncreasePercent) ? '' : '%'
+  const definitionContext = useContext(DefinitionPanelContext)
+
+  return (
+    <Card
+      title="Cases"
+      link={<Link to={`/data/state/${stateSlug}/cases`}>Historical data</Link>}
+    >
+      <CardBody>
+        <Statistic title="Total cases" value={positive}>
+          <DefinitionLink
+            onDefinitionsToggle={() => {
+              definitionContext({
+                fields: ['positive', 'negative'],
+                highlight: 'positive',
+              })
+            }}
+          />
+          <DrillDown label="New cases" value={positiveIncrease} calculated />
+          <DrillDown
+            label="Increase in 7 days"
+            value={drillDownValue}
+            suffix={drillDownSuffix}
+            calculated
+          />
+        </Statistic>
+      </CardBody>
+    </Card>
+  )
+}
--- src/components/common/state/summary.js
+++ src/components/common/state/summary.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import { DefinitionPanel, DefinitionPanelContext } from './definitions-panel'
+import CasesCard from './state-cards/cases-card'
 import {
-  CasesCard,
   PCRTestsCard,
   TestsCard,
   CumulativeHospitalizationCard,
