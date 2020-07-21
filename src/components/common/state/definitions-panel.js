1c1e87b3a8e5f2fbef8fc6fa7568d518f3910ad5
Kevin Miller <keveemiller@gmail.com>
2020-07-21 11:12:05 -0600

    feat: Escape key handling to close pane

--- src/components/common/state/definitions-panel.js
+++ src/components/common/state/definitions-panel.js
@@ -1,60 +1,75 @@
-import React from 'react'
-
+import React, { useEffect, useRef } from 'react'
+import classnames from 'classnames'
 import closeIcon from '~images/icons/close-x.svg'
-
 import definitionsPanelStyles from './definitions-panel.module.scss'
 
-const Definition = ({ key, definition }) => (
-  <p key={key} ref={definition.ref}>
-    <span className={definitionsPanelStyles.title}>{definition.name}</span>:
-    <span
-      dangerouslySetInnerHTML={{
-        __html:
-          definition.childContentfulDataDefinitionDefinitionTextNode
-            .childMarkdownRemark.html,
-      }}
-    />
-  </p>
-)
+const Definition = ({ key, definition, highlighted }) => {
+  const definitionRef = useRef(false)
+  useEffect(() => {
+    if (highlighted) {
+      definitionRef.current.focus()
+    }
+  }, [])
+
+  return (
+    <div
+      key={key}
+      ref={definitionRef}
+      tabIndex="-1"
+      className={classnames(
+        definitionsPanelStyles.definition,
+        highlighted && definitionsPanelStyles.highlight,
+      )}
+    >
+      <span className={definitionsPanelStyles.title}>{definition.name}</span>:
+      <span
+        dangerouslySetInnerHTML={{
+          __html:
+            definition.childContentfulDataDefinitionDefinitionTextNode
+              .childMarkdownRemark.html,
+        }}
+      />
+    </div>
+  )
+}
 
 const DefinitionPanelContext = React.createContext()
 
-const DefinitionPanel = ({ onHide, definitions }) => {
-  /*
-  onHide is the function that will hide the entire definitions panel
-  */
-
-  // Should take up the whole screen below a certain breakpoint.
-  // On opening of dialog, focus the definition.
-  // Hitting escape should close the definition
-  // OK - so then we should both use aria-describedby that points to basically
-  // a direct child of the dialog that contains ALL descriptions
-  // and then move focus to the highlighted definition
-
+const DefinitionPanel = ({ onHide, definitions, highlightedDefinition }) => {
   return (
     <div
       className={definitionsPanelStyles.definitionsPanel}
       role="dialog"
       aria-modal="true"
-      tabIndex={-1}
+      tabIndex="-1"
       aria-labelledby="definitionsDialogLabel"
     >
-      <span id="definitionsDialogLabel" className="a11y-only">
-        Data definitions
-      </span>
-      <div className={definitionsPanelStyles.closePanelContainer}>
-        <button
-          type="button"
-          className={definitionsPanelStyles.closePanel}
-          onClick={onHide}
-        >
-          <img src={closeIcon} alt="Close panel." />
-        </button>
+      <div
+        role="presentation"
+        onKeyDown={event => {
+          if (event.key === 'Escape') {
+            onHide()
+          }
+        }}
+      >
+        <div className={definitionsPanelStyles.closePanelContainer}>
+          <button
+            type="button"
+            className={definitionsPanelStyles.closePanel}
+            onClick={onHide}
+          >
+            <img src={closeIcon} alt="Close panel." />
+          </button>
+        </div>
+        <h2 id="definitionsDialogLabel">Definitions</h2>
+        {Object.keys(definitions).map(key => (
+          <Definition
+            key={key}
+            highlighted={definitions[key].fieldName === highlightedDefinition}
+            definition={definitions[key]}
+          />
+        ))}
       </div>
-      <h2>Definitions</h2>
-      {Object.keys(definitions).map(key => (
-        <Definition key={key} definition={definitions[key]} />
-      ))}
     </div>
   )
 }
