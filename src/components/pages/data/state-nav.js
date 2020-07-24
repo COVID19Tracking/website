236208e537a6e76b764936c12796196f23ef1be6
Andrew Schwartz <andrew@aschwartz.me>
2020-07-24 09:53:42 -0600

    feat(data-state-nav): use tablet disclosure, update small viewport styles

--- src/components/pages/data/state-nav.js
+++ src/components/pages/data/state-nav.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import stateNavStyle from './state-nav.module.scss'
+import {
+  TabletDisclosure,
+  TabletDisclosureHeader,
+  TabletDisclosureContent,
+} from '~components/common/tablet-disclosure'
 
-export default ({ stateList }) => (
-  <>
-    <div className={stateNavStyle.stateNav}>
-      <h3>Jump to a state:</h3>
-      <ul>
-        {stateList.map(state => (
-          <li key={state.state}>
-            <a href={`#state-${state.state.toLowerCase()}`}>{state.state}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>
-)
+export default ({ stateList }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <TabletDisclosure>
+      <TabletDisclosureHeader isOpen={isOpen} setIsOpen={setIsOpen}>
+        <h3 className={stateNavStyle.header}>Jump to a state:</h3>
+      </TabletDisclosureHeader>
+      <TabletDisclosureContent
+        isOpen={isOpen}
+        className={stateNavStyle.container}
+      >
+        <ul>
+          {stateList.map(state => (
+            <li key={state.state}>
+              <a href={`#state-${state.state.toLowerCase()}`}>{state.state}</a>
+            </li>
+          ))}
+        </ul>
+      </TabletDisclosureContent>
+    </TabletDisclosure>
+  )
+}
