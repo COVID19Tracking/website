cc5831e0e087e89c813f27fb02b4ba09523c3f17
Andrew Schwartz <andrew@aschwartz.me>
2020-07-28 10:57:17 -0600

    feat(blog-hero): pass blog categories to return links
    this doesn't have the separator bullets yet

--- src/components/pages/blog/blog-hero.js
+++ src/components/pages/blog/blog-hero.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import Categories from '~components/pages/blog/categories'
+import { Link } from 'gatsby'
+
 import Lede from '~components/pages/blog/blog-lede'
+import ReturnLinks from '~components/layout/header/return-links'
+
+import rightCaret from '~images/icons/right-caret.svg'
 import blogHeroStyles from './blog-hero.module.scss'
 
 export default ({ categories, headline, authors, date, lede, id }) => (
   <div className={blogHeroStyles.container} narrow centered>
-    <Categories categories={categories} />
+    <ReturnLinks>
+      <Link to="/blog">Blog</Link>
+      {categories && (
+        <>
+          <img src={rightCaret} alt="" height="12px" />
+          {categories.map(category => (
+            <Link to={`/blog/category/${category.slug}`}>{category.name}</Link>
+          ))}
+        </>
+      )}
+    </ReturnLinks>
     <Lede
       headline={headline}
       authors={authors}
