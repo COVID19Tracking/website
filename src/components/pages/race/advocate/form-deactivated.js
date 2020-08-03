cb52c4e4f374eb765bd39d244368fe96b9642deb
Andrew Schwartz <andrew@aschwartz.me>
2020-08-03 15:22:45 -0600

    chore(form-placeholder): add isPlaceholder prop, adjust spacing

--- src/components/pages/race/advocate/form-deactivated.js
+++ src/components/pages/race/advocate/form-deactivated.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import classnames from 'classnames'
 import formDeactivatedStyle from './form-deactivated.module.scss'
 
-export default ({ children }) => (
-  <div className={formDeactivatedStyle.container}>{children}</div>
+export default ({ children, isPlaceholder }) => (
+  <div
+    className={classnames(
+      formDeactivatedStyle.container,
+      isPlaceholder && formDeactivatedStyle.placeholder,
+    )}
+  >
+    {children}
+  </div>
 )
