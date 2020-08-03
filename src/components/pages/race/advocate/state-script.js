b23098d64941b0c3d07a5b0b0284fd8bb656e7b4
Andrew Schwartz <andrew@aschwartz.me>
2020-08-03 16:21:12 -0600

    docs(state-script): add defaultProps and propTypes

--- src/components/pages/race/advocate/state-script.js
+++ src/components/pages/race/advocate/state-script.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import stateScriptStyles from './state-script.module.scss'
 
-export default ({ currentState, stateScripts }) => {
+const StateScript = ({ currentState, stateScripts }) => {
   if (currentState === '-- Select a state --') {
     return null
   }
@@ -20,3 +21,23 @@ export default ({ currentState, stateScripts }) => {
     </div>
   )
 }
+
+StateScript.defaultProps = {
+  currentState: '-- Select a state --',
+}
+
+StateScript.propTypes = {
+  currentState: PropTypes.string,
+  stateScripts: PropTypes.arrayOf(
+    PropTypes.shape({
+      state: PropTypes.string.isRequired,
+      message: PropTypes.shape({
+        childMarkdownRemark: PropTypes.shape({
+          html: PropTypes.string,
+        }),
+      }).isRequired,
+    }),
+  ).isRequired,
+}
+
+export default StateScript
