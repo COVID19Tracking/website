443a103bad49825611b33b84247b716d849dd8b8
Gabe O'Leary <oleary.gabe@gmail.com>
2020-08-03 09:29:51 -0700

    chore: make image full width (of parent), add white bg on text

--- src/components/pages/experiments/scrollytelling/pooled-testing.js
+++ src/components/pages/experiments/scrollytelling/pooled-testing.js
@@ -1,21 +1,8 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import { graphql } from 'gatsby'
-
-import { extent, max } from 'd3-array'
-import { scaleLinear, scaleTime } from 'd3-scale'
-import { line, curveCardinal } from 'd3-shape'
-import { timeMonth, timeDay } from 'd3-time'
-
-import { formatNumber, parseDate } from '~utilities/visualization'
 
 import Layout from '~components/layout'
-import Container from '~components/common/container'
 
-import chartStyles from '~components/charts/charts.module.scss'
-import colors from '~scss/colors.module.scss'
-
-import styles from './scrollytelling.module.scss'
+import styles from './pooled-testing.module.scss'
 
 // these next two can only be loaded clientside
 import { Scrollama, Step } from '~utilities/react-scrollama'
@@ -45,47 +32,23 @@ export default ({ images }) => {
       path="/data"
     >
       <h2>In this example, we’ll test 25 people.</h2>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateRows: '50% 50%',
-        }}
-      >
-        <div
-          style={{
-            position: 'sticky',
-            top: 100,
-            height: '20vh',
-            gridColumn: 1,
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
+      <div>
+        <div className={styles.stickyImage}>
           {currentStepIndex !== null && (
             <img src={images[currentStepIndex]} alt="test" />
           )}
         </div>
-        <div style={{ gridColumn: 2 }}>
+        <div>
           <Scrollama onStepEnter={onStepEnter} offset={0.25}>
             {images.map((data, index) => (
               <Step data={index} key={JSON.stringify(data)}>
-                <div
-                  style={{
-                    height: '50vh',
-                    padding: '100px 20px',
-                  }}
-                >
-                  {copy[index]}
+                <div className={styles.stepInner}>
+                  <h4 className={styles.text}>{copy[index]}</h4>
                 </div>
               </Step>
             ))}
           </Scrollama>
-          <div
-            style={{
-              height: '50vh',
-              padding: '100px 20px',
-            }}
-          />
+          <div className={styles.stepInner} />
         </div>
       </div>
     </Layout>
