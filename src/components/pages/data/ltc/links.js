7749a4ce990b1397c82edaf98e4c6aee0de07975
Kevin Miller <keveemiller@gmail.com>
2020-08-07 10:04:34 -0700

    chore: Add links to download, tableau charts

---
+++ src/components/pages/data/ltc/links.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { CtaLink } from '~components/common/landing-page/call-to-action'
+import linksStyle from './links.module.scss'
+
+export default () => (
+  <div className={linksStyle.links}>
+    <CtaLink to="#download" bold centered>
+      Download the dataset
+    </CtaLink>
+    <CtaLink to="#download" centered>
+      Download state notes
+    </CtaLink>
+    <CtaLink to="#download" centered>
+      View the spreadsheet
+    </CtaLink>
+  </div>
+)
---
+++ src/pages/data/ltc.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import TableauChart from '~components/charts/tableau'
+import Total from '~components/common/landing-page/total'
+import { Col, Row } from '~components/common/grid'
+import { FormatNumber } from '~components/utils/format'
+import Links from '~components/pages/data/ltc/links'
+import Layout from '~components/layout'
+
+export default () => (
+  <Layout title="Long-term Care" path="/data/ltc">
+    <h2>Current outbreak</h2>
+    <Row>
+      <Col width={[4, 6, 4]}>
+        <Total label="Cases" number={<FormatNumber number={0} />} />
+      </Col>
+      <Col width={[4, 6, 4]}>
+        <Total label="Deaths" number={<FormatNumber number={0} />} />
+      </Col>
+      <Col width={[4, 6, 4]}>
+        <Total label="Facilities" number={<FormatNumber number={0} />} />
+      </Col>
+    </Row>
+    <Links />
+
+    <p>Some content</p>
+    <TableauChart
+      id="ltc-1"
+      height={700}
+      viewUrl="https://public.tableau.com/views/WebsiteCharts-CTPLong-TermCare/FigMap?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+      viewUrlMobile="https://public.tableau.com/views/WebsiteCharts-CTPLong-TermCaremobile/FigMap?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    />
+    <TableauChart
+      id="ltc-2"
+      height={1600}
+      viewUrl="https://public.tableau.com/views/WebsiteCharts-CTPLong-TermCare/SummaryTable?:language=en&:display_count=y&:origin=viz_share_link"
+      viewUrlMobile="https://public.tableau.com/views/WebsiteCharts-CTPLong-TermCaremobile/SummaryTable?:language=en&:display_count=y&:origin=viz_share_link"
+    />
+  </Layout>
+)
