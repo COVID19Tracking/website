0286efd1fd7b8be796e93ac58f3d8d8800fdc3d7
Andrew Schwartz <andrew@aschwartz.me>
2020-08-18 10:09:05 -0600

    chore(blog-pagination): use caret navigation, rm 'jump to page'

--- src/components/pages/blog/blog-pagination.js
+++ src/components/pages/blog/blog-pagination.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import blogPaginationStyle from './blog-pagination.module.scss'
+import doubleCaret from '~images/icons/double-caret.svg'
+import doubleCaretInactive from '~images/icons/double-caret-inactive.svg'
+
+// todo use classnames
 
 export default ({ currentPage, numPages }) => {
   const isFirst = currentPage === 1
@@ -12,49 +16,50 @@ export default ({ currentPage, numPages }) => {
   const nextPage = `/blog/page/${(currentPage + 1).toString()}`
 
   const disabledPrev = isFirst ? blogPaginationStyle.disabledLink : ''
-  const disableNext = isLast ? blogPaginationStyle.disabledLink : ''
+  const disableNext = isLast ? blogPaginationStyle.disabledLink : '' // todo add d to name
 
   return (
     <div className={blogPaginationStyle.navigationContainer}>
       <div className={blogPaginationStyle.navigation}>
-        <Link
-          to={prevPage}
-          rel="prev"
-          className={`${disabledPrev} ${blogPaginationStyle.navigationItem}`}
-          aria-current={isFirst ? 'false' : 'step'}
-        >
-          ← Previous Page
-        </Link>
-
-        <Link
-          to={nextPage}
-          rel="next"
-          className={`${disableNext} ${blogPaginationStyle.navigationItem}`}
-          aria-current={isLast ? 'false' : 'step'}
-        >
-          Next Page →
-        </Link>
-      </div>
-      <div className={blogPaginationStyle.pageNavigation}>
-        <p>Jump to page:</p>
-        <ul className={blogPaginationStyle.pages}>
-          {Array.from({ length: numPages }, (_, i) => (
-            <li
-              key={`pagination-number${i + 1}`}
-              className={blogPaginationStyle.page}
-            >
-              <Link
-                to={`/blog${i === 0 ? '' : `/page/${i + 1}`}`}
-                className={`${blogPaginationStyle.pageLink} ${
-                  i + 1 === currentPage ? blogPaginationStyle.pageActive : ''
-                }`}
-                aria-current={i + 1 === currentPage ? 'page' : 'false'}
-              >
-                {i + 1}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {disabledPrev ? (
+          <Link
+            to={prevPage}
+            rel="prev"
+            className={`${disabledPrev} ${blogPaginationStyle.navigationItem}`}
+            aria-current={isFirst ? 'false' : 'step'}
+          >
+            <img src={doubleCaretInactive} alt="This is the first page." />
+          </Link>
+        ) : (
+          <Link
+            to={prevPage}
+            rel="prev"
+            className={blogPaginationStyle.navigationItem}
+            aria-current={isFirst ? 'false' : 'step'}
+          >
+            <img src={doubleCaret} alt="Jump to the previous page." />
+          </Link>
+        )}
+        Page {currentPage} of {numPages}
+        {disableNext ? (
+          <Link
+            to={nextPage}
+            rel="next"
+            className={`${disableNext} ${blogPaginationStyle.navigationItem}`}
+            aria-current={isLast ? 'false' : 'step'}
+          >
+            <img src={doubleCaretInactive} alt="This is the last page." />
+          </Link>
+        ) : (
+          <Link
+            to={nextPage}
+            rel="next"
+            className={blogPaginationStyle.navigationItem}
+            aria-current={isLast ? 'false' : 'step'}
+          >
+            <img src={doubleCaret} alt="Jump to the next page." />
+          </Link>
+        )}
       </div>
     </div>
   )
