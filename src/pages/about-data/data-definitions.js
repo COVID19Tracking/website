4d493ff6dfbe1398038e5a110c360961af23d923
Kevin Miller <keveemiller@gmail.com>
2020-08-18 05:22:17 -0700

    chore: Add definition categories

--- src/pages/about-data/data-definitions.js
+++ src/pages/about-data/data-definitions.js
@@ -1,37 +1,68 @@
 import React, { Fragment } from 'react'
 import { graphql } from 'gatsby'
+import slugify from 'slugify'
 import Layout from '~components/layout'
 import LongContent from '~components/common/long-content'
 import ContentfulContent from '~components/common/contentful-content'
 
-export default ({ data }) => (
-  <Layout title="Data Definitions" centered>
-    <LongContent>
-      <ContentfulContent
-        content={
-          data.preamble.childContentfulSnippetContentTextNode
-            .childMarkdownRemark.html
-        }
-        id={data.preamble.contentful_id}
-      />
-      {data.allContentfulDataDefinition.nodes.map(definition => (
-        <Fragment key={definition.contentful_id}>
-          <h3 id={`definition-${definition.fieldName}`}>{definition.name}</h3>
-          <p>
-            API field name: <em>{definition.fieldName}</em>
-          </p>
-          <ContentfulContent
-            content={
-              definition.childContentfulDataDefinitionDefinitionTextNode
-                .childMarkdownRemark.html
-            }
-            id={definition.contentful_id}
-          />
-        </Fragment>
-      ))}
-    </LongContent>
-  </Layout>
-)
+export default ({ data }) => {
+  const categories = [
+    'Cases',
+    'PCR tests',
+    'Antibody tests',
+    'Antigen tests',
+    'Hospitalization',
+    'Outcomes',
+    'State metadata',
+  ]
+  const { nodes } = data.allContentfulDataDefinition
+  return (
+    <Layout title="Data Definitions" centered>
+      <LongContent>
+        <ContentfulContent
+          content={
+            data.preamble.childContentfulSnippetContentTextNode
+              .childMarkdownRemark.html
+          }
+          id={data.preamble.contentful_id}
+        />
+        <ul>
+          {categories.map(category => (
+            <li>
+              <a href={`#${slugify(category, { lower: true })}`}>{category}</a>
+            </li>
+          ))}
+        </ul>
+        {categories.map(category => (
+          <>
+            <h2 id={slugify(category, { lower: true })}>{category}</h2>
+            {nodes
+              .filter(
+                node => node.category.toLowerCase() === category.toLowerCase(),
+              )
+              .map(definition => (
+                <Fragment key={definition.contentful_id}>
+                  <h3 id={`definition-${definition.fieldName}`}>
+                    {definition.name}
+                  </h3>
+                  <p>
+                    API field name: <em>{definition.fieldName}</em>
+                  </p>
+                  <ContentfulContent
+                    content={
+                      definition.childContentfulDataDefinitionDefinitionTextNode
+                        .childMarkdownRemark.html
+                    }
+                    id={definition.contentful_id}
+                  />
+                </Fragment>
+              ))}
+          </>
+        ))}
+      </LongContent>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query {
@@ -47,6 +78,7 @@ export const query = graphql`
       nodes {
         name
         fieldName
+        category
         childContentfulDataDefinitionDefinitionTextNode {
           childMarkdownRemark {
             html
