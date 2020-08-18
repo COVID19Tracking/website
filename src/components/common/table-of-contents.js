a1f3182e7c3282c12edfd4faeb2ad465f24e62d1
Kevin Miller <keveemiller@gmail.com>
2020-08-18 07:11:39 -0700

    chore: Add TOC style

--- src/components/common/table-of-contents.js
+++ src/components/common/table-of-contents.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import tocStyle from './table-of-contents.module.scss'
 
 export default ({ headings }) => (
-  <ul>
+  <ul className={tocStyle.tableOfContents}>
     {headings.map(heading => (
       <li key={`toc-${heading.id}`}>
         <a href={`#${heading.id}`}>{heading.value}</a>
