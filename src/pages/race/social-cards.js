49f890818e37942b90920e6e4936d317bbda476a
Andrew Schwartz <andrew@aschwartz.me>
2020-08-24 13:06:50 -0600

    feat(race-social-cards): use contentful preamble

--- src/pages/race/social-cards.js
+++ src/pages/race/social-cards.js
@@ -1,19 +1,37 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import RaceSocialCards from '~components/social-cards/race/state'
 import SocialCardsSelect from '~components/pages/race/social-cards/select'
 import Layout from '~components/layout'
 
-export default ({ data }) => (
-  <Layout
-    title="Social cards"
-    returnLink="/race"
-    returnLinkTitle="Racial Data Tracker"
-    path="/race/social-cards"
-    centered
-  >
-    <p>Help spread awareness of racial disparity in COVID-19 pandemic.</p>
-    <RaceSocialCards />
-    <SocialCardsSelect />
-  </Layout>
-)
+export default ({ data }) => {
+  const { contentfulSnippet } = useStaticQuery(graphql`
+    query {
+      contentfulSnippet(slug: { eq: "crdt-social-cards-preamble" }) {
+        content {
+          childMarkdownRemark {
+            html
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout
+      title="Social cards"
+      returnLink="/race"
+      returnLinkTitle="Racial Data Tracker"
+      path="/race/social-cards"
+      centered
+    >
+      <div
+        dangerouslySetInnerHTML={{
+          __html: contentfulSnippet.content.childMarkdownRemark.html,
+        }}
+      />
+      <RaceSocialCards />
+      <SocialCardsSelect />
+    </Layout>
+  )
+}
