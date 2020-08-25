08148988f61664b02dfea70316e7c3de6be2ccc3
Andrew Schwartz <andrew@aschwartz.me>
2020-08-25 16:39:25 -0600

    feat(detail-text): add centered prop, support passing className

--- src/components/common/detail-text.js
+++ src/components/common/detail-text.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import classnames from 'classnames'
 import detailTextStyles from './detail-text.module.scss'
 
-const DetailText = ({ children }) => (
-  <div className={`detail-text ${detailTextStyles.detailText}`}>{children}</div>
+const DetailText = ({ children, className, centered = false }) => (
+  <div
+    className={classnames(
+      'detail-text',
+      detailTextStyles.detailText,
+      centered && detailTextStyles.centered,
+      className,
+    )}
+  >
+    {children}
+  </div>
 )
 
 export default DetailText
