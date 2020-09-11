ff7716f4b3c7ebc6bb56704fbd042400b07c6fbc
Kevin Miller <keveemiller@gmail.com>
2020-09-11 12:33:13 -0700

    chore: Better onload

--- src/components/common/recaptcha.js
+++ src/components/common/recaptcha.js
@@ -16,23 +16,22 @@ const Recaptcha = () => {
   const captchaRef = useRef(false)
 
   useEffect(() => {
-    if (
-      typeof window !== 'undefined' &&
-      typeof window.grecaptcha !== 'undefined'
-    ) {
-      window.grecaptcha.render(captchaRef.current, {
-        siteKey: data.site.siteMetadata.recaptchaKey,
-      })
-    }
+    if (typeof window !== 'undefined')
+      window.recaptchaOnload = () => {
+        window.grecaptcha.render(captchaRef.current, {
+          siteKey: data.site.siteMetadata.recaptchaKey,
+        })
+      }
   }, [])
 
   return (
     <>
       <Helmet>
-        <script src="https://www.google.com/recaptcha/api.js?render=explicit" />
+        <script src="https://www.google.com/recaptcha/api.js?render=explicit&onload=recaptchaOnload" />
       </Helmet>
       <div
         className="g-recaptcha"
+        ref={captchaRef}
         data-sitekey={data.site.siteMetadata.recaptchaKey}
       />
     </>
