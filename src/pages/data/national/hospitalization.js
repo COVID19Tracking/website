a6372a2a0b09b6a497b3f0288e2979ab8d65ac1b
Kevin Miller <keveemiller@gmail.com>
2020-09-14 13:47:57 -0700

    chore: Update national tables

--- src/pages/data/national/hospitalization.js
+++ src/pages/data/national/hospitalization.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import TableResponsive from '~components/common/table-responsive'
-import { FormatDate } from '~components/utils/format'
 import Definitions from '~components/pages/data/definitions'
 import Layout from '~components/layout'
 
@@ -22,8 +21,6 @@ const NationalDataHospitalizationPage = ({ data }) => {
         labels={[
           {
             field: 'date',
-
-            format: date => <FormatDate date={date} format="ccc LLL d yyyy" />,
           },
           {
             field: 'hospitalizedCumulative',
@@ -66,7 +63,7 @@ export const query = graphql`
   {
     allCovidUsDaily(sort: { fields: date, order: DESC }) {
       nodes {
-        date
+        date(formatString: "MMM D, YYYY")
         hospitalizedCumulative
         hospitalizedCurrently
         hospitalizedIncrease
