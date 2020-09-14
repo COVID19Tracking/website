f94514426532fb10207e858eef07f3445d2d7882
Júlia Ledur <ledurjulia@gmail.com>
2020-09-14 12:47:20 -0400

    rough ppr scrolly

--- src/components/pages/experiments/scrollytelling/pooled-testing.js
+++ src/components/pages/experiments/scrollytelling/pooled-testing.js
@@ -8,13 +8,13 @@ import styles from './pooled-testing.module.scss'
 import { Scrollama, Step } from '~utilities/react-scrollama'
 
 const copy = [
-  'In this example, we’ll test 25 people.',
-  'In the pooled testing method, people are divided into groups.',
-  'Instead of being tested individually, their samples are combined and tested all at once.',
-  'Each group gets a result. If a group tests negative, everyone in that group is negative.',
-  'If a group tests positive, all samples are tested individually. The lab keeps leftover samples so there is no need for the individuals to go back to testing sites.',
-  'Pooled testing significantly cuts down supplies and testing wait times. In this example, 25 people were tested, but only 10 tests were used. ',
-  'If everyone had been tested individually, 25 tests would have been needed.',
+  'Let’s consider a state where a group of 100 people have been tested for COVID-19.',
+  'Out of these 100, only three people received a positive test.',
+  'In this case, considering the sample of people who were tested, we would say the percent positive in this particular population is 3%.',
+  'But testing is limited and it’s impossible to test 100% of a population. By zooming out and looking at the entire population of the state, we can see how a lot of cases were not identified by our testing sample.',
+  'Just like testing, percent positive only shows a subset of reality. In this example, if we had tested a different [sample] of people, the percent positive in our imaginary state would have varied anywhere from 1 to 16%.',
+  'The choice of who gets tested is based on varying state or county-specific criteria, but is often made based on how sick people appear to be, which influences percent positive. If a state only tests people who have clear symptoms of the virus, it will likely have a higher percent positive than one that is also testing asymptomatic people.',
+  'Just like percent positive can paint a biased picture of a specific state’s outbreak, the metric is also imperfect when used to compare two different states or regions. Let’s look at two states with the same percent positive calculated based on the same period of time.',
 ]
 
 export default ({ images }) => {
@@ -26,7 +26,7 @@ export default ({ images }) => {
 
   return (
     <Layout
-      title="Pooled Testing"
+      title="Percent Positive"
       socialCard={{
         description: 'Our most up-to-date data on COVID-19 in the US.',
       }}
