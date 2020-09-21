8a387bd4b5e144c06942025fda476f60f8f15c36
Kevin Miller <keveemiller@gmail.com>
2020-09-21 12:29:54 -0700

    chore: Switch to scroll placeholder

--- src/components/pages/blog-features/scrollytelling/index.js
+++ src/components/pages/blog-features/scrollytelling/index.js
@@ -9,6 +9,27 @@ export default ({ copy, images }) => {
   // data prop of the step, which in this demo stores the index of the step.
   const onStepEnter = props => setCurrentStepIndex(props.data)
 
+  const ScrollPlaceholder = () => {
+    if (typeof window === 'undefined') {
+      return null
+    }
+
+    return (
+      <Scrollama onStepEnter={onStepEnter} offset={0.55}>
+        {images.map((data, index) => (
+          <Step data={index} key={JSON.stringify(data)}>
+            <div className={styles.stepInner}>
+              <span
+                className={styles.text}
+                dangerouslySetInnerHTML={{ __html: copy[index] }}
+              />
+            </div>
+          </Step>
+        ))}
+      </Scrollama>
+    )
+  }
+
   return (
     <div className={styles.scrollyContainer}>
       <div className={styles.stickyImage}>
@@ -17,18 +38,7 @@ export default ({ copy, images }) => {
         )}
       </div>
       <div>
-        <Scrollama onStepEnter={onStepEnter} offset={0.55}>
-          {images.map((data, index) => (
-            <Step data={index} key={JSON.stringify(data)}>
-              <div className={styles.stepInner}>
-                <span
-                  className={styles.text}
-                  dangerouslySetInnerHTML={{ __html: copy[index] }}
-                />
-              </div>
-            </Step>
-          ))}
-        </Scrollama>
+        <ScrollPlaceholder />
         <div className={styles.stepInner} />
       </div>
     </div>
