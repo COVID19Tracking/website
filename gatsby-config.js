d5206f03b087bc680504b553debde1226cc1a6b7
Kevin Miller <keveemiller@gmail.com>
2020-09-22 16:24:36 -0700

    chore: Change environment to default

--- gatsby-config.js
+++ gatsby-config.js
@@ -231,7 +231,6 @@ const gatsbyConfig = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE,
         accessToken: process.env.CONTENTFUL_TOKEN,
-        environment: 'blog-scrollytelling',
         host: process.env.CONTENTFUL_PREVIEW
           ? 'preview.contentful.com'
           : 'cdn.contentful.com',
