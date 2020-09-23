8a3792098ad674a9d60c2b2a3dbafe65688338ed
Kevin Miller <keveemiller@gmail.com>
2020-09-23 15:57:27 -0700

    chore: Remove scrollama placeholder in favor of null

--- src/components/pages/blog-features/scrollytelling/index.js
+++ src/components/pages/blog-features/scrollytelling/index.js
@@ -1,35 +1,26 @@
 import React, { useState } from 'react'
 import { Scrollama, Step } from 'react-scrollama'
 import styles from './scrollytelling.module.scss'
-
 export default ({ copy, images }) => {
   const [currentStepIndex, setCurrentStepIndex] = useState(0)
 
   // This callback fires when a Step hits the offset threshold. It receives the
   // data prop of the step, which in this demo stores the index of the step.
   const onStepEnter = props => setCurrentStepIndex(props.data)
-
-  const ScrollPlaceholder = () => {
-    if (typeof window === 'undefined') {
-      return null
-    }
-
+  if (typeof window === 'undefined') {
     return (
-      <Scrollama onStepEnter={onStepEnter} offset={0.55}>
+      <div>
         {images.map((data, index) => (
-          <Step data={index} key={JSON.stringify(data)}>
-            <div className={styles.stepInner}>
-              <span
-                className={styles.text}
-                dangerouslySetInnerHTML={{ __html: copy[index] }}
-              />
-            </div>
-          </Step>
+          <>
+            <div
+              className={styles.text}
+              dangerouslySetInnerHTML={{ __html: copy[index] }}
+            />
+          </>
         ))}
-      </Scrollama>
+      </div>
     )
   }
-
   return (
     <div className={styles.scrollyContainer}>
       <div className={styles.stickyImage}>
@@ -38,7 +29,18 @@ export default ({ copy, images }) => {
         )}
       </div>
       <div>
-        <ScrollPlaceholder />
+        <Scrollama onStepEnter={onStepEnter} offset={0.55}>
+          {images.map((data, index) => (
+            <Step data={index} key={JSON.stringify(data)}>
+              <div className={styles.stepInner}>
+                <span
+                  className={styles.text}
+                  dangerouslySetInnerHTML={{ __html: copy[index] }}
+                />
+              </div>
+            </Step>
+          ))}
+        </Scrollama>
         <div className={styles.stepInner} />
       </div>
     </div>
