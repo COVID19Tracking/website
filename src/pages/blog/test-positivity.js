e839917506d316f2c806de1cf73cb2b38c739fc6
Kevin Miller <keveemiller@gmail.com>
2020-09-23 14:55:41 -0700

    chore: Copy edit

--- src/pages/blog/test-positivity.js
+++ src/pages/blog/test-positivity.js
@@ -30,16 +30,16 @@ const secondScrollytellingImages = [pooled6, pooled7]
 
 const firstScrollytellingCopy = [
   'Let’s consider a state where a group of 100 people have been tested for COVID-19.',
-  'Out of these 100, only three people received a positive test.',
-  'In this case, considering the <span>sample</span> of people who were tested, we would say the percent positive in this particular population is 3%.',
-  'But testing is limited and it’s impossible to test 100% of a population. By zooming out and looking at the entire population of the state, we can see how a lot of cases were not identified by our testing sample.',
-  'Test positivity reflects the percentage of people tested who have the virus, but not necessarily the percentage of people who have the virus in the entire population. As such, test positivity percent positive only shows a portion of reality. In this example, if we had tested a different <span>sample</span> of people, the test positivity in our imaginary state would have varied anywhere from 1 to 16%.',
-  'The choice of who gets tested is based on varying state or county-specific criteria, but is often made based on how sick people appear to be, which influences test positivity. If a state only tests people who have clear symptoms of the virus, it will likely have a higher percent positive than one that is also testing asymptomatic people.',
+  'Out of these 100, only three people received a positive test result.',
+  'In this case, considering the <span>sample</span> of people who were tested, we would say the test positivity in this particular population is 3 percent.',
+  'But testing is limited in this state, so it’s impossible to test 100 percent of a population. By zooming out and looking at the entire population of the state, we can see how the sample we selected is not an accurate representation of the larger population.',
+  'Test positivity reflects the percentage of <em>people tested</em> who have the virus, but not necessarily the percentage of people who have the virus in the <em>entire population</em>. That means test positivity only shows a portion of reality. In this example, if we had tested a different <span>sample</span> of people, the test positivity in our imaginary state would have varied anywhere from 1 to 16 percent, even though the prevalence is the same.',
+  'The choice of who gets tested is based on state- or county-specific criteria, but is often made based on how sick people appear to be, which in turn influences test positivity. If a state only tests people who have clear symptoms of the virus, it will likely have a higher test positivity than one that is also testing asymptomatic people.',
 ]
 
 const secondScrollytellingCopy = [
-  "Just like test positivity can paint a biased picture of a specific state’s outbreak, it's also imperfect when used to compare two different states or regions. Let’s look at two states with the same test positivity calculated based on the same period of time.",
-  'Even though the sample of people tested in both states shows the same test positivity during the same period of time, the overall prevalence of the virus — something we don’t see in the test results — can be very different.',
+  'Just as test positivity can paint a biased picture of a specific state’s outbreak, it’s also imperfect when used to compare two different states or regions. Let’s look at two states with the same test positivity calculated based on the same period of time.',
+  'Even though the sample of people tested in both states shows the same test positivity, the overall prevalence of the virus—something we don’t see in the test results—can be very different.',
 ]
 
 const TestPositivityBlogPost = ({ data }) => {
@@ -72,8 +72,7 @@ const TestPositivityBlogPost = ({ data }) => {
     )
   }
 
-  const title =
-    'Test Positivity is a Valuable Metric, When Calculated Correctly'
+  const title = 'Test Positivity: So Valuable, So Easy to Misinterpret'
   return (
     <Layout
       title={title}
@@ -93,12 +92,6 @@ const TestPositivityBlogPost = ({ data }) => {
         copy={firstScrollytellingCopy}
         images={firstScrollytellingImages}
       />
-
-      <Container centered>
-        <LongContent>
-          <Snippet slug="tpp-track" />
-        </LongContent>
-      </Container>
 
       <Container centered>
         <h3>Comparing States or Regions</h3>
