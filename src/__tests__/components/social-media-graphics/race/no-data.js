1f4f3b1b62ff6f2703c3fe250b595159abd8bb4b
Andrew Schwartz <andrew@aschwartz.me>
2020-10-13 12:46:08 -0600

    fix(test): mock date for NoData share card

--- src/__tests__/components/social-media-graphics/race/no-data.js
+++ src/__tests__/components/social-media-graphics/race/no-data.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import MockDate from 'mockdate'
 import NoData from '~components/social-media-graphics/race/no-data'
+
+beforeAll(() => {
+  MockDate.set('2020-10-05')
+})
+
+afterAll(() => {
+  MockDate.reset()
+})
 
 describe('Components : Social Media Graphics : Race : No Data', () => {
   it('renders correctly', () => {
