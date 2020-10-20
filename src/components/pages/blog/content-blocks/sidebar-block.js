53f5bf0e485f5b2d171779fde2cf1226c68b0be2
Kevin Miller <keveemiller@gmail.com>
2020-10-20 13:13:14 -0700

    chore: Center sidebar

--- src/components/pages/blog/content-blocks/sidebar-block.js
+++ src/components/pages/blog/content-blocks/sidebar-block.js
@@ -4,19 +4,21 @@ import { Link } from 'gatsby'
 import sidebarStyles from './sidebar-block.module.scss'
 
 const SidebarContentBlock = ({ headline, lede, references }) => (
-  <aside className={sidebarStyles.sidebar}>
-    <h3 className={sidebarStyles.headline}>{headline}</h3>
-    {lede && <div dangerouslySetInnerHTML={{ __html: marked(lede) }} />}
-    <ul>
-      {references.map(reference => (
-        <li key={reference.fields.slug['en-US']}>
-          <Link to={`/blog/${reference.fields.slug['en-US']}`}>
-            {reference.fields.title['en-US']}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </aside>
+  <div className={sidebarStyles.wrapper}>
+    <aside className={sidebarStyles.sidebar}>
+      <h3 className={sidebarStyles.headline}>{headline}</h3>
+      {lede && <div dangerouslySetInnerHTML={{ __html: marked(lede) }} />}
+      <ul>
+        {references.map(reference => (
+          <li key={reference.fields.slug['en-US']}>
+            <Link to={`/blog/${reference.fields.slug['en-US']}`}>
+              {reference.fields.title['en-US']}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  </div>
 )
 
 export default SidebarContentBlock
