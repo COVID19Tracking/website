1b93a022dfe267e6a24f7981aa853d470f2e2ac8
Kevin Miller <keveemiller@gmail.com>
2020-10-21 11:10:18 -0700

    chore: Add charts with res/staff/posres

--- src/components/charts/line-chart.js
+++ src/components/charts/line-chart.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-import PropTypes from 'prop-types' // ES6
 
 import { extent, max } from 'd3-array'
 import { scaleLinear, scaleTime } from 'd3-scale'
@@ -9,19 +8,15 @@ import { timeMonth, timeDay } from 'd3-time'
 import { formatDate, formatNumber } from '~utilities/visualization'
 
 import chartStyles from './charts.module.scss'
-import styles from './bar-chart.module.scss'
 
 import colors from '~scss/colors.module.scss'
 
 const LineChart = ({
   data,
-  annotations,
-  handleAnnotationClick,
-  lineColor,
   marginBottom,
   marginLeft,
   marginRight,
-  marginTop,
+  marginTop = 0,
   showTicks,
   width,
   height,
@@ -30,21 +25,25 @@ const LineChart = ({
   lastXTick,
   perCapLabel,
 }) => {
-  // Used when placing annotations
-  const getValueForDate = date => {
-    const dateData = data.find(d => d.date.getTime() === date.getTime())
-    return dateData && dateData.value
-  }
   const totalXMargin = marginLeft + marginRight
   const totalYMargin = marginTop + marginBottom
-  const dateDomain = extent(data, d => d.date)
-  // Should probably refactor to use a single x-axis scale
-  // but the bars make use of the band.
+
+  const dates = []
+  const values = []
+  data.forEach(item => {
+    item.data.forEach(row => {
+      dates.push(row.date)
+      values.push(row.value)
+    })
+  })
+
+  const dateDomain = extent(dates)
+
   const xScaleTime = scaleTime()
     .domain(dateDomain)
     .range([marginLeft, width - marginRight])
 
-  const yMaxEffective = yMax || max([...data], d => d.value)
+  const yMaxEffective = yMax || max(values)
 
   const yScale = scaleLinear()
     .domain([0, yMaxEffective])
@@ -157,128 +156,22 @@ const LineChart = ({
 
         {/* data */}
         <g transform={`translate(0 ${marginTop})`} mask="url(#dataMask)">
-          {/* bars (data) */}
           {data && (
-            <path
-              d={lineFn(data)}
-              stroke={lineColor}
-              strokeWidth="3"
-              fill="none"
-            />
+            <>
+              {data.map(dataLine => (
+                <path
+                  d={lineFn(dataLine.data)}
+                  stroke={dataLine.color}
+                  strokeWidth={dataLine.stroke}
+                  fill="none"
+                />
+              ))}
+            </>
           )}
         </g>
-        {/* annotations */}
-        {annotations && (
-          <g transform={`translate(0 ${marginTop})`}>
-            {annotations
-              .filter(
-                annotation =>
-                  xScaleTime(annotation.date) >= xScaleTime(dateDomain[0]) &&
-                  xScaleTime(annotation.date) <= xScaleTime(dateDomain[1]),
-              )
-              .map(d => (
-                <svg
-                  key={d}
-                  width="20"
-                  height="24"
-                  x={xScaleTime(d.date) - 11}
-                  y={
-                    yScale(
-                      getValueForDate(d.date) > 10
-                        ? getValueForDate(d.date)
-                        : 10,
-                    ) - 26
-                  }
-                  viewBox="0 0 20 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  onClick={handleAnnotationClick}
-                  style={{ cursor: 'pointer' }}
-                  className={styles.annotationBubbleSvg}
-                  pointerEvents="bounding-box"
-                >
-                  <mask id="path-1-inside-1" fill="white">
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M3 0C1.34315 0 0 1.34314 0 3V16.9412C0 18.5981 1.34315 19.9412 3 19.9412H6.17188L10.2426 24L14.3134 19.9412H17C18.6569 19.9412 20 18.5981 20 16.9412V3C20 1.34315 18.6569 0 17 0H3Z"
-                    />
-                  </mask>
-
-                  <rect
-                    x="0"
-                    y="0"
-                    height="32"
-                    width="20"
-                    className={styles.annotationBubbleSvgFill}
-                    mask="url(#path-1-inside-1)"
-                  />
-                  <path
-                    d="M6.17188 19.9412L6.87794 19.2331L6.58523 18.9412H6.17188V19.9412ZM10.2426 24L9.53658 24.7081L10.2426 25.4121L10.9487 24.7081L10.2426 24ZM14.3134 19.9412V18.9412H13.9001L13.6073 19.2331L14.3134 19.9412ZM1 3C1 1.89543 1.89543 1 3 1V-1C0.790863 -1 -1 0.790856 -1 3H1ZM1 16.9412V3H-1V16.9412H1ZM3 18.9412C1.89543 18.9412 1 18.0458 1 16.9412H-1C-1 19.1503 0.790861 20.9412 3 20.9412V18.9412ZM6.17188 18.9412H3V20.9412H6.17188V18.9412ZM10.9487 23.2919L6.87794 19.2331L5.46581 20.6493L9.53658 24.7081L10.9487 23.2919ZM13.6073 19.2331L9.53658 23.2919L10.9487 24.7081L15.0195 20.6493L13.6073 19.2331ZM17 18.9412H14.3134V20.9412H17V18.9412ZM19 16.9412C19 18.0458 18.1046 18.9412 17 18.9412V20.9412C19.2091 20.9412 21 19.1503 21 16.9412H19ZM19 3V16.9412H21V3H19ZM17 1C18.1046 1 19 1.89543 19 3H21C21 0.790861 19.2091 -1 17 -1V1ZM3 1H17V-1H3V1Z"
-                    fill="black"
-                    mask="url(#path-1-inside-1)"
-                  />
-                  <text
-                    fill="black"
-                    x="9.5"
-                    y="15"
-                    textAnchor="middle"
-                    style={{ fontSize: 13, fontWeight: 700 }}
-                  >
-                    {d.annotationSymbol}
-                  </text>
-                </svg>
-              ))}
-          </g>
-        )}
       </svg>
     </>
   )
 }
 
-LineChart.defaultProps = {
-  lineColor: 'black',
-  annotations: [],
-  handleAnnotationClick: null,
-  marginBottom: 0,
-  marginLeft: 0,
-  marginRight: 0,
-  marginTop: 0,
-  width: 300,
-  height: 300,
-  yMax: null,
-  yTicks: null,
-  showTicks: 4,
-  perCapLabel: null,
-}
-
-LineChart.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      date: PropTypes.instanceOf(Date).isRequired,
-      /* value isn't required: state's may have started reporting a value
-      _after_ the start date. (i.e. hospitalization values that have only
-      been reported for the past 5 days) */
-    }),
-  ).isRequired,
-  annotations: PropTypes.arrayOf(
-    PropTypes.shape({
-      annotationSymbol: PropTypes.string.isRequired,
-      date: PropTypes.instanceOf(Date).isRequired,
-      value: PropTypes.number,
-    }),
-  ),
-  handleAnnotationClick: PropTypes.func, // also requires chart Annotations to be present at #chart-annotations
-  lineColor: PropTypes.string,
-  width: PropTypes.number,
-  height: PropTypes.number,
-  marginBottom: PropTypes.number,
-  marginLeft: PropTypes.number,
-  marginRight: PropTypes.number,
-  marginTop: PropTypes.number,
-  showTicks: PropTypes.number,
-  yMax: PropTypes.number,
-  yTicks: PropTypes.number,
-  perCapLabel: PropTypes.string,
-}
 export default LineChart
--- src/components/pages/state/long-term-care/chart.js
+++ src/components/pages/state/long-term-care/chart.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react'
 import { DateTime } from 'luxon'
 import { Row, Col } from '~components/common/grid'
@@ -5,6 +6,10 @@ import LineChart from '~components/charts/line-chart'
 import TooltipContents from '~components/charts/tooltip-contents'
 import colors from '~scss/colors.module.scss'
 import chartStyles from './chart.module.scss'
+
+const residentColor = colors.colorHoney500
+const staffColor = colors.colorPlum600
+const staffResColor = colors.colorSlate300
 
 const makeRenderTooltipContents = text => d => (
   <TooltipContents
@@ -28,46 +33,146 @@ const chartProps = {
   showTicks: 6,
 }
 
-const LongTermCareBarChart = ({ data }) => {
-  const chartData = data.map(item => ({
-    date: DateTime.fromISO(item.date).toJSDate(),
-    deaths:
-      item.deathres_other +
-      item.deathres_nh +
-      item.deathres_ltc +
-      item.deathres_alf,
-    cases:
-      item.posres_other + item.posres_nh + item.posres_ltc + item.posres_alf,
-  }))
+const LongTermCareCharts = ({ data }) => {
+  const caseData = [
+    {
+      color: residentColor,
+      stroke: 2,
+      label: 'Resident',
+      data: [],
+    },
+    {
+      color: staffColor,
+      stroke: 2,
+      label: 'Staff',
+      data: [],
+    },
+    {
+      color: staffResColor,
+      stroke: 2,
+      label: 'Staff &amp; residents',
+      data: [],
+    },
+  ]
+  const deathData = [
+    {
+      color: residentColor,
+      stroke: 2,
+      label: 'Resident',
+      data: [],
+    },
+    {
+      color: staffColor,
+      stroke: 2,
+      label: 'Staff',
+      data: [],
+    },
+    {
+      color: staffResColor,
+      stroke: 2,
+      label: 'Staff &amp; residents',
+      data: [],
+    },
+  ]
+  data.forEach(item => {
+    const date = DateTime.fromISO(item.date).toJSDate()
+    caseData[0].data.push({
+      date,
+      value:
+        item.posres_other + item.posres_nh + item.posres_ltc + item.posres_alf,
+    })
+    caseData[1].data.push({
+      date,
+      value:
+        item.posstaff_other +
+        item.posstaff_nh +
+        item.posstaff_ltc +
+        item.posstaff_alf,
+    })
+    caseData[2].data.push({
+      date,
+      value:
+        item.posresstaff_other +
+        item.posresstaff_nh +
+        item.posresstaff_ltc +
+        item.posresstaff_alf,
+    })
+
+    deathData[0].data.push({
+      date,
+      value:
+        item.deathres_other +
+        item.deathres_nh +
+        item.deathres_ltc +
+        item.deathres_alf,
+    })
+    deathData[1].data.push({
+      date,
+      value:
+        item.deathstaff_other +
+        item.deathstaff_nh +
+        item.deathstaff_ltc +
+        item.deathstaff_alf,
+    })
+    deathData[2].data.push({
+      date,
+      value:
+        item.deathresstaff_other +
+        item.deathresstaff_nh +
+        item.deathresstaff_ltc +
+        item.deathresstaff_alf,
+    })
+  })
 
   return (
-    <Row className={chartStyles.charts}>
-      <Col width={[4, 3, 4]}>
-        <h3>Cases</h3>
-        <LineChart
-          data={chartData.map(({ date, cases }) => ({
-            date,
-            value: cases,
-          }))}
-          lineColor={colors.colorStrawberry100}
-          renderTooltipContents={makeRenderTooltipContents('Cases')}
-          {...chartProps}
-        />
-      </Col>
-      <Col width={[4, 3, 4]}>
-        <h3>Deaths</h3>
-        <LineChart
-          data={chartData.map(({ date, deaths }) => ({
-            date,
-            value: deaths,
-          }))}
-          lineColor={colors.colorSlate300}
-          renderTooltipContents={makeRenderTooltipContents('Deaths')}
-          {...chartProps}
-        />
-      </Col>
-    </Row>
+    <>
+      <Row className={chartStyles.charts}>
+        <Col width={[4, 3, 4]}>
+          <h3>Cases</h3>
+          <LineChart
+            data={caseData}
+            lineColor={colors.colorStrawberry100}
+            renderTooltipContents={makeRenderTooltipContents('Cases')}
+            {...chartProps}
+          />
+        </Col>
+        <Col width={[4, 3, 4]}>
+          <h3>Deaths</h3>
+          {
+            <LineChart
+              data={deathData}
+              lineColor={colors.colorSlate300}
+              renderTooltipContents={makeRenderTooltipContents('Deaths')}
+              {...chartProps}
+            />
+          }
+        </Col>
+      </Row>
+      <ul className={chartStyles.legend} aria-hidden>
+        <li>
+          <span
+            className={chartStyles.legendColor}
+            style={{ background: residentColor }}
+          />{' '}
+          Residents
+        </li>
+        <li>
+          <span
+            className={chartStyles.legendColor}
+            style={{ background: staffColor }}
+          />{' '}
+          Staff
+        </li>
+        <li>
+          <span
+            className={chartStyles.legendColor}
+            style={{ background: staffResColor }}
+          />{' '}
+          Staff &amp; residents
+        </li>
+      </ul>
+    </>
   )
 }
 
-export default LongTermCareBarChart
+export default LongTermCareCharts
