d81fd95ec23341a66c84b1f72ce0415acc333e33
Kevin Miller <keveemiller@gmail.com>
2020-10-20 17:04:29 -0700

    chore: Remove unneeded wrapper

--- src/components/pages/blog/content-blocks/related-posts-block.js
+++ src/components/pages/blog/content-blocks/related-posts-block.js
@@ -4,21 +4,19 @@ import { Link } from 'gatsby'
 import relatedPostStyles from './related-posts-block.module.scss'
 
 const RelatedPostsContentBlock = ({ headline, lede, references }) => (
-  <div className={relatedPostStyles.wrapper}>
-    <aside className={relatedPostStyles.pullout}>
-      <h3 className={relatedPostStyles.headline}>{headline}</h3>
-      {lede && <div dangerouslySetInnerHTML={{ __html: marked(lede) }} />}
-      <ul>
-        {references.map(reference => (
-          <li key={reference.fields.slug['en-US']}>
-            <Link to={`/blog/${reference.fields.slug['en-US']}`}>
-              {reference.fields.title['en-US']}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </aside>
-  </div>
+  <aside className={relatedPostStyles.pullout}>
+    <h3 className={relatedPostStyles.headline}>{headline}</h3>
+    {lede && <div dangerouslySetInnerHTML={{ __html: marked(lede) }} />}
+    <ul>
+      {references.map(reference => (
+        <li key={reference.fields.slug['en-US']}>
+          <Link to={`/blog/${reference.fields.slug['en-US']}`}>
+            {reference.fields.title['en-US']}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </aside>
 )
 
 export default RelatedPostsContentBlock
