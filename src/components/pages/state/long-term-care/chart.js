ca0d44bef210bc00e5c8c49eae5508563c30c125
Kevin Miller <keveemiller@gmail.com>
2020-10-26 19:32:03 -0700

    feat: Switch to total death/case charts

--- src/components/pages/state/long-term-care/chart.js
+++ src/components/pages/state/long-term-care/chart.js
@@ -8,10 +8,6 @@ import colors from '~scss/colors.module.scss'
 import chartStyles from './chart.module.scss'
 import Alert from '~components/common/alert'
 
-const residentColor = colors.colorHoney500
-const staffColor = colors.colorPlum600
-const staffResColor = colors.colorSlate300
-
 const makeRenderTooltipContents = text => d => (
   <TooltipContents
     date={d.date}
@@ -35,124 +31,35 @@ const chartProps = {
 }
 
 const LongTermCareCharts = ({ data }) => {
-  const displayLegend = {
-    staff: false,
-    residents: false,
-    staffResidents: false,
-  }
-
   const displayCharts = {
     cases: false,
     deaths: false,
   }
 
-  const caseData = [
-    {
-      color: residentColor,
-      stroke: 2,
-      label: 'Resident',
-      data: [],
-    },
-    {
-      color: staffColor,
-      stroke: 2,
-      label: 'Staff',
-      data: [],
-    },
-    {
-      color: staffResColor,
-      stroke: 2,
-      label: 'Staff &amp; residents',
-      data: [],
-    },
-  ]
-  const deathData = [
-    {
-      color: residentColor,
-      stroke: 2,
-      label: 'Resident',
-      data: [],
-    },
-    {
-      color: staffColor,
-      stroke: 2,
-      label: 'Staff',
-      data: [],
-    },
-    {
-      color: staffResColor,
-      stroke: 2,
-      label: 'Staff &amp; residents',
-      data: [],
-    },
-  ]
+  const caseData = []
+  const deathData = []
+
   data.forEach(item => {
     const date = DateTime.fromISO(item.date).toJSDate()
-    caseData[0].data.push({
-      date,
-      value:
-        item.posres_other + item.posres_nh + item.posres_ltc + item.posres_alf,
+    let totalCases = 0
+    let totalDeath = 0
+    Object.keys(item).forEach(key => {
+      if (key.search(/posres|posstaff/) > -1) {
+        totalCases += item[key]
+      }
+      if (key.search(/deathres|deathstaff/) > -1) {
+        totalDeath += item[key]
+      }
     })
-    caseData[1].data.push({
+    caseData.push({
       date,
-      value:
-        item.posstaff_other +
-        item.posstaff_nh +
-        item.posstaff_ltc +
-        item.posstaff_alf,
+      value: totalCases,
     })
-    caseData[2].data.push({
+    deathData.push({
       date,
-      value:
-        item.posresstaff_other +
-        item.posresstaff_nh +
-        item.posresstaff_ltc +
-        item.posresstaff_alf,
-    })
-
-    deathData[0].data.push({
-      date,
-      value:
-        item.deathres_other +
-        item.deathres_nh +
-        item.deathres_ltc +
-        item.deathres_alf,
-    })
-    deathData[1].data.push({
-      date,
-      value:
-        item.deathstaff_other +
-        item.deathstaff_nh +
-        item.deathstaff_ltc +
-        item.deathstaff_alf,
-    })
-    deathData[2].data.push({
-      date,
-      value:
-        item.deathresstaff_other +
-        item.deathresstaff_nh +
-        item.deathresstaff_ltc +
-        item.deathresstaff_alf,
+      value: totalDeath,
     })
   })
-
-  const setDisplay = (chart, type) => {
-    if (chart[0].data.filter(item => item.value).length > 0) {
-      displayLegend.residents = true
-      displayCharts[type] = true
-    }
-    if (chart[1].data.filter(item => item.value).length > 0) {
-      displayLegend.staff = true
-      displayCharts[type] = true
-    }
-    if (chart[2].data.filter(item => item.value).length > 0) {
-      displayLegend.staffResidents = true
-      displayCharts[type] = true
-    }
-  }
-
-  setDisplay(caseData, 'cases')
-  setDisplay(deathData, 'deaths')
 
   return (
     <>
@@ -160,10 +67,16 @@ const LongTermCareCharts = ({ data }) => {
         <Col width={[4, 3, 4]}>
           <h3>Cases</h3>
 
-          {displayCharts.cases ? (
+          {caseData.filter(item => item.value).length ? (
             <LineChart
-              data={caseData}
-              lineColor={colors.colorStrawberry100}
+              data={[
+                {
+                  color: colors.colorStrawberry200,
+                  stroke: 2,
+                  label: 'Cases',
+                  data: caseData,
+                },
+              ]}
               renderTooltipContents={makeRenderTooltipContents('Cases')}
               {...chartProps}
             />
@@ -173,10 +86,16 @@ const LongTermCareCharts = ({ data }) => {
         </Col>
         <Col width={[4, 3, 4]}>
           <h3>Deaths</h3>
-          {displayCharts.deaths ? (
+          {deathData.filter(item => item.value).length ? (
             <LineChart
-              data={deathData}
-              lineColor={colors.colorSlate300}
+              data={[
+                {
+                  color: colors.colorSlate700,
+                  stroke: 2,
+                  label: 'Deaths',
+                  data: deathData,
+                },
+              ]}
               renderTooltipContents={makeRenderTooltipContents('Deaths')}
               {...chartProps}
             />
@@ -185,35 +104,6 @@ const LongTermCareCharts = ({ data }) => {
           )}
         </Col>
       </Row>
-      <ul className={chartStyles.legend} aria-hidden>
-        {displayLegend.residents && (
-          <li>
-            <span
-              className={chartStyles.legendColor}
-              style={{ background: residentColor }}
-            />{' '}
-            Residents
-          </li>
-        )}
-        {displayLegend.staff && (
-          <li>
-            <span
-              className={chartStyles.legendColor}
-              style={{ background: staffColor }}
-            />{' '}
-            Staff
-          </li>
-        )}
-        {displayLegend.staffResidents && (
-          <li>
-            <span
-              className={chartStyles.legendColor}
-              style={{ background: staffResColor }}
-            />{' '}
-            Staff &amp; residents
-          </li>
-        )}
-      </ul>
     </>
   )
 }
