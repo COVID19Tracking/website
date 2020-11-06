6f8e372f726e484d62ba4792b4bbf79a38442987
Kevin Miller <keveemiller@gmail.com>
2020-11-06 14:36:49 -0800

    chore: no need for colon here

--- src/components/pages/data/cards/definitions-panel.js
+++ src/components/pages/data/cards/definitions-panel.js
@@ -61,7 +61,7 @@ const Annotation = ({ key, annotation, highlighted }) => {
         ))}
       </h3>
       <p>{annotation.warning}</p>
-      {annotation.lastChecked && <p>Last updated: {annotation.lastChecked}</p>}
+      {annotation.lastChecked && <p>Last updated {annotation.lastChecked}</p>}
     </div>
   )
 }
