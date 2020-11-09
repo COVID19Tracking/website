229dc66d59200d9a8adc3f28cf1ef93959ab5f65
Kevin Miller <keveemiller@gmail.com>
2020-11-09 09:32:19 -0800

    chore: Add padding to preamble

--- src/components/common/overview-wrapper.js
+++ src/components/common/overview-wrapper.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import classnames from 'classnames'
 import overviewWrapperStyles from './overview-wrapper.module.scss'
 
-const OverviewWrapper = ({ children }) => (
-  <div className={overviewWrapperStyles.wrapper}>{children}</div>
+const OverviewWrapper = ({ children, className = false }) => (
+  <div
+    className={classnames(
+      overviewWrapperStyles.wrapper,
+      className && className,
+    )}
+  >
+    {children}
+  </div>
 )
 
 export default OverviewWrapper
