3f4981145a47290f2b3840c6a65916a405d03c2d
Kevin Miller <keveemiller@gmail.com>
2020-11-16 11:23:20 -0800

    chore: Remove recovered, deaths

--- src/pages/data/national/deaths.js
+++ src/pages/data/national/deaths.js
@@ -4,13 +4,13 @@ import TableResponsive from '~components/common/table-responsive'
 import Definitions from '~components/pages/data/definitions'
 import Layout from '~components/layout'
 
-const NationalDataOutcomesPage = ({ data }) => {
+const NationalDataDeathsPage = ({ data }) => {
   return (
     <Layout
-      title="National: Outcomes"
+      title="National: Deaths"
       returnLinkTitle="Our Data"
       returnLink="/data"
-      path="/data/national/outcomes"
+      path="/data/national/deaths"
       returnLinks={[
         { link: '/data' },
         { link: `/data/national`, title: 'Totals for the US' },
@@ -18,16 +18,12 @@ const NationalDataOutcomesPage = ({ data }) => {
     >
       <Definitions
         definitions={data.allContentfulDataDefinition.nodes}
-        order={['recovered', 'death']}
+        order={['death']}
       />
       <TableResponsive
         labels={[
           {
             field: 'date',
-          },
-          {
-            field: 'recovered',
-            isNumeric: true,
           },
           {
             field: 'death',
@@ -44,7 +40,7 @@ const NationalDataOutcomesPage = ({ data }) => {
   )
 }
 
-export default NationalDataOutcomesPage
+export default NationalDataDeathsPage
 
 export const query = graphql`
   {
@@ -53,12 +49,11 @@ export const query = graphql`
         date(formatString: "MMM D, YYYY")
         death
         deathIncrease
-        recovered
       }
     }
     allContentfulDataDefinition(
       sort: { fields: name }
-      filter: { fieldName: { in: ["recovered", "death"] } }
+      filter: { fieldName: { in: ["death"] } }
     ) {
       nodes {
         name
