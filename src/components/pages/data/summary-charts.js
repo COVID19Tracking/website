6334ba1c5206c80865c7ad33a7798f4deb637a0e
Kevin Miller <keveemiller@gmail.com>
2020-11-17 05:20:40 -0800

    chore: Fix last-tick error

--- src/components/pages/data/summary-charts.js
+++ src/components/pages/data/summary-charts.js
@@ -274,7 +274,7 @@ const SummaryCharts = ({
     }  COVID-19 ${field} for at least 30% of the past 90 days.`
 
   const showTodaysChartTick =
-    DateTime.fromISO(data[data.length - 1].date).day >= 15
+    DateTime.fromISO(data[data.length - 1].date).day >= 20
 
   const chartProps = {
     height: 280, // these control the dimensions used to render the svg but not the final size
