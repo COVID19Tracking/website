3e92a36921665ccf020195be63d56c3355cc144a
Kevin Miller <keveemiller@gmail.com>
2020-11-24 16:49:32 -0800

    chore: Add thanksgiving warning

--- src/templates/state/index.js
+++ src/templates/state/index.js
@@ -23,7 +23,12 @@ const StateTemplate = ({ pageContext, data, path }) => {
     allCovidAnnotation,
   } = data
   return (
-    <Layout title={state.name} returnLinks={[{ link: '/data' }]} path={path}>
+    <Layout
+      title={state.name}
+      returnLinks={[{ link: '/data' }]}
+      path={path}
+      showWarning
+    >
       <StatePreamble state={state} covidState={covidState} />
       <SummaryCharts
         name={state.name}
