f6612c739eeba14563e86e2101cf5ca33f85a6cc
Kevin Miller <keveemiller@gmail.com>
2020-11-25 11:57:33 -0800

    chore: Add carousel

--- src/components/pages/data/charts/chart-list.js
+++ src/components/pages/data/charts/chart-list.js
@@ -1,7 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
-import { Row, Col } from '~components/common/grid'
+import classnames from 'classnames'
 import chartListStyles from './chart-list.module.scss'
+
+const ChartCarousel = ({ charts }) => {
+  const [activeItem, setActiveItem] = useState(0)
+  const carouselLength = 5
+
+  const updateActiveItem = add => {
+    if (
+      typeof charts[activeItem + add + carouselLength] !== 'undefined' &&
+      activeItem + add >= 0
+    ) {
+      setActiveItem(activeItem + add)
+    }
+  }
+  return (
+    <ul className={chartListStyles.carousel}>
+      {charts.length > carouselLength && (
+        <li className={chartListStyles.navigation}>
+          <button
+            type="button"
+            aria-hidden
+            onClick={event => {
+              event.preventDefault()
+              updateActiveItem(-1)
+            }}
+          >
+            ←<span className="a11y-only">Prior charts</span>
+          </button>
+        </li>
+      )}
+      {charts.map((item, index) => (
+        <li
+          className={classnames(
+            chartListStyles.chart,
+            index >= activeItem &&
+              index < activeItem + carouselLength &&
+              chartListStyles.active,
+          )}
+        >
+          <Link to={`/data/charts/${item.slug}`}>
+            {item.thumbnail && (
+              <img src={item.thumbnail.resize.src} alt="" aria-hidden />
+            )}
+          </Link>
+          <Link to={`/data/charts/${item.slug}`}>{item.title}</Link>
+        </li>
+      ))}
+      {charts.length > carouselLength && (
+        <li className={chartListStyles.navigation}>
+          <button
+            type="button"
+            aria-hidden
+            onClick={event => {
+              event.preventDefault()
+              updateActiveItem(1)
+            }}
+          >
+            →<span className="a11y-only">Next charts</span>
+          </button>
+        </li>
+      )}
+    </ul>
+  )
+}
 
 const DataChartsPage = () => {
   const data = useStaticQuery(graphql`
@@ -31,22 +94,7 @@ const DataChartsPage = () => {
           {charts && charts.length > 0 && (
             <>
               <h3>{name}</h3>
-              <Row className={chartListStyles.list}>
-                {charts.map(item => (
-                  <Col width={[2, 2, 2]}>
-                    <Link to={`/data/charts/${item.slug}`}>
-                      {item.thumbnail && (
-                        <img
-                          src={item.thumbnail.resize.src}
-                          alt=""
-                          aria-hidden
-                        />
-                      )}
-                    </Link>
-                    <Link to={`/data/charts/${item.slug}`}>{item.title}</Link>
-                  </Col>
-                ))}
-              </Row>
+              <ChartCarousel charts={charts} />
             </>
           )}
         </>
