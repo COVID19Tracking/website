a8f522f21a5ef871190f17d6195358e11245f58f
Kevin Miller <keveemiller@gmail.com>
2020-11-30 11:02:38 -0800

    chore(a11y): Add alert role

--- src/components/layout/header/data-warning.js
+++ src/components/layout/header/data-warning.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import marked from 'marked'
 import Container from '~components/common/container'
-import dataWarningStyle from './thanksgiving-warning.module.scss'
+import dataWarningStyle from './data-warning.module.scss'
 
 const DataWarning = () => {
   const data = useStaticQuery(graphql`
@@ -26,7 +26,15 @@ const DataWarning = () => {
   return (
     <div className={dataWarningStyle.warning}>
       <Container>
-        {marked.inlineLexer(data.contentfulSnippet.content.content, [])}
+        <p
+          role="alert"
+          dangerouslySetInnerHTML={{
+            __html: marked.inlineLexer(
+              data.contentfulSnippet.content.content,
+              [],
+            ),
+          }}
+        />
       </Container>
     </div>
   )
