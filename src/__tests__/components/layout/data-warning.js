4ffb5323e6443ba0fab1769448d232ca40e41a98
Kevin Miller <keveemiller@gmail.com>
2020-12-02 10:06:40 -0800

    chore(test): Fix mock of useStaticQuery

--- src/__tests__/components/layout/data-warning.js
+++ src/__tests__/components/layout/data-warning.js
@@ -1,40 +1,43 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import * as Gatsby from 'gatsby'
 import { StaticQuery } from 'gatsby'
 import DataWarning from '~components/layout/header/data-warning'
 
+const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery')
+
 describe('Components : Layout : Data warning', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
   it('is hidden if the snippet is empty', () => {
-    StaticQuery.mockImplementationOnce(({ render }) =>
-      render({
-        contentfulSnippet: {
-          contentful_id: '1234',
-          name: 'data-warning',
-          slug: 'data-warning',
-          content: {
-            content: '',
-          },
+    useStaticQuery.mockImplementation(() => ({
+      contentfulSnippet: {
+        contentful_id: '1234',
+        name: 'data-warning',
+        slug: 'data-warning',
+        content: {
+          content: '',
         },
-      }),
-    )
+      },
+    }))
     const tree = renderer.create(<DataWarning />).toJSON()
     expect(tree).toBeNull()
   })
 
   it('is displayed if there is content', () => {
-    StaticQuery.mockImplementationOnce(({ render }) =>
-      render({
-        contentfulSnippet: {
-          contentful_id: '1234',
-          name: 'data-warning',
-          slug: 'data-warning',
-          content: {
-            content: 'There is a data issue and **we are on it**.',
-          },
+    useStaticQuery.mockImplementation(() => ({
+      contentfulSnippet: {
+        contentful_id: '1234',
+        name: 'data-warning',
+        slug: 'data-warning',
+        content: {
+          content: 'There are some data issues and **we are on it**.',
         },
-      }),
-    )
+      },
+    }))
     const tree = renderer.create(<DataWarning />).toJSON()
+    expect(tree).not.toBeNull()
     expect(tree).toMatchSnapshot()
   })
 })
--- src/components/layout/header/data-warning.js
+++ src/components/layout/header/data-warning.js
@@ -16,6 +16,7 @@ const DataWarning = () => {
       }
     }
   `)
+
   if (
     !data ||
     !data.contentfulSnippet ||
