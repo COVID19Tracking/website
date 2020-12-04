6debd49c040b89bfd1ea487a6f2a1b973211b000
Kevin Miller <keveemiller@gmail.com>
2020-12-04 12:25:02 -0800

    chore: Add image link

--- src/components/pages/blog/content-blocks/image-content-block.js
+++ src/components/pages/blog/content-blocks/image-content-block.js
@@ -4,22 +4,8 @@ import Img from 'gatsby-image'
 import ImageCredit from '~components/common/image-credit'
 import imageContentBlockStyles from './image-content-block.module.scss'
 
-const ImageContentBlock = ({
-  caption,
-  image,
-  className,
-  keepSize = false,
-  fullWidthMobile = false,
-  imageUrl,
-}) => (
-  <div
-    className={classnames(
-      className,
-      imageContentBlockStyles.image,
-      fullWidthMobile && imageContentBlockStyles.fullWidthMobile,
-      keepSize && imageContentBlockStyles.keepSize,
-    )}
-  >
+const ImageBlock = ({ imageUrl, image, keepSize }) => (
+  <>
     {keepSize ? (
       <img
         src={imageUrl}
@@ -34,6 +20,34 @@ const ImageContentBlock = ({
         alt={image.description}
       />
     )}
+  </>
+)
+
+const ImageContentBlock = ({
+  caption,
+  image,
+  className,
+  keepSize = false,
+  fullWidthMobile = false,
+  imageUrl,
+  linkToImage = false,
+}) => (
+  <div
+    className={classnames(
+      className,
+      imageContentBlockStyles.image,
+      fullWidthMobile && imageContentBlockStyles.fullWidthMobile,
+      keepSize && imageContentBlockStyles.keepSize,
+    )}
+  >
+    {linkToImage ? (
+      <a href={imageUrl}>
+        <ImageBlock image={image} imageUrl={imageUrl} keepSize={keepSize} />
+      </a>
+    ) : (
+      <ImageBlock image={image} imageUrl={imageUrl} keepSize={keepSize} />
+    )}
+
     {caption && <ImageCredit>{caption['en-US']}</ImageCredit>}
   </div>
 )
