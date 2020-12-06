409526550c0ddaed0e125161ab5f0db3045d1341
Kevin Miller <keveemiller@gmail.com>
2020-12-05 22:05:43 -0800

    chore: Make sub-element in HHS card

--- src/components/pages/data/cards/hospitalization-hhs-card.js
+++ src/components/pages/data/cards/hospitalization-hhs-card.js
@@ -9,7 +9,11 @@ const HospitalizationHHSCard = ({ hhsHospitalization }) => {
     <Card title="HHS Hospitalization">
       <CardBody>
         <Statistic
-          title="Now hospitalized, confirmed"
+          title="Now hospitalized (confirmed + suspected)"
+          value={parseInt(hhsHospitalization.inpatient_beds_used_covid, 10)}
+        />
+        <Statistic
+          title="Now hospitalized (confirmed only)"
           value={
             parseInt(
               hhsHospitalization.total_adult_patients_hospitalized_confirmed_covid,
@@ -20,13 +24,10 @@ const HospitalizationHHSCard = ({ hhsHospitalization }) => {
               10,
             )
           }
+          subelement
         />
         <Statistic
-          title="Now hospitalized, suspected"
-          value={parseInt(hhsHospitalization.inpatient_beds_used_covid, 10)}
-        />
-        <Statistic
-          title="Now in ICU"
+          title="Now in ICU  (confirmed + suspected)"
           value={parseInt(
             hhsHospitalization.staffed_icu_adult_patients_confirmed_and_suspected_covid,
             10,
