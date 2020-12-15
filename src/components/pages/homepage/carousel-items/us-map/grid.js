f2ab8d9d1a2abc32eb4fbb24559ebb1df89a8b7b
Kevin Miller <keveemiller@gmail.com>
2020-12-14 16:09:05 -0800

    chore: Add US grid

--- src/components/pages/homepage/carousel-items/us-map/grid.js
+++ src/components/pages/homepage/carousel-items/us-map/grid.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import { navigate } from 'gatsby'
-import { State } from './states'
+import { State, US } from './states'
 import createGrid from './create-grid'
 import { mobileGrid } from './state-matrix'
 import mapStyle from './us-map.module.scss'
 
-const Grid = ({ states, metric }) => {
-  const { stateHexes, width, height } = createGrid(states, mobileGrid, true)
+const Grid = ({ states, us, metric }) => {
+  const { stateHexes, width, height, hexRad } = createGrid(
+    states,
+    mobileGrid,
+    true,
+  )
   return (
     <div className={mapStyle.gridWrapper}>
       <h3>{metric.title}</h3>
       <svg
         className={mapStyle.grid}
         style={{ width: '100%', height: '100%' }}
-        viewBox={`0 -50 ${width} ${height}`}
+        viewBox={`0 -10 ${width} ${height}`}
         tabIndex="0"
         aria-hidden
       >
+        <US
+          r={hexRad * 1.5}
+          value={us.value}
+          className={value => metric.getColor(value)}
+          onClick={() => {
+            navigate('/data/national')
+          }}
+        />
         {stateHexes.map(({ x, y, r, state }) => (
           <State
             x={x}
-            y={y}
+            y={y + hexRad * 4}
             r={r}
             state={state}
             onClick={() => {
