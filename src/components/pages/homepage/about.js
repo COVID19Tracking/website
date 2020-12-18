a382cffc3eaa6ed149f5001f0e0729ca1b2ac1cb
Kevin Miller <keveemiller@gmail.com>
2020-12-17 17:25:14 -0800

    chore: Rename about link

--- src/components/pages/homepage/about.js
+++ src/components/pages/homepage/about.js
@@ -6,8 +6,8 @@ const HomepageAbout = () => (
   <HomepageWrapper
     title="The COVID Tracking Project collects and publishes the most complete data about COVID-19 in the US."
     links={[
-      <Link to="/data">Our data</Link>,
-      <Link to="/about-data">About our data</Link>,
+      <Link to="/data">See the data</Link>,
+      <Link to="/about-data">About the data</Link>,
       <Link to="/blog">Our analysis &amp; updates</Link>,
     ]}
   />
