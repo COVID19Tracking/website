9fcb86352bc9555b4a9dce1204f28997efcb38c2
Kevin Miller <keveemiller@gmail.com>
2020-12-19 18:18:11 -0800

    chore(a11y): Remove unneeded tags. Fixes #1693

--- src/components/pages/homepage/press.js
+++ src/components/pages/homepage/press.js
@@ -24,17 +24,11 @@ const HomepagePress = () => {
     <section className={pressStyles.section}>
       <h2 className={pressStyles.header}>Who’s using the data</h2>
       <div className={pressStyles.logos}>
-        <p className="a11y-only">
-          News organizations including The New York Times, The Washington Post,
-          CNN, Vox, ProPublica, and The Wall Street Journal use our data in
-          their reporting.
-        </p>
         {data.allHomepagePressYaml.nodes.map(({ name, logo }) => (
           <img
             key={`homepage-press-${logo}`}
             alt={`${name} logo`}
             src={data.allFile.nodes.find(file => file.name === logo).publicURL}
-            aria-hidden
           />
         ))}
       </div>
