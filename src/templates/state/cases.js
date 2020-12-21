5264ba486100b897f82145b7ec07d5e431c4f1b5
Andrew Schwartz <andrew@aschwartz.me>
2020-12-21 13:51:23 -0700

    feat(cases): add annotations panel

--- src/templates/state/cases.js
+++ src/templates/state/cases.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { graphql } from 'gatsby'
 import TableResponsive from '~components/common/table-responsive'
 import Definitions from '~components/pages/data/definitions'
 import Layout from '~components/layout'
+import {
+  DefinitionPanel,
+  AnnotationPanelContext,
+  AnnotationButton,
+} from '~components/pages/data/cards/definitions-panel'
+import { DefinitionLink } from '~components/common/statistic'
+
+const AnnotationIcon = ({ annotation, annotationFields }) => {
+  const annotationContext = useContext(AnnotationPanelContext)
+
+  // todo stop using definition link
+  return (
+    <AnnotationButton field={annotation.date}>
+      <DefinitionLink
+        title="Warning"
+        onDefinitionsToggle={() => {
+          annotationContext.setCardAnnotations({
+            fields: annotationFields,
+            highlight: annotation.date,
+          })
+        }}
+        label="Annotation for Total Tests (in unclear units)"
+      />
+    </AnnotationButton>
+  )
+}
 
 const StateCasesTemplate = ({ pageContext, path, data }) => {
   const state = pageContext
@@ -11,16 +37,25 @@ const StateCasesTemplate = ({ pageContext, path, data }) => {
   const annotations = data.allContentfulChartAnnotation.nodes
   const dataRows = data.allCovidStateDaily.nodes.slice(0, 10) // todo remove me
 
-  // Match annotations with their respective days.
-  annotations.forEach(annotation => {
+  const annotationDates = annotations.map(annotation => annotation.date) // A list of the dates with annotations.
+
+  annotations.forEach((annotation, index) => {
+    // Standardize the content for the DefinitionsPanel
+    annotations[index].field = annotation.date
+    annotations[index].warning = annotation.description.description // todo use childMarkdownRemark here
+
+    // Match annotations with their respective days.
     const row = dataRows.findIndex(r => r.date === annotation.date)
-    console.log(dataRows[row])
-    // todo use childMarkdownRemark here
-    console.log(annotation)
     dataRows[row].annotations = (
-      <small>{annotation.description.description}</small>
+      <AnnotationIcon
+        annotation={annotation}
+        annotationFields={annotationDates}
+      />
     )
   })
+
+  const [cardAnnotations, setCardAnnotations] = useState(false)
+  const [highlightedAnnotation, setHighlightedAnnotation] = useState(false)
 
   return (
     <Layout
@@ -32,45 +67,63 @@ const StateCasesTemplate = ({ pageContext, path, data }) => {
       path={path}
       showWarning
     >
-      <Definitions
-        definitions={data.allContentfulDataDefinition.nodes}
-        order={[
-          'positive',
-          'positiveIncrease',
-          'positiveCasesViral',
-          'probableCases',
-        ]}
-      />
-      <TableResponsive
-        annotations={data.allContentfulChartAnnotation}
-        labels={[
-          {
-            field: 'annotations',
+      <AnnotationPanelContext.Provider
+        value={{
+          annotations,
+          setCardAnnotations: ({ fields, highlight }) => {
+            setCardAnnotations(fields)
+            setHighlightedAnnotation(highlight)
           },
-          {
-            field: 'date',
-            noWrap: true,
-          },
-          {
-            field: 'positive',
-            isNumeric: true,
-          },
-          {
-            field: 'positiveIncrease',
-            isNumeric: true,
-          },
-          {
-            field: 'positiveCasesViral',
-            isNumeric: true,
-            label: 'Confirmed cases',
-          },
-          {
-            field: 'probableCases',
-            isNumeric: true,
-          },
-        ]}
-        data={dataRows}
-      />
+        }}
+      >
+        {cardAnnotations && (
+          <DefinitionPanel
+            annotations={annotations}
+            highlightedDefinition={highlightedAnnotation}
+            onHide={() => setCardAnnotations(false)}
+            title={`${state.name} Annotations & Warnings`}
+          />
+        )}
+        <Definitions
+          definitions={data.allContentfulDataDefinition.nodes}
+          order={[
+            'positive',
+            'positiveIncrease',
+            'positiveCasesViral',
+            'probableCases',
+          ]}
+        />
+        <TableResponsive
+          annotations={data.allContentfulChartAnnotation}
+          labels={[
+            {
+              field: 'annotations',
+            },
+            {
+              field: 'date',
+              noWrap: true,
+            },
+            {
+              field: 'positive',
+              isNumeric: true,
+            },
+            {
+              field: 'positiveIncrease',
+              isNumeric: true,
+            },
+            {
+              field: 'positiveCasesViral',
+              isNumeric: true,
+              label: 'Confirmed cases',
+            },
+            {
+              field: 'probableCases',
+              isNumeric: true,
+            },
+          ]}
+          data={dataRows}
+        />
+      </AnnotationPanelContext.Provider>
     </Layout>
   )
 }
