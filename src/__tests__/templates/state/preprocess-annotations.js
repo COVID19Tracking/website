5580b5c1fde30befe08485f9eeafd9aed8779aff
Andrew Schwartz <andrew@aschwartz.me>
2020-12-21 18:30:01 -0700

    feat(test): add tests for preprocessing annotations

---
+++ src/__tests__/templates/state/preprocess-annotations.js
@@ -0,0 +1,74 @@
+import preprocessAnnotations from '~templates/state/preprocess-annotations'
+
+const data = [
+  {
+    state: 'TX',
+    date: 'Dec 20, 2020',
+    deathProbable: 946,
+    deathIncrease: 2,
+    deathConfirmed: 9437,
+    death: 10383,
+    recovered: null,
+  },
+  {
+    state: 'TX',
+    date: 'Dec 19, 2020',
+    deathProbable: 946,
+    deathIncrease: 49,
+    deathConfirmed: 9435,
+    death: 10381,
+    recovered: null,
+  },
+  {
+    state: 'TX',
+    date: 'Dec 18, 2020',
+    deathProbable: 936,
+    deathIncrease: 38,
+    deathConfirmed: 9396,
+    death: 10332,
+    recovered: null,
+  },
+]
+
+describe('Templates : State : preprocessAnnotations', () => {
+  describe('preprocessAnnotations', () => {
+    test('two annotations', () => {
+      const annotations = [
+        {
+          date: 'Dec 19, 2020',
+          childContentfulChartAnnotationDescriptionTextNode: {
+            childMarkdownRemark: {
+              html: '<p>Annotation.</p>',
+            },
+          },
+        },
+        {
+          date: 'Dec 20, 2020',
+          childContentfulChartAnnotationDescriptionTextNode: {
+            childMarkdownRemark: {
+              html: '<p>Annotation No. 2.</p>',
+            },
+          },
+        },
+      ]
+      expect(preprocessAnnotations(annotations, data)).toMatchSnapshot()
+    })
+    test('one annotation', () => {
+      const annotations = [
+        {
+          date: 'Dec 19, 2020',
+          childContentfulChartAnnotationDescriptionTextNode: {
+            childMarkdownRemark: {
+              html: '<p>Annotation.</p>',
+            },
+          },
+        },
+      ]
+      expect(preprocessAnnotations(annotations, data)).toMatchSnapshot()
+    })
+    test('no annotations', () => {
+      console.log()
+      expect(preprocessAnnotations([], data)).toMatchSnapshot()
+    })
+  })
+})
