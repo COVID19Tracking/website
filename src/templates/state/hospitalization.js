99e723d298de5c18d2968f11e4c8ac90012a6159
Andrew Schwartz <andrew@aschwartz.me>
2020-12-21 17:40:04 -0700

    feat(hospitalizations): add annotations to historical table

--- src/templates/state/hospitalization.js
+++ src/templates/state/hospitalization.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql } from 'gatsby'
 import TableResponsive from '~components/common/table-responsive'
 import Definitions from '~components/pages/data/definitions'
 import Layout from '~components/layout'
+import {
+  DefinitionPanel,
+  AnnotationPanelContext,
+} from '~components/pages/data/cards/definitions-panel'
+import tableResponsiveStyles from '~components/common/table-responsive.module.scss'
+
+import preprocessAnnotations from './preprocess-annotations'
 
 const StateHospitalizationTemplate = ({ pageContext, path, data }) => {
   const state = pageContext
   const { slug } = state.childSlug
+
+  const { annotations, dataRows } = preprocessAnnotations(
+    data.allContentfulChartAnnotation.nodes,
+    data.allCovidStateDaily.nodes,
+  )
+
+  const [cardAnnotations, setCardAnnotations] = useState(false)
+  const [highlightedAnnotation, setHighlightedAnnotation] = useState(false)
+
   return (
     <Layout
       title={`${state.name}: Hospitalization`}
@@ -17,55 +33,75 @@ const StateHospitalizationTemplate = ({ pageContext, path, data }) => {
       path={path}
       showWarning
     >
-      <Definitions
-        definitions={data.allContentfulDataDefinition.nodes}
-        order={[
-          'hospitalizedCumulative',
-          'hospitalizedCurrently',
-          'inIcuCumulative',
-          'inIcuCurrently',
-          'onVentilatorCumulative',
-          'onVentilatorCurrently',
-        ]}
-      />
+      <AnnotationPanelContext.Provider
+        value={{
+          annotations,
+          setCardAnnotations: ({ fields, highlight }) => {
+            setCardAnnotations(fields)
+            setHighlightedAnnotation(highlight)
+          },
+        }}
+      >
+        {cardAnnotations && (
+          <DefinitionPanel
+            annotations={annotations}
+            highlightedDefinition={highlightedAnnotation}
+            onHide={() => setCardAnnotations(false)}
+            title={`${state.name} Annotations & Warnings`}
+          />
+        )}
+        <Definitions
+          definitions={data.allContentfulDataDefinition.nodes}
+          order={[
+            'hospitalizedCumulative',
+            'hospitalizedCurrently',
+            'inIcuCumulative',
+            'inIcuCurrently',
+            'onVentilatorCumulative',
+            'onVentilatorCurrently',
+          ]}
+        />
 
-      <TableResponsive
-        labels={[
-          {
-            field: 'date',
-            noWrap: true,
-          },
-          {
-            field: 'hospitalizedCumulative',
-            isNumeric: true,
-          },
-          {
-            field: 'hospitalizedCurrently',
-            isNumeric: true,
-          },
-          {
-            field: 'hospitalizedIncrease',
-            isNumeric: true,
-          },
-          {
-            field: 'inIcuCumulative',
-            isNumeric: true,
-          },
-          {
-            field: 'inIcuCurrently',
-            isNumeric: true,
-          },
-          {
-            field: 'onVentilatorCumulative',
-            isNumeric: true,
-          },
-          {
-            field: 'onVentilatorCurrently',
-            isNumeric: true,
-          },
-        ]}
-        data={data.allCovidStateDaily.nodes}
-      />
+        <TableResponsive
+          labels={[
+            {
+              field: 'dateWithAnnotation',
+              noWrap: true,
+              style: tableResponsiveStyles.dateCell,
+              label: 'Date',
+            },
+            {
+              field: 'hospitalizedCumulative',
+              isNumeric: true,
+            },
+            {
+              field: 'hospitalizedCurrently',
+              isNumeric: true,
+            },
+            {
+              field: 'hospitalizedIncrease',
+              isNumeric: true,
+            },
+            {
+              field: 'inIcuCumulative',
+              isNumeric: true,
+            },
+            {
+              field: 'inIcuCurrently',
+              isNumeric: true,
+            },
+            {
+              field: 'onVentilatorCumulative',
+              isNumeric: true,
+            },
+            {
+              field: 'onVentilatorCurrently',
+              isNumeric: true,
+            },
+          ]}
+          data={dataRows}
+        />
+      </AnnotationPanelContext.Provider>
     </Layout>
   )
 }
@@ -88,6 +124,28 @@ export const query = graphql`
         inIcuCurrently
         onVentilatorCumulative
         onVentilatorCurrently
+      }
+    }
+    allContentfulChartAnnotation(
+      filter: {
+        state: { code: { eq: $state } }
+        dataElement: { eq: "hospitalizations" }
+      }
+      sort: { fields: date, order: DESC }
+    ) {
+      nodes {
+        title
+        description {
+          description
+        }
+        date(formatString: "MMM D, YYYY")
+        dataElement
+        contentful_id
+        childContentfulChartAnnotationDescriptionTextNode {
+          childMarkdownRemark {
+            html
+          }
+        }
       }
     }
     allContentfulDataDefinition(
