389325bcee34cf1f37dbf9e76f79df3f07a23962
Kevin Miller <keveemiller@gmail.com>
2021-01-07 12:35:40 -0800

    chore: Allow hiding warning

--- src/components/layout/header/data-warning.js
+++ src/components/layout/header/data-warning.js
@@ -1,10 +1,13 @@
-import React from 'react'
+/* eslint-disable */
+import React, { useState, useEffect } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import marked from 'marked'
 import Container from '~components/common/container'
 import dataWarningStyle from './data-warning.module.scss'
 
 const DataWarning = () => {
+  const [isHidden, setIsHidden] = useState(false)
+
   const data = useStaticQuery(graphql`
     {
       contentfulSnippet(slug: { eq: "data-warning" }) {
@@ -25,20 +28,47 @@ const DataWarning = () => {
   ) {
     return null
   }
+
+  useEffect(() => {
+    if (typeof document === 'undefined') {
+      return
+    }
+    if (document.cookie && document.cookie.search('ctp_warning') > -1) {
+      setIsHidden(true)
+    }
+  }, [])
+
   return (
-    <div className={dataWarningStyle.warning}>
-      <Container>
-        <p
-          role="alert"
-          dangerouslySetInnerHTML={{
-            __html: marked.inlineLexer(
-              data.contentfulSnippet.content.content,
-              [],
-            ),
-          }}
-        />
-      </Container>
-    </div>
+    <>
+      {!isHidden && (
+        <div className={dataWarningStyle.warning}>
+          <Container>
+            <button
+              aria-label="Hide this warning"
+              type="button"
+              onClick={event => {
+                event.preventDefault()
+                setIsHidden(false)
+                const date = new Date()
+                date.setTime(date.getTime() + 10 * 24 * 60 * 60 * 1000)
+                document.cookie = `ctp_warning=1; expires=${date.toISOString()}; path=/`
+              }}
+            >
+              &times;
+            </button>
+            <p
+              role="alert"
+              dangerouslySetInnerHTML={{
+                __html: marked.inlineLexer(
+                  data.contentfulSnippet.content.content,
+                  [],
+                ),
+              }}
+            />
+          </Container>
+        </div>
+      )}
+    </>
   )
 }
 
