90bcb6c9e36b39d43f4b2d23b8cf7fa6f2be2657
Kevin Miller <keveemiller@gmail.com>
2021-01-08 16:49:42 -0800

    chore: Add lede to notes page

--- src/components/pages/state/preamble.js
+++ src/components/pages/state/preamble.js
@@ -1,22 +1,31 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import StateGrade from '~components/pages/state/state-grade'
 import { DownloadDataRow } from '~components/pages/state/download-data'
-import { StateLinks } from '~components/pages/state/state-links'
 import preambleStyle from './preamble.module.scss'
 
-const StatePreamble = ({ state, urls, covidState }) => {
-  const { links } = urls.childTacoYaml
+const StatePreamble = ({ state, covidState }) => {
   // todo make state grade wrap as a circle with the grade description
   return (
     <div className={preambleStyle.preamble}>
       <h2 className={preambleStyle.header}>{state.name} Data Sources</h2>
-      <StateLinks
-        twitter={state.twitter}
-        links={links}
-        stateName={state.name}
-        stateSlug={state.childSlug.slug}
-        fullWidth
-      />
+      <ul>
+        <li>
+          <Link to={`/data/state/${state.childSlug.slug}/notes`}>
+            What you need to know about this data
+          </Link>
+        </li>
+        <li>
+          <a
+            className={preambleStyle.screenshots}
+            rel="noreferrer"
+            target="_blank"
+            href={`https://screenshots.covidtracking.com/${state.childSlug.slug}`}
+          >
+            <span>Sources and screenshots</span>
+          </a>
+        </li>
+      </ul>
       <DownloadDataRow
         slug={state.childSlug.slug}
         lastUpdateEt={covidState.dateModified}
--- src/templates/state/notes.js
+++ src/templates/state/notes.js
@@ -17,6 +17,12 @@ const StateNotesTemplate = ({ pageContext, path, data }) => {
       path={path}
       showWarning
     >
+      <p>
+        When {state.name} reports no data, several days of data, or unusual data
+        (such as decreases in values that should increase), our volunteers note
+        it here on the date the anomaly occurred. We also note here changes in
+        our own methodology that affect the data.
+      </p>
       <StateNotes notes={data.covidStateInfo.notes} />
     </Layout>
   )
