8dd7c1b9fd7e283cacc15ef84d315cbe01c144fa
Kevin Miller <keveemiller@gmail.com>
2021-01-11 15:24:38 -0800

    chore: Change order of preamble

--- src/components/pages/state/preamble.js
+++ src/components/pages/state/preamble.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import StateGrade from '~components/pages/state/state-grade'
 import Timezone from '~components/common/timezone'
 import preambleStyle from './preamble.module.scss'
+import { Row, Col } from '~components/common/grid'
 
 const StatePreamble = ({ state, covidState }) => {
   // todo make state grade wrap as a circle with the grade description
@@ -10,6 +11,11 @@ const StatePreamble = ({ state, covidState }) => {
     <div className={preambleStyle.preamble}>
       <h2 className={preambleStyle.header}>{state.name} Overview</h2>
       <ul>
+        <li>
+          <Link to={`/data/state/${state.childSlug.slug}/notes`}>
+            What you need to know about this data
+          </Link>
+        </li>
         <li>
           <a
             rel="noreferrer"
@@ -19,25 +25,22 @@ const StatePreamble = ({ state, covidState }) => {
             <span>Sources and screenshots</span>
           </a>
         </li>
-        <li>
-          <Link to={`/data/state/${state.childSlug.slug}/notes`}>
-            What you need to know about this data
-          </Link>
-        </li>
-        <li>
-          <StateGrade letterGrade={covidState.dataQualityGrade} />
-        </li>
-        <li>
+      </ul>
+      <Row>
+        <Col width={[4, 6, 4]}>
           Last updated {covidState.dateModified} <Timezone />
-        </li>
-        <li>
+        </Col>
+        <Col width={[4, 6, 4]}>
           Download data as a{' '}
           <a href={`/data/download/${state.childSlug.slug}-history.csv`}>
             CSV file
           </a>{' '}
           or with our <Link to="/data/api">API</Link>.
-        </li>
-      </ul>
+        </Col>
+        <Col width={[4, 6, 4]}>
+          <StateGrade letterGrade={covidState.dataQualityGrade} />
+        </Col>
+      </Row>
     </div>
   )
 }
