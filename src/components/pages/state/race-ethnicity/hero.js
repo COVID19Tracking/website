c0bafb13e9a46549ea0567c7cbfeb43444dcdc83
Andrew Schwartz <andrew@aschwartz.me>
2021-01-24 16:43:45 -0700

    fix(crdt-historical): update unavailable metrics
    show a metric as unavailable despite available total and unknown values

--- src/components/pages/state/race-ethnicity/hero.js
+++ src/components/pages/state/race-ethnicity/hero.js
@@ -34,8 +34,24 @@ const getMetrics = allData => {
     },
   }
 
+  const checkValue = (value, prefix) => {
+    /**
+     * Check if a value is actually reported by the state (true)
+     * or just an 'Unknown' or 'Total' value from CTP (false).
+     */
+    if (
+      value.startsWith(prefix) &&
+      !value.endsWith('Unknown') &&
+      !value.endsWith('Total')
+    ) {
+      return true
+    }
+    return false
+  }
+
   nonNullValues.every(value => {
-    if (value.startsWith('Cases_')) {
+    if (checkValue(value, 'Cases_')) {
+      console.log(value)
       metrics.Cases.available = true
       return false
     }
@@ -43,7 +59,7 @@ const getMetrics = allData => {
   })
 
   nonNullValues.every(value => {
-    if (value.startsWith('Deaths_')) {
+    if (checkValue(value, 'Deaths_')) {
       metrics.Deaths.available = true
       return false
     }
@@ -51,7 +67,7 @@ const getMetrics = allData => {
   })
 
   nonNullValues.every(value => {
-    if (value.startsWith('Hosp_')) {
+    if (checkValue(value, 'Hosp_')) {
       metrics.Hosp.available = true
       return false
     }
@@ -59,7 +75,7 @@ const getMetrics = allData => {
   })
 
   nonNullValues.every(value => {
-    if (value.startsWith('Tests_')) {
+    if (checkValue(value, 'Tests_')) {
       metrics.Tests.available = true
       return false
     }
@@ -67,6 +83,22 @@ const getMetrics = allData => {
   })
 
   return metrics
+}
+
+const getFirstAvailableMetric = metrics => {
+  /**
+   * Gets the first available metric for the state.
+   * Used to set the default state for the metric selector.
+   */
+  let firstMetric
+  Object.keys(metrics).every(metric => {
+    if (metrics[metric].available) {
+      firstMetric = metric
+      return false // break
+    }
+    return true
+  })
+  return firstMetric
 }
 
 const Hero = ({
@@ -79,13 +111,16 @@ const Hero = ({
   combinedNotes,
   separateNotes,
 }) => {
+  const metrics = getMetrics(timeSeriesData)
+  setCurrentMetric(getFirstAvailableMetric(metrics))
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.hero}>
         <MetricSelector
           state={currentMetric}
           setState={setCurrentMetric}
-          metrics={getMetrics(timeSeriesData)}
+          metrics={metrics}
         />
         <NotesAndDownloads
           slug={stateSlug}
