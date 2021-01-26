15fa6002d7bf4f65163e353122764796cae9fda8
Andrew Schwartz <andrew@aschwartz.me>
2021-01-26 11:52:25 -0700

    chore(crdt-historical): refactor metric-selector

--- src/components/pages/state/race-ethnicity/metric-selector.js
+++ src/components/pages/state/race-ethnicity/metric-selector.js
@@ -5,41 +5,60 @@ import alertBang from '~images/race-dashboard/alert-bang-orange.svg'
 
 import styles from './metric-selector.module.scss'
 
-const MetricSelector = ({ state, setState, metrics }) => {
-  // todo collapse into list on mobile like blog categories
-  // todo add popup for unavailable metrics
+const MetricButton = ({ children, option, metrics, state, setState }) => (
+  <button
+    key={option}
+    className={classnames(
+      state === option ? styles.active : undefined,
+      !metrics[option].available && styles.unavailable,
+    )}
+    title={option}
+    aria-checked={state === option}
+    onClick={event => {
+      event.preventDefault()
+      event.stopPropagation()
+      if (metrics[option].available) {
+        setState(option)
+      }
+    }}
+    role="radio"
+    type="button"
+    disabled={!metrics[option].available}
+  >
+    {children}
+  </button>
+)
 
+const MetricSelector = ({ state, setState, metrics }) => {
   return (
     <div className={styles.toggle} role="radiogroup">
       {Object.keys(metrics).map(option => (
-        <button
-          key={option}
-          className={classnames(
-            state === option ? styles.active : undefined,
-            !metrics[option].available && styles.unavailable,
+        <>
+          {!metrics[option].available ? (
+            <MetricButton
+              option={option}
+              metrics={metrics}
+              state={state}
+              setState={setState}
+            >
+              <img
+                src={alertBang}
+                alt={`${option} data is unavailable.`}
+                className={styles.bang}
+              />
+              <span>{option}</span>
+            </MetricButton>
+          ) : (
+            <MetricButton
+              option={option}
+              metrics={metrics}
+              state={state}
+              setState={setState}
+            >
+              <span>{option}</span>
+            </MetricButton>
           )}
-          title={option}
-          aria-checked={state === option}
-          onClick={event => {
-            event.preventDefault()
-            event.stopPropagation()
-            if (metrics[option].available) {
-              setState(option)
-            }
-          }}
-          role="radio"
-          type="button"
-          disabled={!metrics[option].available}
-        >
-          {!metrics[option].available && (
-            <img
-              src={alertBang}
-              alt={`${option} data is unavailable.`}
-              className={styles.bang}
-            />
-          )}
-          <span>{option}</span>
-        </button>
+        </>
       ))}
     </div>
   )
