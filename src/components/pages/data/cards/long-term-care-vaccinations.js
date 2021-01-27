22e40139c75a6994a0e24c7e201956a65b90aa1b
Kevin Miller <keveemiller@gmail.com>
2021-01-26 17:25:53 -0800

    chore: Update fields in LTC vaccination card

--- src/components/pages/data/cards/long-term-care-vaccinations.js
+++ src/components/pages/data/cards/long-term-care-vaccinations.js
@@ -1,50 +1,26 @@
 import React from 'react'
-import { Card, CardBody } from '~components/common/card'
+import { Card, CardBody, CardNote } from '~components/common/card'
 import { Statistic } from '~components/common/statistic'
 
 const LongTermCareVaccinations = ({ ltcFedVaccinations }) => (
   <Card title="Vaccinations">
     <CardBody>
+      <CardNote>
+        Vaccinations administered through the Pharmacy Partnership for Long-Term
+        Care Program
+      </CardNote>
       <Statistic
-        title="Administered_Fed_LTC"
+        title="Total resident &amp; staff doses administered"
         value={ltcFedVaccinations.Administered_Fed_LTC}
       />
       <Statistic
-        title="Administered_Fed_LTC_Dose1"
+        title="Staff &amp; resident 1st dose administered"
         value={ltcFedVaccinations.Administered_Fed_LTC_Dose1}
-      />
-      <Statistic
-        title="Administered_Fed_LTC_Dose1_Residents"
-        value={ltcFedVaccinations.Administered_Fed_LTC_Dose1_Residents}
-      />
-      <Statistic
-        title="Administered_Fed_LTC_Dose1_Staff"
-        value={ltcFedVaccinations.Administered_Fed_LTC_Dose1_Staff}
-      />
-      <Statistic
-        title="Administered_Fed_LTC_Dose1_Unk"
-        value={ltcFedVaccinations.Administered_Fed_LTC_Dose1_Unk}
       />
 
       <Statistic
-        title="Administered_Fed_LTC_Dose2"
+        title="Staff &amp;resident 2nd dose administered"
         value={ltcFedVaccinations.Administered_Fed_LTC_Dose2}
-      />
-      <Statistic
-        title="Administered_Fed_LTC_Dose2_Residents"
-        value={ltcFedVaccinations.Administered_Fed_LTC_Dose2_Residents}
-      />
-      <Statistic
-        title="Administered_Fed_LTC_Dose2_Staff"
-        value={ltcFedVaccinations.Administered_Fed_LTC_Dose2_Staff}
-      />
-      <Statistic
-        title="Administered_Fed_LTC_Dose2_Staff"
-        value={ltcFedVaccinations.Administered_Fed_LTC_Dose2_Staff}
-      />
-      <Statistic
-        title="Administered_Fed_LTC_Dose2_Unk"
-        value={ltcFedVaccinations.Administered_Fed_LTC_Dose2_Unk}
       />
     </CardBody>
   </Card>
