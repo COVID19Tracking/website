a77c02c902ea2757d27b8c023b597e9604402b05
Andrew Schwartz <andrew@aschwartz.me>
2021-01-30 21:36:27 -0700

    feat(table-responsive): add disclosure
    supports hiding rows on mobile

--- src/components/common/table-responsive.js
+++ src/components/common/table-responsive.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import classnames from 'classnames'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@reach/disclosure'
+
 import { FieldName } from '~components/utils/field-name'
 import { FormatNumber } from '~components/utils/format'
+
 import tableStyles from './table.module.scss'
 import tableResponsiveStyles from './table-responsive.module.scss'
 
-const TableResponsive = ({ labels, data, header }) => (
+const TableRow = ({ labels, row }) => (
+  <tr>
+    {labels.map(
+      ({ field, label, format, isNumeric, noWrap, alignLeft, style }) => (
+        <td
+          className={classnames(
+            noWrap && tableResponsiveStyles.noWrap,
+            alignLeft && tableStyles.alignLeft,
+            style,
+          )}
+        >
+          <span className={tableResponsiveStyles.cellLabel} aria-hidden>
+            {label || <FieldName field={field} />}
+          </span>
+          <span className={tableResponsiveStyles.value}>
+            {isNumeric ? (
+              <FormatNumber number={row[field]} />
+            ) : (
+              <>{format ? format(row[field]) : row[field] || 'N/A'}</>
+            )}
+          </span>
+        </td>
+      ),
+    )}
+  </tr>
+)
+
+const TableResponsive = ({
+  labels,
+  data,
+  header,
+  mobileShowNRows = data.length, // the number of rows to show on mobile
+}) => (
   <table className={classnames(tableStyles.table, tableResponsiveStyles.table)}>
     {header && (
       <thead className={tableResponsiveStyles.headerNoBorder}>{header}</thead>
@@ -26,32 +65,27 @@ const TableResponsive = ({ labels, data, header }) => (
       </tr>
     </thead>
     <tbody>
-      {data.map(row => (
-        <tr>
-          {labels.map(
-            ({ field, label, format, isNumeric, noWrap, alignLeft, style }) => (
-              <td
-                className={classnames(
-                  noWrap && tableResponsiveStyles.noWrap,
-                  alignLeft && tableStyles.alignLeft,
-                  style,
-                )}
-              >
-                <span className={tableResponsiveStyles.cellLabel} aria-hidden>
-                  {label || <FieldName field={field} />}
-                </span>
-                <span className={tableResponsiveStyles.value}>
-                  {isNumeric ? (
-                    <FormatNumber number={row[field]} />
-                  ) : (
-                    <>{format ? format(row[field]) : row[field] || 'N/A'}</>
-                  )}
-                </span>
-              </td>
-            ),
-          )}
-        </tr>
-      ))}
+      <>
+        {data.slice(0, mobileShowNRows).map(row => (
+          <TableRow labels={labels} row={row} />
+        ))}
+        <Disclosure>
+          <tr className={tableResponsiveStyles.disclosureButton}>
+            <td colSpan={labels.length}>
+              <DisclosureButton>
+                <span className={tableResponsiveStyles.closed}>Show</span>
+                <span className={tableResponsiveStyles.expanded}>Hide</span> all
+                rows
+              </DisclosureButton>
+            </td>
+          </tr>
+          <DisclosurePanel className={tableResponsiveStyles.disclosurePanel}>
+            {data.slice(mobileShowNRows, data.length).map(row => (
+              <TableRow labels={labels} row={row} />
+            ))}
+          </DisclosurePanel>
+        </Disclosure>
+      </>
     </tbody>
   </table>
 )
