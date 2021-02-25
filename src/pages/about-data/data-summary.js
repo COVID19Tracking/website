86f0a9e254a3a66258a5c6e5bd46c3fcc4dacef6
Kevin Miller <keveemiller@gmail.com>
2021-02-25 10:39:10 -0800

    chore:Use categories

--- src/pages/about-data/data-summary.js
+++ src/pages/about-data/data-summary.js
@@ -2,54 +2,87 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '~components/layout'
 
-const DataSummaryPage = ({ data }) => (
-  <Layout title="Data Summary">
-    <p>[Lede content TK]</p>
-    {data.allContentfulDataSummary.nodes.map(summary => (
-      <div key={summary.slug} id={summary.slug}>
-        <h3>{summary.title}</h3>
-        <p>
-          <strong>
-            <a href={summary.sourceLink}>Data source</a>
-          </strong>
-        </p>
-        <div
-          dangerouslySetInnerHTML={{
-            __html:
-              summary.childContentfulDataSummaryDescriptionTextNode
-                .childMarkdownRemark.html,
-          }}
-        />
-        <h4>How to use it</h4>
-        <div
-          dangerouslySetInnerHTML={{
-            __html:
-              summary.childContentfulDataSummaryUseTextNode.childMarkdownRemark
-                .html,
-          }}
-        />
-      </div>
-    ))}
-  </Layout>
-)
+const categoryOrder = [
+  'testing-outcomes-data',
+  'long-term-care-datasets',
+  'vaccine-metadata',
+  'city-data',
+  'miscellaneous-repositories',
+]
+
+const DataSummaryPage = ({ data }) => {
+  const categories = categoryOrder.map(slug =>
+    data.allContentfulDataSummaryCategory.nodes.find(
+      node => node.slug === slug,
+    ),
+  )
+  return (
+    <Layout title="Data Summary">
+      <p>[Lede content TK]</p>
+      {categories.map(({ title, summaries }) => (
+        <>
+          <h2>{title}</h2>
+          {summaries.map(summary => (
+            <div key={summary.slug} id={summary.slug}>
+              <h3>{summary.title}</h3>
+              <p>
+                <strong>
+                  <a href={summary.sourceLink}>Data source</a>
+                </strong>
+                {summary.downloadLink && (
+                  <>
+                    {' | '}
+                    <strong>
+                      <a href={summary.downloadLink}>Download data</a>
+                    </strong>
+                  </>
+                )}
+              </p>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html:
+                    summary.childContentfulDataSummaryDescriptionTextNode
+                      .childMarkdownRemark.html,
+                }}
+              />
+              <h4>How to use it</h4>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html:
+                    summary.childContentfulDataSummaryUseTextNode
+                      .childMarkdownRemark.html,
+                }}
+              />
+            </div>
+          ))}
+        </>
+      ))}
+    </Layout>
+  )
+}
 
 export default DataSummaryPage
 
 export const query = graphql`
   {
-    allContentfulDataSummary {
+    allContentfulDataSummaryCategory {
       nodes {
         title
         slug
-        sourceLink
-        childContentfulDataSummaryUseTextNode {
-          childMarkdownRemark {
-            html
+        summaries {
+          title
+          slug
+          sourceLink
+          downloadLink
+          childContentfulDataSummaryUseTextNode {
+            childMarkdownRemark {
+              html
+            }
           }
-        }
-        childContentfulDataSummaryDescriptionTextNode {
-          childMarkdownRemark {
-            html
+          childContentfulDataSummaryDescriptionTextNode {
+            childMarkdownRemark {
+              html
+            }
           }
         }
       }
