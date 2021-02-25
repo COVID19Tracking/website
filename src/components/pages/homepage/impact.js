c240d9bccd66e9f1809e31c87067190219583717
Kevin Miller <keveemiller@gmail.com>
2021-02-25 09:37:00 -0800

    chore: Make wall of faces

--- src/components/pages/homepage/impact.js
+++ src/components/pages/homepage/impact.js
@@ -1,26 +1,20 @@
 import React from 'react'
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
+import Container from '~components/common/container'
+import faces from '~data/sample-faces.json'
 import impactStyle from './impact.module.scss'
 
 const HomepageImpact = () => {
-  const people = [1, 2, 3]
   return (
-    <ParallaxProvider>
+    <Container>
       <div className={impactStyle.root}>
-        {people.map(person => (
-          <Parallax
-            key={person}
-            y={[-1 * Math.random() * 100, Math.random() * 100]}
-          >
-            <img
-              className={impactStyle.image}
-              alt={`person-${person}`}
-              src="http://placeimg.com/140/200/animals/grayscale"
-            />
-          </Parallax>
+        {faces.map(person => (
+          <span
+            className={impactStyle.image}
+            style={{ backgroundImage: `url(${person.url})` }}
+          />
         ))}
       </div>
-    </ParallaxProvider>
+    </Container>
   )
 }
 
