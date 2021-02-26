5616ea80d10026d034cd723561ee8ccab7e41660
Kevin Miller <keveemiller@gmail.com>
2021-02-26 14:18:49 -0800

    chore: Add lede content

--- src/pages/about-data/data-summary.js
+++ src/pages/about-data/data-summary.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import DataSummary from '~components/pages/about-data/data-summary'
+import ContentfulContent from '~components/common/contentful-content'
 import Layout from '~components/layout'
 
-const DataSummaryPage = () => (
+const DataSummaryPage = ({ data }) => (
   <Layout title="Data Summary" centered>
-    <p>[Lede content TK]</p>
+    <ContentfulContent
+      content={
+        data.contentfulSnippet.childContentfulSnippetContentTextNode
+          .childMarkdownRemark.html
+      }
+      id={data.contentfulSnippet.contentful_id}
+    />
     <DataSummary />
   </Layout>
 )
 
 export default DataSummaryPage
+
+export const query = graphql`
+  query {
+    contentfulSnippet(slug: { eq: "data-summary-lede" }) {
+      contentful_id
+      childContentfulSnippetContentTextNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`
