428e5dbeec6a3d90d0f0ea0d22898409dc547768
Kevin Miller <keveemiller@gmail.com>
2021-02-26 16:30:23 -0800

    chore: Remove unset Link

--- src/templates/state/index.js
+++ src/templates/state/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '~components/layout'
 import StateNavWrapper from '~components/common/state-nav-wrapper'
 import StatePreamble from '~components/pages/state/preamble'
