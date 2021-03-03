ee152e5d6d401f59539d8aeae83682b4aeb7d9c5
Andrew Schwartz <andrew@aschwartz.me>
2021-03-02 22:11:39 -0700

    chore(crdt-breakout): move secondary to AvailabilityUpdatePanel

---
+++ src/components/pages/race/breakouts/availability-update-panel.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import Percent from '~components/pages/race/dashboard/percent'
+import styles from './hero.module.scss'
+
+const AvailabilityNote = ({ core, testHosp, combined }) => {
+  const Availability = ({ value, title }) => (
+    <span>
+      <Percent number={value} /> {title}
+    </span>
+  )
+
+  if (combined) {
+    return (
+      <div className={styles.availabilityWrapper}>
+        <p>Race/ethnicity data available for:</p>
+        <div className={styles.availabilityGrid}>
+          <Availability
+            value={parseFloat(core.knownRaceEthPos)}
+            title="Cases"
+          />
+          <Availability
+            value={parseFloat(core.knownRaceEthDeath)}
+            title="Deaths"
+          />
+          <Availability
+            value={parseFloat(testHosp.knownRaceEthTest)}
+            title="Tests"
+          />
+          <Availability
+            value={parseFloat(testHosp.knownRaceEthHosp)}
+            title="Hospitalizations"
+          />
+        </div>
+      </div>
+    )
+  }
+  if (!combined) {
+    return (
+      <div className={styles.availabilityWrapper}>
+        <p>Race data available for:</p>
+        <div className={styles.availabilityGrid}>
+          <Availability value={parseFloat(core.knownRacePos)} title="Cases" />
+          <Availability
+            value={parseFloat(core.knownRaceDeath)}
+            title="Deaths"
+          />
+          <Availability
+            value={parseFloat(testHosp.knownRaceTest)}
+            title="Tests"
+          />
+          <Availability
+            value={parseFloat(testHosp.knownRaceHosp)}
+            title="Hospitalizations"
+          />
+        </div>
+        <p>Ethnicity data available for:</p>
+        <div className={styles.availabilityGrid}>
+          <Availability value={parseFloat(core.knownEthPos)} title="Cases" />
+          <Availability value={parseFloat(core.knownEthDeath)} title="Deaths" />
+          <Availability
+            value={parseFloat(testHosp.knownEthTest)}
+            title="Tests"
+          />
+          <Availability
+            value={parseFloat(testHosp.knownEthHosp)}
+            title="Hospitalizations"
+          />
+        </div>
+      </div>
+    )
+  }
+  return null
+}
+
+const AvailabilityUpdatePanel = ({ coreData, testHospData, isCombined }) => {
+  return (
+    <div className={styles.secondary}>
+      <AvailabilityNote
+        core={coreData}
+        testHosp={testHospData}
+        combined={isCombined}
+      />
+      <div>Last updated</div>
+    </div>
+  )
+}
+
+export default AvailabilityUpdatePanel
--- src/components/pages/race/breakouts/hero.js
+++ src/components/pages/race/breakouts/hero.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import Percent from '~components/pages/race/dashboard/percent'
 import Container from '~components/common/container'
 import { GradeInner } from '~components/pages/state/state-grade'
 
 import rightCaret from '~images/icons/right-caret-dark.svg'
 import styles from './hero.module.scss'
+import AvailabilityUpdatePanel from './availability-update-panel'
 
 const Hero = ({
   stateName,
@@ -19,79 +19,6 @@ const Hero = ({
   const isCombined = combinedData !== null
   const coreData = isCombined ? combinedData : separateData
   const testHospData = isCombined ? combinedTestHosp : separateTestHosp
-
-  const AvailabilityNote = ({ core, testHosp, combined }) => {
-    const Availability = ({ value, title }) => (
-      <span>
-        <Percent number={value} /> {title}
-      </span>
-    )
-
-    if (combined) {
-      return (
-        <div className={styles.availabilityWrapper}>
-          <p>Race/ethnicity data available for:</p>
-          <div className={styles.availabilityGrid}>
-            <Availability
-              value={parseFloat(core.knownRaceEthPos)}
-              title="Cases"
-            />
-            <Availability
-              value={parseFloat(core.knownRaceEthDeath)}
-              title="Deaths"
-            />
-            <Availability
-              value={parseFloat(testHosp.knownRaceEthTest)}
-              title="Tests"
-            />
-            <Availability
-              value={parseFloat(testHosp.knownRaceEthHosp)}
-              title="Hospitalizations"
-            />
-          </div>
-        </div>
-      )
-    }
-    if (!combined) {
-      return (
-        <div className={styles.availabilityWrapper}>
-          <p>Race data available for:</p>
-          <div className={styles.availabilityGrid}>
-            <Availability value={parseFloat(core.knownRacePos)} title="Cases" />
-            <Availability
-              value={parseFloat(core.knownRaceDeath)}
-              title="Deaths"
-            />
-            <Availability
-              value={parseFloat(testHosp.knownRaceTest)}
-              title="Tests"
-            />
-            <Availability
-              value={parseFloat(testHosp.knownRaceHosp)}
-              title="Hospitalizations"
-            />
-          </div>
-          <p>Ethnicity data available for:</p>
-          <div className={styles.availabilityGrid}>
-            <Availability value={parseFloat(core.knownEthPos)} title="Cases" />
-            <Availability
-              value={parseFloat(core.knownEthDeath)}
-              title="Deaths"
-            />
-            <Availability
-              value={parseFloat(testHosp.knownEthTest)}
-              title="Tests"
-            />
-            <Availability
-              value={parseFloat(testHosp.knownEthHosp)}
-              title="Hospitalizations"
-            />
-          </div>
-        </div>
-      )
-    }
-    return null
-  }
 
   return (
     <div className={styles.container}>
@@ -108,14 +35,11 @@ const Hero = ({
             link={`/data/state/${stateSlug}/assessment#long-term-care`}
           />
         </div>
-        <div className={styles.secondary}>
-          <AvailabilityNote
-            core={coreData}
-            testHosp={testHospData}
-            combined={isCombined}
-          />
-          <div>Last updated</div>
-        </div>
+        <AvailabilityUpdatePanel
+          coreData={coreData}
+          testHospData={testHospData}
+          isCombined={isCombined}
+        />
       </Container>
     </div>
   )
