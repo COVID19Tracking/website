086fe3be4a9d34e3f95d0b47a7e0124271df943d
Andrew Schwartz <andrew@aschwartz.me>
2021-03-07 21:27:49 -0700

    feat(cumulative-notes): add styles

--- src/components/pages/race/breakouts/cumulative-notes.js
+++ src/components/pages/race/breakouts/cumulative-notes.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { getGroups } from '~components/social-media-graphics/race/utils'
 import { FormatDate } from '~components/utils/format'
 
+import styles from './cumulative-notes.module.scss'
+
 const CumulativeNotes = ({
   availableMetrics,
   state,
@@ -16,9 +18,12 @@ const CumulativeNotes = ({
     deaths: 'died from',
   }
   return (
-    <div>
-      Cumulative Per Capita Data In {state.name}, through{' '}
-      <FormatDate date={lastUpdated} format="LLLL d yyyy" />:
+    <div className={styles.container}>
+      <h4 className={styles.header}>Cumulative Per Capita Data</h4>
+      <p>
+        In {state.name}, through{' '}
+        <FormatDate date={lastUpdated} format="LLLL d yyyy" />:
+      </p>
       <ul>
         {availableMetrics.map(metric => (
           <li>
