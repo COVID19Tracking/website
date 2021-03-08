7ec4679af5cc250ae1e5a485bf81057ba1c54bb3
Kevin Miller <keveemiller@gmail.com>
2021-03-08 10:19:20 -0800

    chore: Add text to volunteer page

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -4,6 +4,13 @@ import ThankYou from '~components/pages/thank-you'
 
 const ThankYouPage = () => (
   <Layout title="Thank You to Our Volunteers">
+    <h2>Our volunteers</h2>
+    <p>
+      Every day for a year, hundreds of COVID Tracking Project contributors from
+      all walks of life compiled, published, and interpreted vitally important
+      COVID-19 data as a service to their fellow Americans. We offer thanks and
+      heartfelt gratitude for their labor and sacrifice.
+    </p>
     <ThankYou />
   </Layout>
 )
