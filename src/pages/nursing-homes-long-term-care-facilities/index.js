4a4df224e21209b756c55be7eeb7a1bdf730fe68
Kevin Miller <kevin@kevee.net>
2021-04-08 10:45:20 -0700

    chore: Remove snippet filter

--- src/pages/nursing-homes-long-term-care-facilities/index.js
+++ src/pages/nursing-homes-long-term-care-facilities/index.js
@@ -9,60 +9,49 @@ import Layout from '~components/layout'
 import Paragraph from '~components/common/landing-page/paragraph'
 import { CtaLink } from '~components/common/call-to-action'
 
-const LongTermCarePage = ({ data }) => {
-  const Snippet = ({ slug }) => {
-    const item = data.contentfulSnippetCollection.snippets.find(
-      snippet => snippet.slug === slug,
-    )
-    return (
-      <ContentfulContent
-        id={item.contentful_id}
-        content={
-          item.childContentfulSnippetContentTextNode.childMarkdownRemark.html
-        }
+const LongTermCarePage = ({ data }) => (
+  <Layout
+    title="The Long-Term Care COVID Tracker"
+    path="/nursing-homes-long-term-care-facilities"
+    description="To date, the Long-Term Care COVID Tracker is the most comprehensive dataset about COVID-19 in US long-term care facilities."
+  >
+    <Paragraph detail="Using state and federal data, we can estimate that as of March 2021:">
+      <span
+        dangerouslySetInnerHTML={{
+          __html: marked.inlineLexer(data.lede.content.content, []),
+        }}
       />
-    )
-  }
+    </Paragraph>
+    <ContentfulContent
+      id={data.contentfulSnippet.contentful_id}
+      content={
+        data.contentfulSnippet.childContentfulSnippetContentTextNode
+          .childMarkdownRemark.html
+      }
+    />
+    <Container centered>
+      <ImageBlock
+        keepSize
+        imageUrl="/images/cms-ctp.png"
+        image={{
+          description:
+            'Chart comparing Covid Tracking Project data to federal CMS data.',
+        }}
+        caption="[TKTK]"
+      />
+    </Container>
 
-  return (
-    <Layout
-      title="The Long-Term Care COVID Tracker"
-      path="/nursing-homes-long-term-care-facilities"
-      description="To date, the Long-Term Care COVID Tracker is the most comprehensive dataset about COVID-19 in US long-term care facilities."
-    >
-      <Paragraph detail="Using state and federal data, we can estimate that as of March 2021:">
-        <span
-          dangerouslySetInnerHTML={{
-            __html: marked.inlineLexer(data.lede.content.content, []),
-          }}
-        />
-      </Paragraph>
-
-      <Snippet slug="ltc-top-notes" />
-      <Container centered>
-        <ImageBlock
-          keepSize
-          imageUrl="/images/cms-ctp.png"
-          image={{
-            description:
-              'Chart comparing Covid Tracking Project data to federal CMS data.',
-          }}
-          caption="[TKTK]"
-        />
-      </Container>
-
-      <Container centered>
-        <h2>Our Analysis &amp; Updates</h2>
-        {data.allContentfulBlogPost.nodes.map(post => (
-          <BlogItem post={post} extraMargin hideReadLink />
-        ))}
-        <CtaLink bold to="/analysis-updates/category/long-term-care">
-          See all our analysis of long-term care data
-        </CtaLink>
-      </Container>
-    </Layout>
-  )
-}
+    <Container centered>
+      <h2>Our Analysis &amp; Updates</h2>
+      {data.allContentfulBlogPost.nodes.map(post => (
+        <BlogItem post={post} extraMargin hideReadLink />
+      ))}
+      <CtaLink bold to="/analysis-updates/category/long-term-care">
+        See all our analysis of long-term care data
+      </CtaLink>
+    </Container>
+  </Layout>
+)
 
 export default LongTermCarePage
 
@@ -87,20 +76,12 @@ export const query = graphql`
         }
       }
     }
-    contentfulSnippetCollection(slug: { eq: "long-term-care-landing-page" }) {
-      snippets {
-        contentful_id
-        slug
-        childContentfulSnippetContentTextNode {
-          childMarkdownRemark {
-            html
-          }
+    contentfulSnippet(slug: { eq: "ltc-top-notes" }) {
+      contentful_id
+      childContentfulSnippetContentTextNode {
+        childMarkdownRemark {
+          html
         }
-      }
-    }
-    lede: contentfulSnippet(slug: { eq: "ltc-lede" }) {
-      content {
-        content
       }
     }
   }
