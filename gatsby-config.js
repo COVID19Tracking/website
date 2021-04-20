526e71b5a381c568f2a037a8a56d9a90d2babe6d
Kevin Miller <keveemiller@gmail.com>
2021-04-20 11:03:21 -0700

    chore: Remove environment

--- gatsby-config.js
+++ gatsby-config.js
@@ -427,7 +427,6 @@ const gatsbyConfig = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE,
         accessToken: process.env.CONTENTFUL_TOKEN,
-        environment: 'ltc-landing-page',
         host: process.env.CONTENTFUL_PREVIEW
           ? 'preview.contentful.com'
           : 'cdn.contentful.com',
