3d16dc32077294232bfc9c2446083fa516cbc8eb
Kevin Miller <keveemiller@gmail.com>
2021-04-21 12:13:22 -0700

    chore: Fix assessment date

--- src/templates/state/notes.js
+++ src/templates/state/notes.js
@@ -7,7 +7,12 @@ import Layout from '~components/layout'
 const StateNotesTemplate = ({ pageContext, path, data }) => {
   const state = pageContext
   const { slug } = state.childSlug
-  const { covidState, covidStateInfo, covidGradeStateAssessment } = data
+  const {
+    covidState,
+    covidStateInfo,
+    covidGradeStateAssessment,
+    assessmentDate,
+  } = data
   return (
     <Layout
       title={`${state.name} Notes`}
@@ -21,6 +26,7 @@ const StateNotesTemplate = ({ pageContext, path, data }) => {
         state={state}
         covidState={covidState}
         assessment={covidGradeStateAssessment}
+        assessmentDate={assessmentDate.date}
         hideNotesLink
       />
       <StateNotes stateName={state.name} notes={covidStateInfo.notes} />
@@ -43,6 +49,9 @@ export const query = graphql`
       taco
       ltc
       crdt
+    }
+    assessmentDate: covidGradeStateAssessment(date: { ne: null }) {
+      date
     }
   }
 `
