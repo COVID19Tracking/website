6f67a32dc3f393c5b61d2e02393a00b6a5074870
Kevin Miller <keveemiller@gmail.com>
2021-05-18 12:54:14 -0700

    chore: Add packages

--- functions/preserve-thread/preserve-thread.js
+++ functions/preserve-thread/preserve-thread.js
@@ -1,4 +1,12 @@
+const Airtable = require('airtable')
+const { WebClient } = require('@slack/web-api')
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  'appbpvthclasxpwu7',
+)
+const slackClient = new WebClient(process.env.SLACK_PRESERVER_TOKEN)
+
 exports.handler = async event => {
   console.log(event)
-  return { statusCode: 200, body: 'Done' }
+
+  return { statusCode: 200, body: 'Success' }
 }
