eac43966ea5ae87482887b20c1644698418559d9
Kevin Miller <keveemiller@gmail.com>
2021-05-18 13:19:34 -0700

    chore: Add challenge

--- functions/preserve-thread/preserve-thread.js
+++ functions/preserve-thread/preserve-thread.js
@@ -6,7 +6,24 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 const slackClient = new WebClient(process.env.SLACK_PRESERVER_TOKEN)
 
 exports.handler = async event => {
+  if (event.queryStringParameters.challenge) {
+    return { statusCode: 200, body: event.queryStringParameters.challenge }
+  }
   console.log(event)
-
-  return { statusCode: 200, body: 'Success' }
+  base('Threads')
+    .create([
+      {
+        fields: {
+          ID: '1',
+          Channel: 'data-entry',
+          Link: 'http://something.com',
+        },
+      },
+    ])
+    .then(response => {
+      return { statusCode: 200, body: 'Success' }
+    })
+    .catch(error => {
+      console.log(error)
+    })
 }
