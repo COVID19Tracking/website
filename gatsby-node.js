290866ea57eb9ca33bd16acafa98dcd39d4ac3bf
Kevin Miller <kevin@kevee.net>
2022-06-04 06:45:55 -0700

    chore: Redirect all blog URLs

--- gatsby-node.js
+++ gatsby-node.js
@@ -268,6 +268,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   result.data.allContentfulBlogPost.nodes.forEach(node => {
     const longPath = `/analysis-updates/${node.slug}`
+    const oldPath = `/blog/${node.slug}`
     const shortPath = `/${node.contentful_id}`
 
     if (node.overrideBlogPage) {
@@ -285,6 +286,12 @@ exports.createPages = async ({ graphql, actions }) => {
 
       createRedirect({
         fromPath: shortPath,
+        toPath: longPath,
+        isPermanent: true,
+      })
+
+      createRedirect({
+        fromPath: oldPath,
         toPath: longPath,
         isPermanent: true,
       })
